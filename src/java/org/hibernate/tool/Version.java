26a5564d3818389723ee4cb4521841bee7435836
Max Rydahl Andersen <max@hibernate.org>
2010-09-22 22:21:59 +0000

    Fix JBIDE-7145 set proper versions
    git-svn-id: https://anonsvn.jboss.org/repos/hibernate/tools/trunk@20683 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "3.3.0.GA";
+	public static final String VERSION = "3.4.0.Beta1";
 	
 	private static final Version instance = new Version();
 	
