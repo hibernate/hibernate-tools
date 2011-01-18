fe5d0a21334949994ceed35a803630e5b5dcda3c
Dmitry Geraskov <dgeraskov@exadel.com>
2011-01-18 13:01:02 +0000

    https://issues.jboss.org/browse/JBIDE-6396 Updated version to 3.4.0.CR1
    git-svn-id: https://anonsvn.jboss.org/repos/hibernate/tools/trunk@20878 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "3.4.0.Beta1";
+	public static final String VERSION = "3.4.0.CR1";
 	
 	private static final Version instance = new Version();
 	
