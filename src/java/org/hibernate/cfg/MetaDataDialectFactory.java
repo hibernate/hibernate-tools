8a3c096afcfffadb96c4c9a39a4b956be27cb354
Koen Aers <koen.aers@gmail.com>
2012-09-05 15:56:20 +0200

    comment out autodiscovery of SQLServer until properly verified

--- src/java/org/hibernate/cfg/MetaDataDialectFactory.java
+++ src/java/org/hibernate/cfg/MetaDataDialectFactory.java
@@ -8,14 +8,14 @@ import org.hibernate.cfg.reveng.dialect.JDBCMetaDataDialect;
 import org.hibernate.cfg.reveng.dialect.MetaDataDialect;
 import org.hibernate.cfg.reveng.dialect.MySQLMetaDataDialect;
 import org.hibernate.cfg.reveng.dialect.OracleMetaDataDialect;
-import org.hibernate.cfg.reveng.dialect.SQLServerMetaDataDialect;
+//import org.hibernate.cfg.reveng.dialect.SQLServerMetaDataDialect;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.dialect.H2Dialect;
 import org.hibernate.dialect.HSQLDialect;
 import org.hibernate.dialect.MySQLDialect;
 import org.hibernate.dialect.Oracle8iDialect;
 import org.hibernate.dialect.Oracle9Dialect;
-import org.hibernate.dialect.SQLServerDialect;
+//import org.hibernate.dialect.SQLServerDialect;
 import org.hibernate.util.ReflectHelper;
 
 public class MetaDataDialectFactory {
@@ -62,8 +62,8 @@ public class MetaDataDialectFactory {
 				return new MySQLMetaDataDialect();
 			} else if (dialect instanceof HSQLDialect) {
 				return new HSQLMetaDataDialect();
-			}else if (dialect instanceof SQLServerDialect) {
-				return new SQLServerMetaDataDialect();
+//			}else if (dialect instanceof SQLServerDialect) {
+//				return new SQLServerMetaDataDialect();
 			}			
 		}
 		return null;
@@ -82,9 +82,9 @@ public class MetaDataDialectFactory {
 		if (dialect.toLowerCase().contains("hsql")) {
 			return new HSQLMetaDataDialect();
 		}
-		if (dialect.toLowerCase().contains("sqlserver")) {
-			return new SQLServerMetaDataDialect();
-		}
+//		if (dialect.toLowerCase().contains("sqlserver")) {
+//			return new SQLServerMetaDataDialect();
+//		}
 		return null;
 	}
 
