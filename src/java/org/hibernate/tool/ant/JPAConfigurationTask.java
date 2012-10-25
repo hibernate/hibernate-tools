2129e1f9a47c74b270f72d868d234135b01da95e
hydra1983 <hydra1983@gmail.com>
2012-10-25 13:26:08 +0800

    scan Entities in multi jars.

--- src/java/org/hibernate/tool/ant/JPAConfigurationTask.java
+++ src/java/org/hibernate/tool/ant/JPAConfigurationTask.java
@@ -3,84 +3,113 @@ package org.hibernate.tool.ant;
 import java.io.File;
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 import org.apache.tools.ant.BuildException;
 import org.hibernate.HibernateException;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.util.ReflectHelper;
+import org.reflections.Reflections;
 import org.xml.sax.EntityResolver;
 
 public class JPAConfigurationTask extends ConfigurationTask {
-	
+
 	private String persistenceUnit = null;
+
+	private String basePackage = null;
 
 	public JPAConfigurationTask() {
 		setDescription("JPA Configuration");
 	}
-	
+
 	protected Configuration createConfiguration() {
 		try {
 			Map overrides = new HashMap();
 			Properties p = getProperties();
-			
-			if(p!=null) {
-				overrides.putAll( p );
+
+			if (p != null) {
+				overrides.putAll(p);
 			}
 
-			Class clazz = ReflectHelper.classForName("org.hibernate.ejb.Ejb3Configuration", JPAConfigurationTask.class);
+			Class clazz = ReflectHelper.classForName(
+					"org.hibernate.ejb.Ejb3Configuration",
+					JPAConfigurationTask.class);
 			Object ejb3cfg = clazz.newInstance();
-			
-			if(entityResolver!=null) {
-				Class resolver = ReflectHelper.classForName(entityResolver, this.getClass());
+
+			if (entityResolver != null) {
+				Class resolver = ReflectHelper.classForName(entityResolver,
+						this.getClass());
 				Object object = resolver.newInstance();
-				Method method = clazz.getMethod("setEntityResolver", new Class[] { EntityResolver.class });
-				method.invoke(ejb3cfg, new Object[] { object } );
+				Method method = clazz.getMethod("setEntityResolver",
+						new Class[] { EntityResolver.class });
+				method.invoke(ejb3cfg, new Object[] { object });
 			}
-			
-			Method method = clazz.getMethod("configure", new Class[] { String.class, Map.class });
-			if ( method.invoke(ejb3cfg, new Object[] { persistenceUnit, overrides } ) == null ) {
-				throw new BuildException("Persistence unit not found: '" + persistenceUnit + "'.");
+
+			Method method = clazz.getMethod("configure", new Class[] {
+					String.class, Map.class });
+			if (method.invoke(ejb3cfg, new Object[] { persistenceUnit,
+					overrides }) == null) {
+				throw new BuildException("Persistence unit not found: '"
+						+ persistenceUnit + "'.");
 			}
-			
+
+			if (basePackage != null) {
+				Method addAnnotatedClass = clazz.getMethod("addAnnotatedClass",
+						new Class[] { Class.class });
+				Reflections reflections = new Reflections(basePackage, null);
+				Class entity = ReflectHelper.classForName(
+						"javax.persistence.Entity", JPAConfigurationTask.class);
+				Set jpaEntities = reflections.getTypesAnnotatedWith(entity);
+				Iterator i = jpaEntities.iterator();
+				while (i.hasNext()) {
+					Object entityClass = i.next();
+					addAnnotatedClass.invoke(ejb3cfg,
+							new Object[] { entityClass });
+				}
+			}
+
 			method = clazz.getMethod("getHibernateConfiguration", new Class[0]);
 			return (Configuration) method.invoke(ejb3cfg, null);
-		} 
-		catch(HibernateException he) {
+		} catch (HibernateException he) {
 			throw new BuildException(he);
-		}
-		catch(BuildException be) {
+		} catch (BuildException be) {
 			throw be;
+		} catch (Exception t) {
+			throw new BuildException(
+					"Problems in creating a configuration for JPA. Have you remembered to add hibernate EntityManager jars to the classpath ?",
+					t);
 		}
-		catch(Exception t) {
-			throw new BuildException("Problems in creating a configuration for JPA. Have you remembered to add hibernate EntityManager jars to the classpath ?",t);			
-		}
-		
+
 	}
-	
+
 	protected void doConfiguration(Configuration configuration) {
 	}
-	
+
 	protected void validateParameters() throws BuildException {
-		
+
 	}
-	
+
 	public String getPersistenceUnit() {
 		return persistenceUnit;
 	}
-	
+
 	public void setPersistenceUnit(String persistenceUnit) {
 		this.persistenceUnit = persistenceUnit;
 	}
-	
+
 	public void setConfigurationFile(File configurationFile) {
 		complain("configurationfile");
 	}
 
 	private void complain(String param) {
-		throw new BuildException("<" + getTaskName() + "> currently only support autodiscovery from META-INF/persistence.xml. Thus setting the " + param + " attribute is not allowed");
+		throw new BuildException(
+				"<"
+						+ getTaskName()
+						+ "> currently only support autodiscovery from META-INF/persistence.xml. Thus setting the "
+						+ param + " attribute is not allowed");
 	}
-	
-	
+
 }
