f1920d552af5d2e2083815326c3d24308ab496e5
Koen Aers <koen.aers@gmail.com>
2015-07-24 15:26:58 +0200

    Update version to 4.0.0.Final

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "4.0.0";
+	public static final String VERSION = "4.0.0.Final";
 	
 	private static final Version instance = new Version();
 	
