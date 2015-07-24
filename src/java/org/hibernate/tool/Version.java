06ff4381144df67a5dde28c23f1dc1bc5754940a
Koen Aers <koen.aers@gmail.com>
2015-07-24 19:43:26 +0200

    Update version to 5.0.0.Alpha1

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "4.3.1";
+	public static final String VERSION = "5.0.0.Alpha1";
 	
 	private static final Version instance = new Version();
 	
