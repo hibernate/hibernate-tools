e64ad3752fefb619fb7708c514b378249e88c7f1
Koen Aers <koen.aers@gmail.com>
2015-07-24 12:30:01 +0200

    Update version to 3.6.0.Final

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "3.6.0";
+	public static final String VERSION = "3.6.0.Final";
 	
 	private static final Version instance = new Version();
 	
