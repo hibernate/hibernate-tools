2f93a80c79a41f79c44d455d419bdf0462a7354d
Koen Aers <koen.aers@gmail.com>
2015-07-27 17:57:06 +0200

    Replace jboss-ejb3x.jar by javaee-api-7.0.jar

--- src/test/org/hibernate/tool/hbm2x/Hbm2EJBDaoTest.java
+++ src/test/org/hibernate/tool/hbm2x/Hbm2EJBDaoTest.java
@@ -49,7 +49,7 @@ public class Hbm2EJBDaoTest extends NonReflectiveTestCase {
 		List jars = new ArrayList();
 		jars.add("commons-logging-1.0.4.jar");
 		jars.add("hibernate-jpa-2.0-api-1.0.1.Final.jar");
-		jars.add("jboss-ejb3x.jar");
+		jars.add("javaee-api-7.0.jar");
 		TestHelper.compile(getOutputDir(), file, TestHelper.visitAllFiles(getOutputDir(), list), "1.5", TestHelper.buildClasspath(jars) );
 		
 		
