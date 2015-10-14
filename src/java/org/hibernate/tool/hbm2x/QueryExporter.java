2e38a7b293fb39da807dd03b9e9a2c1b85eace8a
Koen Aers <koen.aers@gmail.com>
2015-10-14 14:21:46 +0200

    Cleanup org.hibernate.tool.hbm2x.QueryExporter

--- src/java/org/hibernate/tool/hbm2x/QueryExporter.java
+++ src/java/org/hibernate/tool/hbm2x/QueryExporter.java
@@ -6,11 +6,19 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 
 import org.hibernate.HibernateException;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.SessionFactoryObserver;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.boot.registry.internal.StandardServiceRegistryImpl;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.internal.util.config.ConfigurationHelper;
+import org.hibernate.service.ServiceRegistry;
 
 /** 
  * exporter for query execution.
@@ -19,21 +27,37 @@ import org.hibernate.Session;
 public class QueryExporter extends AbstractExporter {
 
 	private String filename;
-	private List queryStrings;
+	private List<String> queryStrings;
 
+	@SuppressWarnings({ "serial", "unchecked" })
 	public void doStart() {
 		Session session = null;
 		SessionFactory sessionFactory = null;
 		Transaction transaction = null;
-		try {		 
-			sessionFactory = getConfiguration().buildSessionFactory();
+		ServiceRegistry serviceRegistry = null;
+		try {	
+			Configuration configuration = getConfiguration();
+			Properties properties = configuration.getProperties();
+			Environment.verifyProperties( getConfiguration().getProperties() );
+			ConfigurationHelper.resolvePlaceHolders( properties );
+			serviceRegistry =  new StandardServiceRegistryBuilder()
+					.applySettings( properties )
+					.build();
+			configuration.setSessionFactoryObserver(
+				new SessionFactoryObserver() {
+					public void sessionFactoryCreated(SessionFactory factory) {
+					}
+					public void sessionFactoryClosed(SessionFactory factory) {
+					}
+				}
+			);
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 			session = sessionFactory.openSession();
 			transaction = session.beginTransaction();
-			// TODO: this is not the most efficient loop (opening/closing file)
-			for (Iterator iter = queryStrings.iterator(); iter.hasNext();) {
+			for (Iterator<String> iter = queryStrings.iterator(); iter.hasNext();) {
 				String query = (String) iter.next();
 				
-				List list = session.createQuery(query).list();
+				List<Object> list = session.createQuery(query).list();
 				
 				if(getFileName()!=null) {
 					PrintWriter pw = null;
@@ -43,7 +67,7 @@ public class QueryExporter extends AbstractExporter {
 						pw = new PrintWriter( new FileWriter( file, true ) );			
 						getArtifactCollector().addFile( file, "query-output" );
 						
-						for (Iterator iter1 = list.iterator(); iter1.hasNext();) {
+						for (Iterator<Object> iter1 = list.iterator(); iter1.hasNext();) {
 							Object element = iter1.next();
 							pw.println(element);
 						}
@@ -71,8 +95,10 @@ public class QueryExporter extends AbstractExporter {
 			}
 			if(sessionFactory!=null) {
 				sessionFactory.close();
+				if (serviceRegistry != null) {
+					( (StandardServiceRegistryImpl) serviceRegistry ).destroy();
+				}
 			}
-			
 			
 		}
 	}
@@ -85,7 +111,7 @@ public class QueryExporter extends AbstractExporter {
 		this.filename = filename;
 	}
 	
-	public void setQueries(List queryStrings) {
+	public void setQueries(List<String> queryStrings) {
 		this.queryStrings = queryStrings;		
 	}
 
