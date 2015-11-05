bf11c342ef60371c1c9be3d8552e01c4bac0fdec
Koen Aers <koen.aers@gmail.com>
2015-11-05 12:50:41 +0100

    Add a test for JDBCMetaDataConfiguration.readFromJDBC()

---
+++ src/test/org/hibernate/cfg/JDBCMetaDataConfigurationTest.java
@@ -0,0 +1,72 @@
+package org.hibernate.cfg;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
+import org.hibernate.service.ServiceRegistry;
+
+import junit.framework.TestCase;
+
+public class JDBCMetaDataConfigurationTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		executeDDL(getCreateSQL());
+	}
+
+	protected void tearDown() throws Exception {
+		executeDDL(getDropSQL());
+	}
+
+	public void testReadFromJDBC() throws Exception {
+		JDBCMetaDataConfiguration cfg = new JDBCMetaDataConfiguration();
+		cfg.readFromJDBC();
+		assertNotNull("Withrealtimestamp", cfg.getClassMapping("Withrealtimestamp"));
+		assertNotNull("Noversion", cfg.getClassMapping("Noversion"));
+		assertNotNull("Withfaketimestamp", cfg.getClassMapping("Withfaketimestamp"));
+		assertNotNull("Withversion", cfg.getClassMapping("Withversion"));
+	}
+
+	private void executeDDL(String[] sqls) throws SQLException {
+		Configuration configuration = new Configuration();
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
+		builder.applySettings(configuration.getProperties());
+		ServiceRegistry serviceRegistry = builder.build();
+		ConnectionProvider connectionProvider = serviceRegistry.getService(ConnectionProvider.class);
+		Statement statement = null;
+		Connection con = null;
+		con = connectionProvider.getConnection();
+
+		statement = con.createStatement();
+
+		for (int i = 0; i < sqls.length; i++) {
+			String ddlsql = sqls[i];
+			statement.execute(ddlsql);
+			con.commit();
+		}
+
+		if (statement != null)
+			statement.close();
+		connectionProvider.closeConnection(con);
+
+	}
+
+	private String[] getCreateSQL() {
+		return new String[] {
+				"create table withVersion (first int, second int, version int, name varchar(256), primary key (first))",
+				"create table noVersion (first int, second int, name varchar(256), primary key (second))",
+				"create table withRealTimestamp (first int, second int, timestamp timestamp, name varchar(256), primary key (first))",
+				"create table withFakeTimestamp (first int, second int, timestamp int, name varchar(256), primary key (first))", };
+	}
+
+	private String[] getDropSQL() {
+		return new String[] { 
+				"drop table withVersion", 
+				"drop table noVersion", 
+				"drop table withRealTimestamp",
+				"drop table withFakeTimestamp" };
+	}
+
+}
