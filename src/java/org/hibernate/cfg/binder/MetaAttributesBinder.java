b35522c8bff3e79adab959ae0d58817e93acdefb
Koen Aers <koen.aers@gmail.com>
2015-11-19 08:54:50 +0100

    Change TableIdentifier argument in MetaAttributesBinder.bindMetaAttribures(...) to Table

--- src/java/org/hibernate/cfg/binder/MetaAttributesBinder.java
+++ src/java/org/hibernate/cfg/binder/MetaAttributesBinder.java
@@ -8,18 +8,19 @@ import org.hibernate.cfg.reveng.TableIdentifier;
 import org.hibernate.mapping.Column;
 import org.hibernate.mapping.Property;
 import org.hibernate.mapping.Selectable;
+import org.hibernate.mapping.Table;
 
 public class MetaAttributesBinder {
 
     public static Property bindMetaAttributes(
     		Property property, 
     		ReverseEngineeringStrategy revengStrategy,
-    		TableIdentifier identifier) {
+    		Table table) {
     	Iterator<Selectable> columnIterator = property.getValue().getColumnIterator();
 		while(columnIterator.hasNext()) {
 			Column col = (Column) columnIterator.next();
 			Map<?,?> map = revengStrategy.columnToMetaAttributes( 
-					identifier, 
+					TableIdentifier.create(table), 
 					col.getName());
 			if(map!=null) { 
 				property.setMetaAttributes(map);
