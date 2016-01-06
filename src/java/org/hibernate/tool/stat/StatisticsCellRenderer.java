9d29e659b104a6c4ec4c762029d22c77c9fd5c4e
Koen Aers <koen.aers@gmail.com>
2016-01-06 15:15:20 +0100

    Add @SuppressWarnings('serial') annotation to StatisticsCellRenderer

--- src/java/org/hibernate/tool/stat/StatisticsCellRenderer.java
+++ src/java/org/hibernate/tool/stat/StatisticsCellRenderer.java
@@ -14,6 +14,7 @@ import org.hibernate.stat.EntityStatistics;
 import org.hibernate.stat.QueryStatistics;
 import org.hibernate.stat.Statistics;
 
+@SuppressWarnings("serial")
 public class StatisticsCellRenderer extends DefaultTreeCellRenderer {
 
 	SimpleDateFormat formatter = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
