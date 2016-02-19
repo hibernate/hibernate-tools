105696f6a763fe7d2dbef401f3e0f86bb3a7409e
Koen Aers <koen.aers@gmail.com>
2016-02-19 11:08:06 +0100

    Get ready for the 5.0.0.Alpha3 release

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.0.0.Alpha1";
+	public static final String VERSION = "5.0.0.Alpha3";
 	
 	private static final Version instance = new Version();
 	
