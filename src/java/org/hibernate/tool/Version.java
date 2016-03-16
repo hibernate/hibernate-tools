6a4239f5d834d2e998e3f0451ecdbd8e19968fbf
Koen Aers <koen.aers@gmail.com>
2016-03-16 11:44:25 +0100

    Update version attribute to 5.0.0.Beta1 for next development cycle

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.0.0.Alpha3";
+	public static final String VERSION = "5.0.0.Beta1";
 	
 	private static final Version instance = new Version();
 	
