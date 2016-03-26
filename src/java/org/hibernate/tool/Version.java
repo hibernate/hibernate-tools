8311148b01db144e8703685f173f4270fcc59c5c
Koen Aers <koen.aers@gmail.com>
2016-03-26 10:42:45 +0100

    Prepare for next development iteration - Update version to 5.1.0.Alpha3

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.1.0.Alpha2";
+	public static final String VERSION = "5.1.0.Alpha3";
 	
 	private static final Version instance = new Version();
 	
