e44f7613c3663281cd666e115368b21e7a29332e
Koen Aers <koen.aers@gmail.com>
2016-03-26 10:44:08 +0100

    Prepare for next development interation - Update version to 5.0.0.CR1

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.0.0.Beta1";
+	public static final String VERSION = "5.0.0.CR1";
 	
 	private static final Version instance = new Version();
 	
