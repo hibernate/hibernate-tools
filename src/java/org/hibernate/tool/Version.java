ec057a7e99fcdc2e87cdd32d5bd47c750fc28548
Koen Aers <koen.aers@gmail.com>
2016-04-21 06:56:21 +0200

    Prepare for 4.3.3.Final release

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "4.3.2-SNAPSHOT";
+	public static final String VERSION = "4.3.3.Final";
 	
 	private static final Version instance = new Version();
 	
