ab4445dc03bc8f6934d876d8dba3d18ed183ea2e
Koen Aers <koen.aers@gmail.com>
2016-04-21 15:17:59 +0200

    Prepare for 5.0.0.Final, update version strings

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.0.0.CR1";
+	public static final String VERSION = "5.0.0.Final";
 	
 	private static final Version instance = new Version();
 	
