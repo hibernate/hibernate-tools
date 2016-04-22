26dba42be927b0eeeeb27662215616e81583d696
Koen Aers <koen.aers@gmail.com>
2016-04-22 19:23:07 +0200

    Prepare for next devopment cycle: update version to 3.6.1.Final

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "3.6.0.Final";
+	public static final String VERSION = "3.6.1.Final";
 	
 	private static final Version instance = new Version();
 	
