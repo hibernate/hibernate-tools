64917e53593f4e743449f1bda42a4cae302fd1c9
Koen Aers <koen.aers@gmail.com>
2016-05-18 12:45:37 +0200

    HBX-1255: Clean up class org.hibernate.tool.hbm2x.hbm2hbmxml.Address2

--- src/test/org/hibernate/tool/hbm2x/hbm2hbmxml/Address2.java
+++ src/test/org/hibernate/tool/hbm2x/hbm2hbmxml/Address2.java
@@ -10,7 +10,7 @@ import java.util.HashSet;
  */
 public class Address2 {
 	private Long id;
-	private Set lines = new HashSet();
+	private Set<String> lines = new HashSet<String>();
 
 	public Long getId() {
 		return id;
@@ -20,11 +20,11 @@ public class Address2 {
 		this.id = id;
 	}
 
-	public Set getLines() {
+	public Set<String> getLines() {
 		return lines;
 	}
 
-	public void setLines(Set lines) {
+	public void setLines(Set<String> lines) {
 		this.lines = lines;
 	}
 }
