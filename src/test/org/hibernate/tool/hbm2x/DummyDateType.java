ce46cf3a2f0b206030b72c6c9747d39b0f0b2699
Koen Aers <koen.aers@gmail.com>
2016-07-06 11:04:29 +0200

    HBX-1255: Clean up class org.hibernate.tool.hbm2x.DummyDateType

--- src/test/org/hibernate/tool/hbm2x/DummyDateType.java
+++ src/test/org/hibernate/tool/hbm2x/DummyDateType.java
@@ -21,55 +21,51 @@ public class DummyDateType implements UserType {
 	}
 
 	public boolean equals(Object x, Object y) throws HibernateException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public int hashCode(Object x) throws HibernateException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public Object deepCopy(Object value) throws HibernateException {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public boolean isMutable() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public Serializable disassemble(Object value) throws HibernateException {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public Object assemble(Serializable cached, Object owner) throws HibernateException {
-		// TODO Auto-generated method stub
+	public Object assemble(Serializable cached, Object owner) 
+			throws HibernateException {
 		return null;
 	}
 
-	public Object replace(Object original, Object target, Object owner) throws HibernateException {
-		// TODO Auto-generated method stub
+	public Object replace(
+			Object original, 
+			Object target, 
+			Object owner) throws HibernateException {
 		return null;
 	}
 	
 	//@Override
-	public Object nullSafeGet(ResultSet rs, String[] names,
-			SharedSessionContractImplementor session, Object owner)
-			throws HibernateException, SQLException {
-		// TODO Auto-generated method stub
+	public Object nullSafeGet(
+			ResultSet rs, 
+			String[] names,
+			SharedSessionContractImplementor session, 
+			Object owner) throws HibernateException, SQLException {
 		return null;
 	}
 
 	//@Override
-	public void nullSafeSet(PreparedStatement st, Object value, int index,
-			SharedSessionContractImplementor session) throws HibernateException, SQLException {
-		// TODO Auto-generated method stub
-		
-	}
-
-
+	public void nullSafeSet(
+			PreparedStatement st, 
+			Object value, 
+			int index,
+			SharedSessionContractImplementor session) throws HibernateException, SQLException {}
 
 }
