57e74a71aed19aa9320400788ed5338fd60cca56
Koen Aers <koen.aers@gmail.com>
2016-07-12 10:47:49 +0200

    HBX-1272: Release version 5.2.0.Alpha2

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.2.0.Alpha1";
+	public static final String VERSION = "5.2.0.Alpha2";
 	
 	private static final Version instance = new Version();
 	
