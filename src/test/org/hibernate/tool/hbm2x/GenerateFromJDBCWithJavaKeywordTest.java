bf0b0ea9a2f92a9800a14d8f9f391078dacb4bd5
Koen Aers <koen.aers@gmail.com>
2016-07-19 12:51:15 +0200

    HBX-1261: reveng *.java emitter does not escape java keyword 'return' as variable name

---
+++ src/test/org/hibernate/tool/hbm2x/GenerateFromJDBCWithJavaKeywordTest.java
@@ -0,0 +1,86 @@
+package org.hibernate.tool.hbm2x;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+
+import org.hibernate.cfg.JDBCMetaDataConfiguration;
+import org.hibernate.cfg.reveng.DefaultReverseEngineeringStrategy;
+import org.hibernate.cfg.reveng.OverrideRepository;
+import org.hibernate.cfg.reveng.ReverseEngineeringSettings;
+import org.hibernate.tool.JDBCMetaDataBinderTestCase;
+import org.hibernate.tool.test.TestHelper;
+import org.junit.Assert;
+
+/**
+ * @author koen@hibernate.org
+ */
+public class GenerateFromJDBCWithJavaKeywordTest extends JDBCMetaDataBinderTestCase {
+	
+	private static String REVENG_XML =
+			"<!DOCTYPE hibernate-reverse-engineering                                            \n"+
+			"          SYSTEM                                                                   \n"+
+	        "          'http://hibernate.sourceforge.net/hibernate-reverse-engineering-3.0.dtd'>\n"+
+			"<hibernate-reverse-engineering>                                                    \n"+
+	        "   <table name='my_return_history'>                                                \n"+
+            "      <foreign-key                                                                 \n"+
+            "            constraint-name='FK_MY_RETURN_HISTORY_RETURN_ID'                       \n"+
+            "            foreign-table='my_return'>                                             \n"+
+            "          <column-ref local-column='my_return_ref' foreign-column='return_id'/>        \n"+
+            "          <many-to-one property=\"return\"/>                                       \n"+
+            "      </foreign-key>                                                               \n"+
+	        "   </table>                                                                        \n"+
+			"</hibernate-reverse-engineering>                                                     ";
+
+	public GenerateFromJDBCWithJavaKeywordTest() {
+		super("genfromjdbcjavakeyword");
+	}
+
+	
+	protected String[] getCreateSQL() {		
+		return new String[] {
+				"create table my_return (                            "+
+				"   return_id varchar(20) not null,                  "+
+				"   constraint pk_my_return primary key (return_id) )",
+				
+				"create table my_return_history ( "+
+				"   id varchar(20) not null, "+
+				"   my_return_ref varchar(20), "+
+				"   constraint pk_my_return_history primary key (id),"+
+				"   constraint fk_my_return_history_return_id foreign key (my_return_ref) references my_return(return_id) )"			
+		};
+	}
+
+	protected String[] getDropSQL() {		
+		return new String[]  {
+				"drop table my_return_history",
+				"drop table my_return",				
+		};
+	}
+	
+	protected void configure(JDBCMetaDataConfiguration cfg2configure) {
+		DefaultReverseEngineeringStrategy configurableNamingStrategy = new DefaultReverseEngineeringStrategy();
+		configurableNamingStrategy.setSettings(new ReverseEngineeringSettings(configurableNamingStrategy).setDefaultPackageName("org.reveng").setCreateCollectionForForeignKey(false));
+		cfg2configure.setReverseEngineeringStrategy(configurableNamingStrategy);
+		OverrideRepository overrideRepository = new OverrideRepository();
+		InputStream inputStream = new ByteArrayInputStream(REVENG_XML.getBytes());
+		overrideRepository.addInputStream(inputStream);
+		cfg2configure.setReverseEngineeringStrategy(overrideRepository.getReverseEngineeringStrategy(configurableNamingStrategy));
+	}
+	
+	public void testGenerateJava() throws Exception {	
+		POJOExporter exporter = new POJOExporter(cfg,getOutputDir());		
+		exporter.start();
+		File myReturn = new File(getOutputDir(), "org/reveng/MyReturn.java");
+		Assert.assertTrue(myReturn.exists());
+		File myReturnHistory = new File(getOutputDir(), "org/reveng/MyReturnHistory.java");
+		Assert.assertTrue(myReturnHistory.exists());
+		try {
+			TestHelper.compile(getOutputDir(), getOutputDir());
+			Assert.fail();
+		} catch (RuntimeException e) {
+			Assert.assertTrue(e.getMessage().contains("Syntax error on token \"return\""));
+		}
+	}
+	
+}
