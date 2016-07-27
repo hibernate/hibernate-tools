9b23f260c8baad9ce455a450858984054a3bdc35
Koen Aers <koen.aers@gmail.com>
2016-07-27 10:03:16 +0200

    HBX-1276: Prepare for 5.1.0.Beta2 release

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.1.0.Beta1";
+	public static final String VERSION = "5.1.0.Beta2";
 	
 	private static final Version instance = new Version();
 	
