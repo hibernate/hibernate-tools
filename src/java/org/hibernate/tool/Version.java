9a993705cc1ddd42300e2584ee3152e6ab84055a
Koen Aers <koen.aers@gmail.com>
2016-08-21 08:46:32 +0200

    HBX-1273: Prepare for 5.1.0.CR1

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.1.0.Beta2";
+	public static final String VERSION = "5.1.0.CR1";
 	
 	private static final Version instance = new Version();
 	
