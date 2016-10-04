6c2393ffd84b6550f3e15d7555c1aa121278432c
Koen Aers <koen.aers@gmail.com>
2016-10-04 16:11:52 +0200

    HBX-1297: Create org.hibernate.tool.hbm2x.HibernateMappingExporterTest

---
+++ src/test/org/hibernate/tool/hbm2x/HibernateMappingExporterTest.java
@@ -0,0 +1,51 @@
+package org.hibernate.tool.hbm2x;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import org.hibernate.cfg.PojoMetaDataConfiguration;
+import org.hibernate.mapping.Column;
+import org.hibernate.mapping.RootClass;
+import org.hibernate.mapping.SimpleValue;
+import org.hibernate.mapping.Table;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HibernateMappingExporterTest {
+
+	private static final File TMP_DIR = new File(System.getProperty("java.io.tmpdir"));
+
+	@Test
+	public void testStart() throws Exception {
+		PojoMetaDataConfiguration configuration = new PojoMetaDataConfiguration();
+		RootClass persistentClass = new RootClass(null);
+		Table table = new Table("FOO");
+		Column keyColumn = new Column("BAR");
+		SimpleValue key = new SimpleValue(configuration.getMetadataImplementor());
+		key.setTypeName("String");
+		key.addColumn(keyColumn);
+		key.setTable(table);
+		persistentClass.setClassName("Foo");
+		persistentClass.setEntityName("Foo");
+		persistentClass.setJpaEntityName("Foo");
+		persistentClass.setTable(table);
+		persistentClass.setIdentifier(key);
+		configuration.addClass(persistentClass);	
+		final File outputDir = new File(TMP_DIR, "HibernateMappingExporterTest.testStart");
+		outputDir.mkdir();
+		HibernateMappingExporter exporter = new HibernateMappingExporter(configuration, outputDir);
+		final File fooHbmXml = new File(outputDir, "Foo.hbm.xml");
+		Assert.assertFalse(fooHbmXml.exists());
+		exporter.start();
+		Assert.assertTrue(fooHbmXml.exists());
+		BufferedReader br = new BufferedReader(new FileReader(fooHbmXml));
+		 String line = null;
+		 while ((line = br.readLine()) != null) {
+		   System.out.println(line);
+		 }	
+		 br.close();
+		 Assert.assertTrue(fooHbmXml.delete());
+	}
+
+}
