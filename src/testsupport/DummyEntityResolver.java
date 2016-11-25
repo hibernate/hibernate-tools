ca70b3dd8e9b68f03f1900877ab05f72318acd18
Koen Aers <koen.aers@gmail.com>
2016-11-25 10:05:05 +0100

    HBX-1308: Remove references to DTDEntityResolver

--- src/testsupport/DummyEntityResolver.java
+++
@@ -1,9 +0,0 @@
-import org.hibernate.tool.xml.DTDEntityResolver;
-/**
- * Dummy entity resolver used merely for testing.
- * @author Max Rydahl Andersen
- *
- */
-public class DummyEntityResolver extends DTDEntityResolver {
-
-}
