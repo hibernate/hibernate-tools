32810fa6030eb067f86592d62f3abb49a3d7310c
Koen Aers <koen.aers@gmail.com>
2016-12-05 16:38:36 +0100

    HBX-1299: Increment version identifier to 5.2.0.Final

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.2.0.CR1";
+	public static final String VERSION = "5.2.0.Final";
 	
 	private static final Version instance = new Version();
 	
