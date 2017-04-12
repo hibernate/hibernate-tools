211c4f2513fc118288677a41a94a64e86443c2c4
Koen Aers <koen.aers@gmail.com>
2017-04-12 09:47:38 +0200

    HBX-1344: Update version identifier to 5.0.6.Final

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.0.6-SNAPSHOT";
+	public static final String VERSION = "5.0.6.Final";
 	
 	private static final Version instance = new Version();
 	
