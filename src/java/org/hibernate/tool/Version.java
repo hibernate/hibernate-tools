bf95f0c5134e73ef860af85094026d222a698504
Koen Aers <koen.aers@gmail.com>
2017-04-12 11:08:20 +0200

    HBX-1357: Update version identifier to 5.0.7-SNAPSHOT

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.0.6.Final";
+	public static final String VERSION = "5.0.7-SNAPSHOT";
 	
 	private static final Version instance = new Version();
 	
