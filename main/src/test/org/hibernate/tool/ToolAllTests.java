4b09f905de8e42e3dc6e0fe16091e66f05814097
Koen Aers <koen.aers@gmail.com>
2017-04-15 10:57:02 +0200

    HBX-1356: Create a 'No Database' Integration Test Project to Include the Tests that are Database Independent - Remove the tests in 'org.hibernate.tool.ide.completion' from the hibernate-tools project

--- main/src/test/org/hibernate/tool/ToolAllTests.java
+++ main/src/test/org/hibernate/tool/ToolAllTests.java
@@ -1,11 +1,10 @@
 package org.hibernate.tool;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
-
 import org.hibernate.tool.hbm2x.query.QueryAllTests;
 import org.hibernate.tool.hbmlint.HbmLintAllTests;
-import org.hibernate.tool.ide.completion.CompletionAllTests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
 
 public class ToolAllTests {
 
@@ -16,7 +15,6 @@ public static Test suite() {
 	suite.addTest(org.hibernate.tool.hbm2x.hbm2hbmxml.Cfg2HbmAllTests.suite() );
 	suite.addTest(org.hibernate.tool.test.jdbc2cfg.Jdbc2CfgAllTests.suite() );
 	suite.addTest(org.hibernate.tool.hbm2x.Hbm2XAllTests.suite() );
-	suite.addTest(CompletionAllTests.suite() );
 	suite.addTest(QueryAllTests.suite() );
 	suite.addTest(HbmLintAllTests.suite() );
 	return suite;
