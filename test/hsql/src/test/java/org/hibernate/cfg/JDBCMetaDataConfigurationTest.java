4adc0033b98c00cdf18b7e1328f98fd854354e62
Koen Aers <koen.aers@gmail.com>
2017-04-20 20:42:53 +0200

    HBX-1352: Move the Hibernate Tools integration tests to the hibernate-tools-hsql-tests project - Use 'org.hibernate.tools.test.util.JdbcUtil' in 'org.hibernate.cfg.JDBCMetaDataConfigurationTest'

--- test/hsql/src/test/java/org/hibernate/cfg/JDBCMetaDataConfigurationTest.java
+++ test/hsql/src/test/java/org/hibernate/cfg/JDBCMetaDataConfigurationTest.java
@@ -1,22 +1,38 @@
 package org.hibernate.cfg;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 import org.hibernate.boot.Metadata;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
-import org.hibernate.service.ServiceRegistry;
+import org.hibernate.tools.test.util.JdbcUtil;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class JDBCMetaDataConfigurationTest {
 
+	private static final String[] CREATE_SQL = new String[] {
+			"create table withVersion (first int, second int, version int, name varchar(256), primary key (first))",
+			"create table noVersion (first int, second int, name varchar(256), primary key (second))",
+			"create table withRealTimestamp (first int, second int, timestamp timestamp, name varchar(256), primary key (first))",
+			"create table withFakeTimestamp (first int, second int, timestamp int, name varchar(256), primary key (first))", 
+		};
+
+	private static final String[] DROP_SQL = new String[] {
+			"drop table withVersion", 
+			"drop table noVersion", 
+			"drop table withRealTimestamp",
+			"drop table withFakeTimestamp" 
+		};
+
 	@Before
-	public void setUp() throws Exception {
-		executeDDL(getCreateSQL());
+	public void setUp() {
+		JdbcUtil.establishJdbcConnection(this);
+		JdbcUtil.executeDDL(this, CREATE_SQL);
+	}
+
+	@After
+	public void tearDown() {
+		JdbcUtil.executeDDL(this, DROP_SQL);
+		JdbcUtil.releaseJdbcConnection(this);
 	}
 
 	@Test
@@ -28,38 +44,6 @@ public class JDBCMetaDataConfigurationTest {
 		Assert.assertNotNull("Noversion", metadata.getEntityBinding("Noversion"));
 		Assert.assertNotNull("Withfaketimestamp", metadata.getEntityBinding("Withfaketimestamp"));
 		Assert.assertNotNull("Withversion", metadata.getEntityBinding("Withversion"));
-	}
-
-	private void executeDDL(String[] sqls) throws SQLException {
-		Configuration configuration = new Configuration();
-		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
-		builder.applySettings(configuration.getProperties());
-		ServiceRegistry serviceRegistry = builder.build();
-		ConnectionProvider connectionProvider = serviceRegistry.getService(ConnectionProvider.class);
-		Statement statement = null;
-		Connection con = null;
-		con = connectionProvider.getConnection();
-
-		statement = con.createStatement();
-
-		for (int i = 0; i < sqls.length; i++) {
-			String ddlsql = sqls[i];
-			statement.execute(ddlsql);
-			con.commit();
-		}
-
-		if (statement != null)
-			statement.close();
-		connectionProvider.closeConnection(con);
-
-	}
-
-	private String[] getCreateSQL() {
-		return new String[] {
-				"create table withVersion (first int, second int, version int, name varchar(256), primary key (first))",
-				"create table noVersion (first int, second int, name varchar(256), primary key (second))",
-				"create table withRealTimestamp (first int, second int, timestamp timestamp, name varchar(256), primary key (first))",
-				"create table withFakeTimestamp (first int, second int, timestamp int, name varchar(256), primary key (first))", };
 	}
 
 }
