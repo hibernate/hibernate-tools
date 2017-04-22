d1975432ad3f95846692da6c395381b303e8cc3d
Koen Aers <koen.aers@gmail.com>
2017-04-22 17:15:17 +0200

    HBX-1367: Create a new project 'hibernate-tools-tests-db-common' that will contain all database related tests common to all the dbms's - Implement 'org.hibernate.tool.test.db.CommonTestSuite'

---
+++ test/common/src/main/java/org/hibernate/tool/test/db/CommonTestSuite.java
@@ -0,0 +1,12 @@
+package org.hibernate.tool.test.db;
+
+import org.hibernate.cfg.JDBCMetaDataConfigurationTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	JDBCMetaDataConfigurationTest.class 
+})
+public class CommonTestSuite {}
---
+++ test/hsql/src/test/java/org/hibernate/tool/test/db/HsqlTestSuite.java
@@ -0,0 +1,9 @@
+package org.hibernate.tool.test.db;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CommonTestSuite.class })
+public class HsqlTestSuite {}
