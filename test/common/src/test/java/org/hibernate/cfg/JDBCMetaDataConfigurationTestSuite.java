1d95e32fb93a15ce328eb5c1b7f2394f7db9f556
Koen Aers <koen.aers@gmail.com>
2017-04-22 22:06:31 +0200

    HBX-1367: Create a new project 'hibernate-tools-tests-db-common' that will contain all database related tests common to all the dbms's - Remove unneeded test JDBCMetaDataConfigurationTestSuite

--- test/common/src/test/java/org/hibernate/cfg/JDBCMetaDataConfigurationTestSuite.java
+++
@@ -1,9 +0,0 @@
-package org.hibernate.cfg;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-@RunWith(Suite.class)
-@SuiteClasses({JDBCMetaDataConfigurationTest.class })
-public class JDBCMetaDataConfigurationTestSuite { }
