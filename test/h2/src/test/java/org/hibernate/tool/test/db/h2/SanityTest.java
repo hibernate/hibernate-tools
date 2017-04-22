e74617212d03929b2423e8d19ccafb3d4e4adb2b
Koen Aers <koen.aers@gmail.com>
2017-04-22 22:21:21 +0200

    HBX-1353: Implement the Hibernate Tools integration tests in the hibernate-tools-h2-tests project - Rename 'org.hibernate.too.test.H2Test' to 'org.hibernate.tools.test.db.h2.SanityTest'

--- test/h2/src/test/java/org/hibernate/tool/test/db/h2/SanityTest.java
+++ test/h2/src/test/java/org/hibernate/tool/test/db/h2/SanityTest.java
@@ -1,9 +1,9 @@
-package org.hibernate.tool.test;
+package org.hibernate.tool.test.db.h2;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-public class H2Test {
+public class SanityTest {
 	
 	@Test
 	public void testH2Present() {
