6230a19757c855acd47b820ba03ec71ebaf2035e
Koen Aers <koen.aers@gmail.com>
2017-04-22 16:24:49 +0200

    Create a new project 'hibernate-tools-tests-db-common' that will contain all database related tests common to all the dbms's

---
+++ test/common/src/test/java/org/hibernate/cfg/JDBCMetaDataConfigurationTestSuite.java
@@ -0,0 +1,9 @@
+package org.hibernate.cfg;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({JDBCMetaDataConfigurationTest.class })
+public class JDBCMetaDataConfigurationTestSuite { }
