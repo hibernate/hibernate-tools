151d763430e319f081792b257d66bdebb75ff950
Koen Aers <koen.aers@gmail.com>
2017-04-23 00:07:00 +0200

    HBX-1367: Create a new project 'hibernate-tools-tests-db-common' that will contain all database related tests common to all the dbms's - Move 'org.hibernate.tool.test.jdbc2cfg.BasicTest' from 'h2' and 'hsql' to 'common'

--- test/common/src/main/java/org/hibernate/tool/test/db/CommonTestSuite.java
+++ test/common/src/main/java/org/hibernate/tool/test/db/CommonTestSuite.java
@@ -1,12 +1,14 @@
 package org.hibernate.tool.test.db;
 
 import org.hibernate.cfg.JDBCMetaDataConfigurationTest;
+import org.hibernate.tool.test.jdbc2cfg.BasicTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
+	BasicTest.class,
 	JDBCMetaDataConfigurationTest.class 
 })
 public class CommonTestSuite {}
