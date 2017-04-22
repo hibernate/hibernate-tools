c039ab38a69dd23b33c2431c675d73359f14e373
Koen Aers <koen.aers@gmail.com>
2017-04-22 18:24:48 +0200

    HBX-1367: Create a new project 'hibernate-tools-tests-db-common' that will contain all database related tests common to all the dbms's - Rename H2TestSuite and HsqlTestSuite to TestSuite and put them in appropriate package

--- test/h2/src/test/java/org/hibernate/tool/test/db/h2/TestSuite.java
+++ test/h2/src/test/java/org/hibernate/tool/test/db/h2/TestSuite.java
@@ -1,9 +1,10 @@
-package org.hibernate.tool.test.db;
+package org.hibernate.tool.test.db.h2;
 
+import org.hibernate.tool.test.db.CommonTestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ CommonTestSuite.class })
-public class H2TestSuite {}
+public class TestSuite {}
--- test/hsql/src/test/java/org/hibernate/tool/test/db/hsql/TestSuite.java
+++ test/hsql/src/test/java/org/hibernate/tool/test/db/hsql/TestSuite.java
@@ -1,9 +1,10 @@
-package org.hibernate.tool.test.db;
+package org.hibernate.tool.test.db.hsql;
 
+import org.hibernate.tool.test.db.CommonTestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ CommonTestSuite.class })
-public class HsqlTestSuite {}
+public class TestSuite {}
