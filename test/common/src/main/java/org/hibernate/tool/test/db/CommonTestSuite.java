f35a0eb5b844bd310a8d001e65daf6b56e92d04b
Koen Aers <koen.aers@gmail.com>
2017-04-26 22:29:45 +0200

    HBX-1367: Create a new project 'hibernate-tools-tests-db-common' that will contain all database related tests common to all the dbms's - Move 'org.hibernate.tool.test.jdbc2cfg.ForeignKeyTest' from 'h2' and 'hsql' to 'common'

--- test/common/src/main/java/org/hibernate/tool/test/db/CommonTestSuite.java
+++ test/common/src/main/java/org/hibernate/tool/test/db/CommonTestSuite.java
@@ -5,6 +5,7 @@ import org.hibernate.tool.test.jdbc2cfg.AutoQuoteTest;
 import org.hibernate.tool.test.jdbc2cfg.BasicMultiSchemaTest;
 import org.hibernate.tool.test.jdbc2cfg.BasicTest;
 import org.hibernate.tool.test.jdbc2cfg.CompositeIdTest;
+import org.hibernate.tool.test.jdbc2cfg.ForeignKeysTest;
 import org.hibernate.tool.test.jdbc2cfg.KeyPropertyCompositeIdTest;
 import org.hibernate.tool.test.jdbc2cfg.VersioningTest;
 import org.junit.runner.RunWith;
@@ -17,6 +18,7 @@ import org.junit.runners.Suite.SuiteClasses;
 	BasicTest.class,
 	BasicMultiSchemaTest.class,
 	CompositeIdTest.class,
+	ForeignKeysTest.class,
 	JDBCMetaDataConfigurationTest.class,
 	KeyPropertyCompositeIdTest.class,
 	VersioningTest.class
