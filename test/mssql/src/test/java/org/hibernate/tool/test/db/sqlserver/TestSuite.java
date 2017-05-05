f0cc5219267fb1e82487e72046964cd3cb0dce0e
Koen Aers <koen.aers@gmail.com>
2017-05-05 14:11:09 +0200

    HBX-1372: Implement the Hibernate Tools integration tests in the hibernate-tools-mssql-tests project - Add 'org.hibernate.tool.test.db.sqlserver.TestSuite' and remove 'org.hibernate.tool.test.db.sqlserver.BasicTest' and 'org.hibernate.tool.test.db.sqlserver.SanityTest'

---
+++ test/mssql/src/test/java/org/hibernate/tool/test/db/sqlserver/TestSuite.java
@@ -0,0 +1,9 @@
+package org.hibernate.tool.test.db.sqlserver;
+
+import org.hibernate.tools.test.util.DbSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(DbSuite.class)
+@SuiteClasses({})
+public class TestSuite {}
