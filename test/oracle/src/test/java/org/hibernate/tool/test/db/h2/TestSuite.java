7b6418b7ef5734960932f6e98c73a60eff4b02ef
Koen Aers <koen.aers@gmail.com>
2017-05-11 14:20:16 +0200

    HBX-1382: Implement the Hibernate Tools integration tests in the hibernate-tools-tests-oracle project - Add 'org.hibernate.cfg.JDBCMetaDataConfigurationTest'

---
+++ test/oracle/src/test/java/org/hibernate/tool/test/db/h2/TestSuite.java
@@ -0,0 +1,10 @@
+package org.hibernate.tool.test.db.h2;
+
+import org.hibernate.cfg.JDBCMetaDataConfigurationTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ JDBCMetaDataConfigurationTest.class })
+public class TestSuite {}
