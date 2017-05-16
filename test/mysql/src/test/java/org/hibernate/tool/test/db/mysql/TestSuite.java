c5ef7ea3aa7f7a9d8539d45b8687f5d6d7be7561
Koen Aers <koen.aers@gmail.com>
2017-05-16 08:34:23 +0200

    HBX-1383: Implement the Hibernate Tools integration tests in the hibernate-tools-tests-mysql project - Add 'org.hibernate.tool.test.jdbc2cfg.BasicMultiSchemaTest'

--- test/mysql/src/test/java/org/hibernate/tool/test/db/mysql/TestSuite.java
+++ test/mysql/src/test/java/org/hibernate/tool/test/db/mysql/TestSuite.java
@@ -2,6 +2,7 @@ package org.hibernate.tool.test.db.mysql;
 
 import org.hibernate.cfg.JDBCMetaDataConfigurationTest;
 import org.hibernate.tool.test.jdbc2cfg.AutoQuoteTest;
+import org.hibernate.tool.test.jdbc2cfg.BasicMultiSchemaTest;
 import org.hibernate.tools.test.util.DbSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
@@ -9,6 +10,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(DbSuite.class)
 @SuiteClasses({ 
 	AutoQuoteTest.class,
+	BasicMultiSchemaTest.class,
 	JDBCMetaDataConfigurationTest.class 
 })
 public class TestSuite {}
