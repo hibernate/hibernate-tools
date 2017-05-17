96e965178ebdadbd17e88c67e2ac07ae9a3e0b9e
Koen Aers <koen.aers@gmail.com>
2017-05-17 21:59:31 +0200

    HBX-1383: Implement the Hibernate Tools integration tests in the hibernate-tools-tests-mysql project - Add 'org.hibernate.tool.test.jdbc2cfg.CompositeIdTest'

--- test/mysql/src/test/java/org/hibernate/tool/test/db/mysql/TestSuite.java
+++ test/mysql/src/test/java/org/hibernate/tool/test/db/mysql/TestSuite.java
@@ -4,6 +4,7 @@ import org.hibernate.cfg.JDBCMetaDataConfigurationTest;
 import org.hibernate.tool.test.jdbc2cfg.AutoQuoteTest;
 import org.hibernate.tool.test.jdbc2cfg.BasicMultiSchemaTest;
 import org.hibernate.tool.test.jdbc2cfg.BasicTest;
+import org.hibernate.tool.test.jdbc2cfg.CompositeIdTest;
 import org.hibernate.tools.test.util.DbSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
@@ -13,6 +14,7 @@ import org.junit.runners.Suite.SuiteClasses;
 	AutoQuoteTest.class,
 	BasicMultiSchemaTest.class,
 	BasicTest.class,
+	CompositeIdTest.class,
 	JDBCMetaDataConfigurationTest.class 
 })
 public class TestSuite {}
