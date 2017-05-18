3485b564699adaf5a38ac26dc7384b5e301fda29
Koen Aers <koen.aers@gmail.com>
2017-05-18 18:05:28 +0200

    HBX-1383: Implement the Hibernate Tools integration tests in the hibernate-tools-tests-mysql project - Add 'org.hibernate.tool.test.jdbc2cfg.IndexTest'

--- test/mysql/src/test/java/org/hibernate/tool/test/db/mysql/TestSuite.java
+++ test/mysql/src/test/java/org/hibernate/tool/test/db/mysql/TestSuite.java
@@ -6,6 +6,7 @@ import org.hibernate.tool.test.jdbc2cfg.BasicMultiSchemaTest;
 import org.hibernate.tool.test.jdbc2cfg.BasicTest;
 import org.hibernate.tool.test.jdbc2cfg.CompositeIdTest;
 import org.hibernate.tool.test.jdbc2cfg.ForeignKeysTest;
+import org.hibernate.tool.test.jdbc2cfg.IndexTest;
 import org.hibernate.tools.test.util.DbSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
@@ -17,6 +18,7 @@ import org.junit.runners.Suite.SuiteClasses;
 	BasicTest.class,
 	CompositeIdTest.class,
 	ForeignKeysTest.class,
+	IndexTest.class,
 	JDBCMetaDataConfigurationTest.class 
 })
 public class TestSuite {}
