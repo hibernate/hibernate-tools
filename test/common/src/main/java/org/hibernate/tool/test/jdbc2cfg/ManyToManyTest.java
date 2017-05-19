20f5181b49b9bfdb80fadb024ff6dd004f6cb3fb
Koen Aers <koen.aers@gmail.com>
2017-05-19 18:43:50 +0200

    HBX-1367: Create a new project 'hibernate-tools-tests-db-common' that will contain all database related tests common to all the dbms's - Use capitals in the SQL scripts of 'org.hibernate.tool.test.jdbc2cfg.ManyToManyTest'

--- test/common/src/main/java/org/hibernate/tool/test/jdbc2cfg/ManyToManyTest.java
+++ test/common/src/main/java/org/hibernate/tool/test/jdbc2cfg/ManyToManyTest.java
@@ -31,26 +31,26 @@ import org.junit.rules.TemporaryFolder;
 public class ManyToManyTest {
 	
 	static final String[] CREATE_SQL = new String[] {
-			"create table PROJECT ( project_id integer not null, name varchar(50), primary key (project_id) )",
-			"create table EMPLOYEE ( id integer not null, name varchar(50), manager_id integer, primary key (id), constraint employee_manager foreign key (manager_id) references EMPLOYEE(id))",
-			"create table WORKS_ON ( project_id integer not null, employee_id integer not null, primary key (project_id, employee_id), constraint workson_employee foreign key (employee_id) references EMPLOYEE(id), foreign key (project_id) references PROJECT(project_id) )",
-			"create table WORKS_ON_CONTEXT ( project_id integer not null, employee_id integer not null, created_by integer, primary key (project_id, employee_id), constraint workson_ctx_employee foreign key (employee_id) references EMPLOYEE, foreign key (project_id) references PROJECT(project_id), foreign key (created_by) references EMPLOYEE(id) )",
+			"CREATE TABLE PROJECT (PROJECT_ID INTEGER NOT NULL, NAME VARCHAR(50), PRIMARY KEY (PROJECT_ID) )",
+			"CREATE TABLE EMPLOYEE (ID INTEGER NOT NULL, NAME VARCHAR(50), MANAGER_ID INTEGER, PRIMARY KEY (ID), CONSTRAINT EMPLOYEE_MANAGER FOREIGN KEY (MANAGER_ID) REFERENCES EMPLOYEE(ID))",
+			"CREATE TABLE WORKS_ON (PROJECT_ID INTEGER NOT NULL, EMPLOYEE_ID INTEGER NOT NULL, PRIMARY KEY (PROJECT_ID, EMPLOYEE_ID), CONSTRAINT WORKSON_EMPLOYEE FOREIGN KEY (EMPLOYEE_ID) REFERENCES EMPLOYEE(ID), FOREIGN KEY (PROJECT_ID) REFERENCES PROJECT(PROJECT_ID) )",
+			"CREATE TABLE WORKS_ON_CONTEXT (PROJECT_ID INTEGER NOT NULL, EMPLOYEE_ID INTEGER NOT NULL, CREATED_BY INTEGER, PRIMARY KEY (PROJECT_ID, EMPLOYEE_ID), CONSTRAINT WORKSON_CTX_EMPLOYEE FOREIGN KEY (EMPLOYEE_ID) REFERENCES EMPLOYEE, FOREIGN KEY (PROJECT_ID) REFERENCES PROJECT(PROJECT_ID), FOREIGN KEY (CREATED_BY) REFERENCES EMPLOYEE(ID) )",
 			//"alter  table PROJECT add constraint project_manager foreign key (team_lead) references EMPLOYEE"
 			// nonmiddle left and right are used to test a false association table isn't detected.
-			"create table LEFT_TABLE ( id integer not null, primary key (id) )",
-			"create table RIGHT_TABLE ( id integer not null, primary key (id) )",
-			"create table NON_MIDDLE ( left_id integer not null, right_id integer not null, primary key (left_id), constraint FK_MIDDLE_LEFT foreign key (left_id) references LEFT_TABLE(id), constraint FK_MIDDLE_RIGHT foreign key (right_id) references RIGHT_TABLE(id))",
+			"CREATE TABLE LEFT_TABLE ( id INTEGER NOT NULL, PRIMARY KEY (ID) )",
+			"CREATE TABLE RIGHT_TABLE ( id INTEGER NOT NULL, PRIMARY KEY (ID) )",
+			"CREATE TABLE NON_MIDDLE ( left_id INTEGER NOT NULL, RIGHT_ID INTEGER NOT NULL, PRIMARY KEY (LEFT_ID), CONSTRAINT FK_MIDDLE_LEFT FOREIGN KEY (LEFT_ID) REFERENCES LEFT_TABLE(ID), CONSTRAINT FK_MIDDLE_RIGHT FOREIGN KEY (RIGHT_ID) REFERENCES RIGHT_TABLE(ID))",
 		};
 
 	static final String[] DROP_SQL = new String[] {
 				//"alter table PROJECT drop constraint project_manager",
-				"drop table WORKS_ON_CONTEXT",
-				"drop table WORKS_ON",
-				"drop table EMPLOYEE",
-				"drop table PROJECT",
-				"drop table NON_MIDDLE",
-				"drop table LEFT_TABLE",
-				"drop table RIGHT_TABLE",
+				"DROP TABLE WORKS_ON_CONTEXT",
+				"DROP TABLE WORKS_ON",
+				"DROP TABLE EMPLOYEE",
+				"DROP TABLE PROJECT",
+				"DROP TABLE NON_MIDDLE",
+				"DROP TABLE LEFT_TABLE",
+				"DROP TABLE RIGHT_TABLE",
 			};
 
 	private JDBCMetaDataConfiguration jmdcfg = null;
