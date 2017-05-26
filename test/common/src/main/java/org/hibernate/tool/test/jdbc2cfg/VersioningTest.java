cfb9db0dab7bdc6f279da0ee130601985afaee7f
Koen Aers <koen.aers@gmail.com>
2017-05-26 15:52:20 +0200

    HBX-1367: Create a new project 'hibernate-tools-tests-db-common' that will contain all database related tests common to all the dbms's - Use capitals in the SQL scripts of 'org.hibernate.tool.test.jdbc2cfg.VersioningTest'

--- test/common/src/main/java/org/hibernate/tool/test/jdbc2cfg/VersioningTest.java
+++ test/common/src/main/java/org/hibernate/tool/test/jdbc2cfg/VersioningTest.java
@@ -14,7 +14,9 @@ import org.hibernate.mapping.Property;
 import org.hibernate.tool.hbm2x.Exporter;
 import org.hibernate.tool.hbm2x.HibernateMappingExporter;
 import org.hibernate.tools.test.util.JdbcUtil;
+import org.hibernate.type.BigDecimalType;
 import org.hibernate.type.IntegerType;
+import org.hibernate.type.RowVersionType;
 import org.hibernate.type.TimestampType;
 import org.junit.After;
 import org.junit.Assert;
@@ -31,18 +33,18 @@ import org.junit.rules.TemporaryFolder;
  */
 public class VersioningTest {
 	
-	private static final String[] CREATE_SQL = new String[] {
-				"create table withVersion (first int, second int, version int, name varchar(256), primary key (first))",
-				"create table noVersion (first int, second int, name varchar(256), primary key (second))",
-				"create table withRealTimestamp (first int, second int, timestamp timestamp, name varchar(256), primary key (first))",
-				"create table withFakeTimestamp (first int, second int, timestamp int, name varchar(256), primary key (first))",
+	static final String[] CREATE_SQL = new String[] {
+				"CREATE TABLE WITH_VERSION (FIRST INT, SECOND INT, VERSION INT, NAME VARCHAR(256), PRIMARY KEY (FIRST))",
+				"CREATE TABLE NO_VERSION (FIRST INT, SECOND INT, NAME VARCHAR(256), PRIMARY KEY (SECOND))",
+				"CREATE TABLE WITH_REAL_TIMESTAMP (FIRST INT, SECOND INT, TIMESTAMP TIMESTAMP, NAME VARCHAR(256), PRIMARY KEY (FIRST))",
+				"CREATE TABLE WITH_FAKE_TIMESTAMP (FIRST INT, SECOND INT, TIMESTAMP INT, NAME VARCHAR(256), PRIMARY KEY (FIRST))",
 		};
 
-	private static final String[] DROP_SQL = new String[]  {
-				"drop table withVersion",
-				"drop table noVersion",
-				"drop table withRealTimestamp",
-				"drop table withFakeTimestamp"
+	static final String[] DROP_SQL = new String[]  {
+				"DROP TABLE WITH_VERSION",
+				"DROP TABLE NO_VERSION",
+				"DROP TABLE WITH_REAL_TIMESTAMP",
+				"DROP TABLE WITH_FAKE_TIMESTAMP"
 		};
 
 	private JDBCMetaDataConfiguration jmdcfg = null;
@@ -52,25 +54,23 @@ public class VersioningTest {
 
 	@Before
 	public void setUp() {
-		JdbcUtil.establishJdbcConnection(this);
-		JdbcUtil.executeSql(this, CREATE_SQL);
+		JdbcUtil.createDatabase(this);;
 		jmdcfg = new JDBCMetaDataConfiguration();
 		jmdcfg.readFromJDBC();
 	}
 
 	@After
 	public void tearDown() {
-		JdbcUtil.executeSql(this, DROP_SQL);
-		JdbcUtil.releaseJdbcConnection(this);
+		JdbcUtil.dropDatabase(this);;
 	}
 
 	@Test
 	public void testVersion() {		
-		PersistentClass cl = jmdcfg.getMetadata().getEntityBinding("Withversion");		
+		PersistentClass cl = jmdcfg.getMetadata().getEntityBinding("WithVersion");		
 		Property version = cl.getVersion();
 		Assert.assertNotNull(version);
 		Assert.assertEquals("version", version.getName());		
-		cl = jmdcfg.getMetadata().getEntityBinding("Noversion");
+		cl = jmdcfg.getMetadata().getEntityBinding("NoVersion");
 		Assert.assertNotNull(cl);
 		version = cl.getVersion();
 		Assert.assertNull(version);		
@@ -87,24 +87,28 @@ public class VersioningTest {
 		derived.addFile(new File(testFolder, "Withrealtimestamp.hbm.xml") );
 		derived.addFile(new File(testFolder, "Withfaketimestamp.hbm.xml") );		
 		Metadata metadata = derived.buildMetadata();		
-		PersistentClass cl = metadata.getEntityBinding( "Withversion" );				
+		PersistentClass cl = metadata.getEntityBinding( "WithVersion" );				
 		Property version = cl.getVersion();
 		Assert.assertNotNull(version);
 		Assert.assertEquals("version", version.getName());	
-		cl = metadata.getEntityBinding( "Noversion" );
+		cl = metadata.getEntityBinding( "NoVersion" );
 		Assert.assertNotNull(cl);
 		version = cl.getVersion();
 		Assert.assertNull(version);
-		cl = metadata.getEntityBinding( "Withrealtimestamp" );
+		cl = metadata.getEntityBinding( "WithRealTimestamp" );
 		Assert.assertNotNull(cl);
 		version = cl.getVersion();
 		Assert.assertNotNull(version);
-		Assert.assertTrue(version.getType() instanceof TimestampType);	
-		cl = metadata.getEntityBinding( "Withfaketimestamp" );
+		Assert.assertTrue(
+				version.getType() instanceof TimestampType || 
+				version.getType() instanceof RowVersionType);	// on MS SQL Server
+		cl = metadata.getEntityBinding( "WithFakeTimestamp" );
 		Assert.assertNotNull(cl);
 		version = cl.getVersion();
 		Assert.assertNotNull(version);
-		Assert.assertTrue(version.getType() instanceof IntegerType);
+		Assert.assertTrue(
+				version.getType() instanceof IntegerType ||
+				version.getType() instanceof BigDecimalType); // on Oracle
 	}
     
 }
