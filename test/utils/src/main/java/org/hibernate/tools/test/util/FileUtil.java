54ef6779d8d5e595fbf9f2bbead9d711c488d97e
Koen Aers <koen.aers@gmail.com>
2017-06-06 13:13:20 +0200

    HBX-1398: Create FileUtil#generateNoopComparator(File)

--- test/utils/src/main/java/org/hibernate/tools/test/util/FileUtil.java
+++ test/utils/src/main/java/org/hibernate/tools/test/util/FileUtil.java
@@ -3,7 +3,9 @@ package org.hibernate.tools.test.util;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class FileUtil {
 
@@ -22,6 +24,22 @@ public class FileUtil {
 			throw new RuntimeException("trouble with searching in " + file,e);
 	    }
 		return str;
+	}
+	
+	static public void generateNoopComparator(File sourceFolder) throws IOException {
+		File file = new File(sourceFolder.getAbsolutePath() + "/comparator/NoopComparator.java");
+		file.getParentFile().mkdirs();
+		FileWriter fileWriter = new FileWriter(file);
+		PrintWriter pw = new PrintWriter(fileWriter);
+		pw.println("package comparator;                                ");
+		pw.println("import java.util.Comparator;                       ");
+		pw.println("public class NoopComparator implements Comparator {"); 
+		pw.println("	public int compare(Object o1, Object o2) {     ");
+		pw.println("		return 0;                                  ");
+		pw.println("	}                                              ");
+		pw.println("}                                                  ");
+		pw.flush();
+		pw.close();
 	}
 
 }
--- test/utils/src/test/java/org/hibernate/tools/test/util/FileUtilTest.java
+++ test/utils/src/test/java/org/hibernate/tools/test/util/FileUtilTest.java
@@ -3,9 +3,14 @@ package org.hibernate.tools.test.util;
 import java.io.File;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 public class FileUtilTest {
+	
+	@Rule
+	public TemporaryFolder temporaryFolder = new TemporaryFolder();
 	
 	@Test
 	public void testFindFirstString() throws Exception {
@@ -15,6 +20,18 @@ public class FileUtilTest {
 		Assert.assertNull(FileUtil.findFirstString("foobar", file));
 		Assert.assertEquals("This is a foo test", FileUtil.findFirstString("test", file));
 		Assert.assertEquals("And a third line...", FileUtil.findFirstString("third", file));
+	}
+	
+	@Test
+	public void testGenerateNoopComparator() throws Exception {
+		FileUtil.generateNoopComparator(temporaryFolder.getRoot());
+		File pkg = new File(temporaryFolder.getRoot(), "comparator");
+		Assert.assertTrue(pkg.isDirectory() && pkg.exists());
+		File comp = new File(pkg, "NoopComparator.java");
+		Assert.assertTrue(comp.isFile() && comp.exists());
+		Assert.assertEquals(
+				"public class NoopComparator implements Comparator {", 
+				FileUtil.findFirstString("NoopComparator", comp));
 	}
 
 }
