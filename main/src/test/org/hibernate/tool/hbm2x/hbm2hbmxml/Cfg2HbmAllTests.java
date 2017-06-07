409efc4d2797c4091d204cb4380b1c561660feaf
Koen Aers <koen.aers@gmail.com>
2017-06-07 20:26:59 +0200

    HBX-1367: Create a new project 'hibernate-tools-tests-db-common' that will contain all database related tests common to all the dbms's - Move 'org.hibernate.tool.hbm2x.hbm2hbmxml.AbstractTest' from 'hibernate-tools' into 'common'

--- main/src/test/org/hibernate/tool/hbm2x/hbm2hbmxml/Cfg2HbmAllTests.java
+++ main/src/test/org/hibernate/tool/hbm2x/hbm2hbmxml/Cfg2HbmAllTests.java
@@ -8,7 +8,6 @@ public class Cfg2HbmAllTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for org.hibernate.tool.cfg2hbm");
 		//$JUnit-BEGIN$
-		suite.addTest(AbstractTest.suite());
 		//$JUnit-END$
 		return suite;
 	}
---
+++ test/nodb/src/test/java/org/hibernate/tool/hbm2x/hbm2hbmxml/AbstractTest/TestCase.java
@@ -0,0 +1,113 @@
+/*
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.hibernate.tool.hbm2x.hbm2hbmxml.AbstractTest;
+
+import java.io.File;
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.XPath;
+import org.dom4j.io.SAXReader;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.hbm2x.Exporter;
+import org.hibernate.tool.hbm2x.HibernateMappingExporter;
+import org.hibernate.tool.util.MetadataHelper;
+import org.hibernate.tools.test.util.HibernateUtil;
+import org.hibernate.tools.test.util.JUnitUtil;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+/**
+ * @author Dmitry Geraskov
+ * @author koen
+ */
+public class TestCase {
+
+	private static final String[] HBM_XML_FILES = new String[] {
+			"Car.hbm.xml"
+	};
+	
+	@Rule
+	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+	
+	private File outputDir = null;
+	private Exporter hbmexporter = null;
+
+	@Before
+	public void setUp() throws Exception {
+		Configuration configuration = 
+				HibernateUtil.initializeConfiguration(this, HBM_XML_FILES);
+		outputDir = temporaryFolder.getRoot();
+		hbmexporter = new HibernateMappingExporter(
+				configuration, 
+				outputDir);
+		hbmexporter.start();
+	}
+
+	@Test
+	public void testAllFilesExistence() {
+		JUnitUtil.assertIsNonEmptyFile(new File(
+				outputDir,  
+				"org/hibernate/tool/hbm2x/hbm2hbmxml/AbstractTest/Car.hbm.xml") );
+		JUnitUtil.assertIsNonEmptyFile(new File(
+				outputDir,  
+				"org/hibernate/tool/hbm2x/hbm2hbmxml/AbstractTest/CarPart.hbm.xml") );
+	}
+	
+	@Test
+	public void testAbstractPresent() {
+		File outputXml = new File(
+				outputDir,
+				"/org/hibernate/tool/hbm2x/hbm2hbmxml/AbstractTest/CarPart.hbm.xml");
+		JUnitUtil.assertIsNonEmptyFile(outputXml);
+		SAXReader xmlReader =  new SAXReader();
+		Document document;
+		try {
+			document = xmlReader.read(outputXml);
+			XPath xpath = DocumentHelper.createXPath("//hibernate-mapping/class");
+			List<?> list = xpath.selectNodes(document);
+			Assert.assertEquals("Expected to get one class element", 1, list.size());
+			Element node = (Element) list.get(0);
+			Assert.assertNotNull("Abstract attrinute was not exported.", node.attribute( "abstract" ));
+			Assert.assertEquals(node.attribute( "abstract" ).getText(),"true");
+		} catch (DocumentException e) {
+			Assert.fail("Can't parse file " + outputXml.getAbsolutePath());
+		}
+	}
+	
+	public void testReadable() {
+        Configuration cfg = new Configuration();
+        cfg.setProperty(
+        		AvailableSettings.DIALECT, 
+        		HibernateUtil.Dialect.class.getName());
+        cfg.addFile(new File(
+        		outputDir, 
+        		"org/hibernate/tool/hbm2x/hbm2hbmxml/AbstractTest/Car.hbm.xml"));
+        cfg.addFile(new File(
+        		outputDir, 
+        		"org/hibernate/tool/hbm2x/hbm2hbmxml/AbstractTest/CarPart.hbm.xml"));
+        Assert.assertNotNull(MetadataHelper.getMetadata(cfg));
+    }
+
+}
