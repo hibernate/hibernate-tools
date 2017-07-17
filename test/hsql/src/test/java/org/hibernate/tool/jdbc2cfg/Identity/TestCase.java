a02530c169d95ddc618f2833f0240d1c7cd1c86f
Koen Aers <koen.aers@gmail.com>
2017-07-17 12:35:03 +0200

    HBX-1352: Move the Hibernate Tools integration tests to the hibernate-tools-hsql-tests project - Move 'org.hibernate.tool.test.jdbc2cfg.identity.HSQLIdentityTest'

---
+++ test/hsql/src/test/java/org/hibernate/tool/jdbc2cfg/Identity/TestCase.java
@@ -0,0 +1,63 @@
+/*
+ * Created on 2004-11-23
+ *
+ */
+package org.hibernate.tool.jdbc2cfg.Identity;
+
+import java.sql.SQLException;
+
+import org.hibernate.cfg.JDBCMetaDataConfiguration;
+import org.hibernate.mapping.PersistentClass;
+import org.hibernate.mapping.SimpleValue;
+import org.hibernate.tools.test.util.JdbcUtil;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author max
+ * @author koen
+ */
+public class TestCase {
+
+	private JDBCMetaDataConfiguration jmdcfg = null;
+
+	@Before
+	public void setUp() {
+		JdbcUtil.createDatabase(this);
+		jmdcfg = new JDBCMetaDataConfiguration();
+		jmdcfg.readFromJDBC();
+	}
+
+	@After
+	public void tearDown() {
+		JdbcUtil.dropDatabase(this);
+	}
+
+	@Test
+	public void testIdentity() throws SQLException {
+
+		PersistentClass classMapping = jmdcfg
+				.getMetadata()
+				.getEntityBinding("Autoinc");
+		Assert.assertNotNull(classMapping);
+		Assert.assertEquals(
+				"identity", 
+				((SimpleValue)classMapping
+						.getIdentifierProperty()
+						.getValue())
+					.getIdentifierGeneratorStrategy());
+		
+		classMapping = jmdcfg
+				.getMetadata()
+				.getEntityBinding("Noautoinc");
+		Assert.assertEquals(
+				"assigned", 
+				((SimpleValue)classMapping
+						.getIdentifierProperty()
+						.getValue())
+					.getIdentifierGeneratorStrategy());
+	}
+
+}
