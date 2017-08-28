0cc5678e203b1e23d9e2ae50a32894871a4faae9
Koen Aers <koen.aers@gmail.com>
2017-08-28 14:37:19 +0200

    HBX-1415: Phase out the use of the deprecated Configuration class - Use HibernateUtil#initializeMetadata(...) in org.hibernate.tool.hbmlint.HbmLintTest

--- test/common/src/main/java/org/hibernate/tool/hbmlint/HbmLintTest/TestCase.java
+++ test/common/src/main/java/org/hibernate/tool/hbmlint/HbmLintTest/TestCase.java
@@ -1,8 +1,6 @@
 package org.hibernate.tool.hbmlint.HbmLintTest;
 
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.boot.Metadata;
 import org.hibernate.tool.hbm2x.HbmLintExporter;
 import org.hibernate.tool.hbmlint.Detector;
 import org.hibernate.tool.hbmlint.HbmLint;
@@ -10,7 +8,6 @@ import org.hibernate.tool.hbmlint.detector.BadCachingDetector;
 import org.hibernate.tool.hbmlint.detector.InstrumentationDetector;
 import org.hibernate.tool.hbmlint.detector.ShadowedIdentifierDetector;
 import org.hibernate.tools.test.util.HibernateUtil;
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -28,60 +25,40 @@ public class TestCase {
 	@Rule
 	public TemporaryFolder temporaryFolder = new TemporaryFolder();
 	
+	private Metadata metadata = null;
+	
 	@Before
 	public void setUp() {
-		System.setProperty("hibernate.dialect", HibernateUtil.Dialect.class.getName());
+		metadata = HibernateUtil.initializeMetadata(this, HBM_XML_FILES);
 	}
 	
-	@After
-	public void tearDown() {
-		System.getProperties().remove("hibernate.dialect");
-	}
-
 	@Test
 	public void testExporter() {	
-		MetadataSources metadataSources = initializeMetadataSources();
 		HbmLintExporter exporter = new HbmLintExporter();		
-		exporter.setMetadata(metadataSources.buildMetadata());
+		exporter.setMetadata(metadata);
 		exporter.setOutputDirectory(temporaryFolder.getRoot());
 		exporter.start();
 	}
 	
 	@Test
 	public void testValidateCache() {	
-		MetadataSources metadataSources = initializeMetadataSources();
 		HbmLint analyzer = new HbmLint(new Detector[] { new BadCachingDetector() });		
-		analyzer.analyze(metadataSources.buildMetadata());
+		analyzer.analyze(metadata);
 		Assert.assertEquals(1,analyzer.getResults().size());		
 	}
 
 	@Test
 	public void testValidateIdentifier() {		
-		MetadataSources metadataSources = initializeMetadataSources();
 		HbmLint analyzer = new HbmLint(new Detector[] { new ShadowedIdentifierDetector() });		
-		analyzer.analyze(metadataSources.buildMetadata());
+		analyzer.analyze(metadata);
 		Assert.assertEquals(1,analyzer.getResults().size());
 	}
 	
 	@Test
 	public void testBytecodeRestrictions() {		
-		MetadataSources metadataSources = initializeMetadataSources();
 		HbmLint analyzer = new HbmLint(new Detector[] { new InstrumentationDetector() });		
-		analyzer.analyze(metadataSources.buildMetadata());
+		analyzer.analyze(metadata);
 		Assert.assertEquals(analyzer.getResults().toString(), 2,analyzer.getResults().size());
 	}
-	
-	private MetadataSources initializeMetadataSources() {
-		StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder();
-		ssrb.applySetting(AvailableSettings.DIALECT, HibernateUtil.Dialect.class.getName());
-		MetadataSources result = new MetadataSources(ssrb.build());
-		String resourcesLocation = '/' + getClass().getPackage().getName().replace(".", "/") + '/';
-		for (int i = 0; i < HBM_XML_FILES.length; i++) {
-			result.addResource(resourcesLocation + HBM_XML_FILES[i]);
-		}
-		return result;
-	}
-	 
-
 	
 }
