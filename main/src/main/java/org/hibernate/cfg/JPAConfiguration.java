6691cbc5fbac6d0f124049ee1ea3f29c4774ff4c
Koen Aers <koen.aers@gmail.com>
2017-08-31 14:33:02 +0200

    HBX-1431: Add a JPAConfiguration(String,Properties) constructor to replace JPAConfiguration(EntityManagerFactoryBuilderImpl) - Implement the new constructor

--- main/src/main/java/org/hibernate/cfg/JPAConfiguration.java
+++ main/src/main/java/org/hibernate/cfg/JPAConfiguration.java
@@ -1,16 +1,24 @@
 package org.hibernate.cfg;
 
+import java.util.Map;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
 
 import org.hibernate.boot.Metadata;
+import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.hibernate.jpa.boot.internal.EntityManagerFactoryBuilderImpl;
 
 public class JPAConfiguration extends Configuration {
 	
 	private Properties properties = new Properties();
 	private Metadata metadata = null;
+	
+	public JPAConfiguration(
+			final String persistenceUnit, 
+			final Properties properties) {
+		this(createEntityManagerFactoryBuilder(persistenceUnit, properties));
+	}
 	
 	public JPAConfiguration(EntityManagerFactoryBuilderImpl entityManagerFactoryBuilder) {
 		EntityManagerFactory entityManagerFactory = 
@@ -25,6 +33,24 @@ public class JPAConfiguration extends Configuration {
 	
 	public Properties getProperties() {
 		return properties;
+	}
+	
+	private static class PersistenceProvider extends HibernatePersistenceProvider {
+		public EntityManagerFactoryBuilderImpl getEntityManagerFactoryBuilder(
+				String persistenceUnit, 
+				Map<Object, Object> properties) {
+			return (EntityManagerFactoryBuilderImpl)getEntityManagerFactoryBuilderOrNull(
+					persistenceUnit, 
+					properties);
+		}
+	}
+
+	private static EntityManagerFactoryBuilderImpl createEntityManagerFactoryBuilder(
+			final String persistenceUnit, 
+			final Map<Object, Object> properties) {
+		return new PersistenceProvider().getEntityManagerFactoryBuilder(
+				persistenceUnit, 
+				properties);
 	}
 	
 }
