7a2fcc915d7f653e9abf4e56051a873a542c98c6
Koen Aers <koen.aers@gmail.com>
2017-09-01 17:44:51 +0200

    HBX-1433: Create class org.hibernate.tool.metadata.JdbcMetadataSources as a future replacement of JDBCMetaDataConfiguration - Initial class creation

---
+++ main/src/main/java/org/hibernate/tool/metadata/JdbcMetadataSources.java
@@ -0,0 +1,9 @@
+package org.hibernate.tool.metadata;
+
+import org.hibernate.cfg.JDBCMetaDataConfiguration;
+
+public class JdbcMetadataSources 
+	extends JDBCMetaDataConfiguration 
+	implements MetadataSources {
+
+}
