927bf81b49d3e0d07871edd62cca5c7516580ae6
Koen Aers <koen.aers@gmail.com>
2017-09-01 20:25:44 +0200

    HBX-1432: Add Factory class org.hibernate.tool.metadata.MetadataSourcesFactory - Add method MetadataSourcesFactory#createJpaSources(String,Properties)

--- main/src/main/java/org/hibernate/tool/metadata/MetadataSourcesFactory.java
+++ main/src/main/java/org/hibernate/tool/metadata/MetadataSourcesFactory.java
@@ -1,9 +1,15 @@
 package org.hibernate.tool.metadata;
 
+import java.util.Properties;
+
 public class MetadataSourcesFactory {
 	
 	public MetadataSources createJdbcSources() {
 		return new JdbcMetadataSources();
+	}
+	
+	public MetadataSources createJpaSources(String persistenceUnit, Properties properties) {
+		return new JpaMetadataSources(persistenceUnit, properties);
 	}
 	
 }
