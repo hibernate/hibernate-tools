b7664d9ad2c5ee2d9cd1fb08f149833aad9abe8d
Koen Aers <koen.aers@gmail.com>
2017-09-04 00:30:37 +0200

    HBX-1433: Create class org.hibernate.tool.metadata.JdbcMetadataSources as a future replacement of JDBCMetaDataConfiguration - Replace JDBCMetaDataConfiguration by MetadataSources in org.hibernate.tool.hdbc2cfg.Views.TestCase

--- test/oracle/src/main/java/org/hibernate/tool/jdbc2cfg/Views/TestCase.java
+++ test/oracle/src/main/java/org/hibernate/tool/jdbc2cfg/Views/TestCase.java
@@ -6,9 +6,12 @@ package org.hibernate.tool.jdbc2cfg.Views;
 
 import java.sql.SQLException;
 
+import org.hibernate.boot.Metadata;
 import org.hibernate.cfg.JDBCMetaDataConfiguration;
 import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.Table;
+import org.hibernate.tool.metadata.MetadataSources;
+import org.hibernate.tool.metadata.MetadataSourcesFactory;
 import org.hibernate.tools.test.util.HibernateUtil;
 import org.hibernate.tools.test.util.JdbcUtil;
 import org.junit.After;
@@ -24,13 +27,12 @@ import org.junit.Test;
  */
 public class TestCase {
 	
-	private JDBCMetaDataConfiguration jmdcfg;
+	private Metadata metadata;
 	
 	@Before
 	public void setUp() {
 		JdbcUtil.createDatabase(this);
-		jmdcfg = new JDBCMetaDataConfiguration();
-		jmdcfg.readFromJDBC();
+		metadata = MetadataSourcesFactory.createJdbcSources().buildMetadata();
 	}
 	
 	@After
@@ -41,22 +43,18 @@ public class TestCase {
 	@Test
 	public void testViewAndSynonyms() throws SQLException {
 		
-		PersistentClass classMapping = jmdcfg
-				.getMetadata()
-				.getEntityBinding("Basicview");
+		PersistentClass classMapping = metadata.getEntityBinding("Basicview");
 		Assert.assertNotNull(classMapping);
 	
-		classMapping = jmdcfg
-				.getMetadata()
-				.getEntityBinding("Weirdname");
+		classMapping = metadata.getEntityBinding("Weirdname");
 		Assert.assertTrue("If this is not-null synonyms apparently work!",classMapping==null);
 
 		// get comments
-		Table table = HibernateUtil.getTable(jmdcfg.getMetadata(), "BASIC");
+		Table table = HibernateUtil.getTable(metadata, "BASIC");
 		Assert.assertEquals("a basic comment", table.getComment());
 		Assert.assertEquals("a solid key", table.getPrimaryKey().getColumn(0).getComment());
 		
-		table = HibernateUtil.getTable(jmdcfg.getMetadata(), "MULTIKEYED");
+		table = HibernateUtil.getTable(metadata, "MULTIKEYED");
 		Assert.assertNull(table.getComment());
 		Assert.assertNull(table.getColumn(0).getComment());
 		
