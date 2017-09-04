ff8cbfe1b37eeca1cc59eaacd2eace38d1950d11
Koen Aers <koen.aers@gmail.com>
2017-09-04 08:57:31 +0200

    HBX-1433: Create class org.hibernate.tool.metadata.JdbcMetadataSources as a future replacement of JDBCMetaDataConfiguration - Replace JDBCMetaDataConfiguration by MetadataSources in org.hibernate.tool.jdbc2cfg.SearchEscapeString.TestCase

--- test/common/src/main/java/org/hibernate/tool/jdbc2cfg/SearchEscapeString/TestCase.java
+++ test/common/src/main/java/org/hibernate/tool/jdbc2cfg/SearchEscapeString/TestCase.java
@@ -6,8 +6,9 @@ package org.hibernate.tool.jdbc2cfg.SearchEscapeString;
 
 import java.sql.SQLException;
 
-import org.hibernate.cfg.JDBCMetaDataConfiguration;
+import org.hibernate.boot.Metadata;
 import org.hibernate.mapping.Table;
+import org.hibernate.tool.metadata.MetadataSourcesFactory;
 import org.hibernate.tools.test.util.HibernateUtil;
 import org.hibernate.tools.test.util.JUnitUtil;
 import org.hibernate.tools.test.util.JdbcUtil;
@@ -22,13 +23,12 @@ import org.junit.Test;
  */
 public class TestCase {
 
-	private JDBCMetaDataConfiguration jmdcfg = null;
+	private Metadata metadata = null;
 	
 	@Before
 	public void setUp() {
 		JdbcUtil.createDatabase(this);
-		jmdcfg = new JDBCMetaDataConfiguration();
-		jmdcfg.readFromJDBC();
+		metadata = MetadataSourcesFactory.createJdbcSources(null, null).buildMetadata();
 	}
 	
 	@After
@@ -41,11 +41,11 @@ public class TestCase {
 
 		JUnitUtil.assertIteratorContainsExactly( 
 				"There should be 2 tables!", 
-				jmdcfg.getMetadata().collectTableMappings().iterator(),
+				metadata.collectTableMappings().iterator(),
 				2);
 
-		Table table = HibernateUtil.getTable(jmdcfg.getMetadata(), JdbcUtil.toIdentifier(this, "B_TAB" ) );
-		Table table2 = HibernateUtil.getTable(jmdcfg.getMetadata(), JdbcUtil.toIdentifier(this, "B2TAB" ) );
+		Table table = HibernateUtil.getTable(metadata, JdbcUtil.toIdentifier(this, "B_TAB" ) );
+		Table table2 = HibernateUtil.getTable(metadata, JdbcUtil.toIdentifier(this, "B2TAB" ) );
 
 		Assert.assertNotNull(table);
 		Assert.assertNotNull(table2);
