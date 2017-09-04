cf7ba021b1e4c5d754d6ed7237cad20ac3f88796
Koen Aers <koen.aers@gmail.com>
2017-09-04 16:50:11 +0200

    HBX-1433: Create class org.hibernate.tool.metadata.JdbcMetadataSources as a future replacement of JDBCMetaDataConfiguration - Replace JDBCMetaDataConfiguration by MetadataSources in org.hibernate.tool.jdbc2cfg.BasicMultiSchema.TestCase

--- test/common/src/main/java/org/hibernate/tool/jdbc2cfg/BasicMultiSchema/TestCase.java
+++ test/common/src/main/java/org/hibernate/tool/jdbc2cfg/BasicMultiSchema/TestCase.java
@@ -6,10 +6,11 @@ package org.hibernate.tool.jdbc2cfg.BasicMultiSchema;
 
 import java.sql.SQLException;
 
-import org.hibernate.cfg.JDBCMetaDataConfiguration;
+import org.hibernate.boot.Metadata;
 import org.hibernate.mapping.Column;
 import org.hibernate.mapping.PrimaryKey;
 import org.hibernate.mapping.Table;
+import org.hibernate.tool.metadata.MetadataSourcesFactory;
 import org.hibernate.tools.test.util.HibernateUtil;
 import org.hibernate.tools.test.util.JUnitUtil;
 import org.hibernate.tools.test.util.JdbcUtil;
@@ -24,13 +25,14 @@ import org.junit.Test;
  */
 public class TestCase {
 
-	private JDBCMetaDataConfiguration jmdcfg = null;
+	private Metadata metadata = null;
 
 	@Before
 	public void setUp() {
 		JdbcUtil.createDatabase(this);
-		jmdcfg = new JDBCMetaDataConfiguration();
-		jmdcfg.readFromJDBC();
+		metadata = MetadataSourcesFactory
+				.createJdbcSources(null, null, true)
+				.buildMetadata();
 	}
 
 	@After
@@ -43,11 +45,11 @@ public class TestCase {
 
 		JUnitUtil.assertIteratorContainsExactly(
 				"There should be three tables!", 
-				jmdcfg.getMetadata().getEntityBindings().iterator(),
+				metadata.getEntityBindings().iterator(),
 				3);
 
 		Table table = HibernateUtil.getTable(
-				jmdcfg.getMetadata(), 
+				metadata, 
 				JdbcUtil.toIdentifier(this, "BASIC" ) );
 
 		Assert.assertEquals( 
@@ -79,7 +81,7 @@ public class TestCase {
 	@Test
 	public void testScalePrecisionLength() {
 		Table table = HibernateUtil.getTable(
-				jmdcfg.getMetadata(), 
+				metadata, 
 				JdbcUtil.toIdentifier(this, "BASIC" ) );
 		Column nameCol = table.getColumn( new Column( JdbcUtil.toIdentifier(this, "NAME" ) ) );
 		Assert.assertEquals( nameCol.getLength(), 20 );
@@ -131,7 +133,7 @@ public class TestCase {
 	@Test
 	public void testCompositeKeys() {
 		Table table = HibernateUtil.getTable(
-				jmdcfg.getMetadata(), 
+				metadata, 
 				JdbcUtil.toIdentifier(this, "MULTIKEYED"));
 		PrimaryKey primaryKey = table.getPrimaryKey();
 		Assert.assertEquals( 2, primaryKey.getColumnSpan() );
