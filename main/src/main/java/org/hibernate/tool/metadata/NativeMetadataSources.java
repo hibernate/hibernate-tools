89bd0e711588610454331a3eedd736acdfaf2d92
Koen Aers <koen.aers@gmail.com>
2017-09-05 19:43:38 +0200

    HBX-1449: Add constructor NativeMetadataSources(File, File[], Properties)

--- main/src/main/java/org/hibernate/tool/metadata/NativeMetadataSources.java
+++ main/src/main/java/org/hibernate/tool/metadata/NativeMetadataSources.java
@@ -1,10 +1,38 @@
 package org.hibernate.tool.metadata;
 
+import java.io.File;
+import java.util.Properties;
+
 import org.hibernate.boot.Metadata;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.util.MetadataHelper;
 
 public class NativeMetadataSources extends Configuration implements MetadataSources {
+	
+	public NativeMetadataSources() {
+		this(null, null, null);
+	}
+	
+	public NativeMetadataSources(
+			File cfgXmlFile, 
+			File[] mappingFiles, 
+			Properties properties) {
+		if (cfgXmlFile != null) {
+			configure(cfgXmlFile);
+		}
+		if (mappingFiles != null) {
+			for (File file : mappingFiles) {
+				if (file.getName().endsWith(".jar")) {
+					addJar(file); 
+				} else {
+					addFile(file);
+				}
+			}
+		}
+		if (properties != null) {
+			getProperties().putAll(properties);
+		}
+	}
 	
 	public Metadata buildMetadata() {
 		return MetadataHelper.getMetadata(this);
