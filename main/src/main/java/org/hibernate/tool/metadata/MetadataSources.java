7bb6e8c34f00458298404b1988416c78f3ae9961
Koen Aers <koen.aers@gmail.com>
2017-09-06 15:56:36 +0200

    HBX-1415: Phase out the use of the deprecated Configuration class - Add method MetadataSources#getProperties()

--- main/src/main/java/org/hibernate/tool/metadata/MetadataSources.java
+++ main/src/main/java/org/hibernate/tool/metadata/MetadataSources.java
@@ -1,9 +1,13 @@
 package org.hibernate.tool.metadata;
 
+import java.util.Properties;
+
 import org.hibernate.boot.Metadata;
 
 public interface MetadataSources {
 	
 	Metadata buildMetadata();
+	
+	Properties getProperties();
 
 }
