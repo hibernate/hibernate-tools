b290ee02f168091e769acb113340fe894bb6c9e4
Koen Aers <koen.aers@gmail.com>
2017-09-11 09:11:49 +0200

    HBX-1415: Phase out the use of the deprecated Configuration class - Remove Configuration as the superclass of JpaMetadataSources

--- main/src/main/java/org/hibernate/tool/metadata/JpaMetadataSources.java
+++ main/src/main/java/org/hibernate/tool/metadata/JpaMetadataSources.java
@@ -2,12 +2,59 @@ package org.hibernate.tool.metadata;
 
 import java.util.Properties;
 
-import org.hibernate.cfg.JPAConfiguration;
+import javax.persistence.EntityManagerFactory;
 
-public class JpaMetadataSources extends JPAConfiguration {
+import org.hibernate.HibernateException;
+import org.hibernate.boot.Metadata;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.hibernate.jpa.boot.internal.EntityManagerFactoryBuilderImpl;
 
-	public JpaMetadataSources(String persistenceUnit, Properties properties) {
-		super(persistenceUnit, properties);
+public class JpaMetadataSources implements MetadataSources {
+
+	private Properties properties = new Properties();
+	private Metadata metadata = null;
+	
+	public JpaMetadataSources(
+			final String persistenceUnit, 
+			final Properties properties) {
+		EntityManagerFactoryBuilderImpl entityManagerFactoryBuilder = 
+				createEntityManagerFactoryBuilder(persistenceUnit, properties);
+		EntityManagerFactory entityManagerFactory = 
+				entityManagerFactoryBuilder.build();
+		metadata = entityManagerFactoryBuilder.getMetadata();
+		properties.putAll(entityManagerFactory.getProperties());
+	}
+	
+	public Metadata buildMetadata() {
+		return metadata;
+	}
+	
+	public Properties getProperties() {
+		return properties;
+	}
+	
+	private static class PersistenceProvider extends HibernatePersistenceProvider {
+		public EntityManagerFactoryBuilderImpl getEntityManagerFactoryBuilder(
+				String persistenceUnit, 
+				Properties properties) {
+			EntityManagerFactoryBuilderImpl result = (EntityManagerFactoryBuilderImpl)getEntityManagerFactoryBuilderOrNull(
+					persistenceUnit, 
+					properties);
+			if (result == null) {
+				throw new HibernateException(
+						"Persistence unit not found: '" + persistenceUnit + "'."
+					);
+			}
+			return result;
+		}
+	}
+
+	private static EntityManagerFactoryBuilderImpl createEntityManagerFactoryBuilder(
+			final String persistenceUnit, 
+			final Properties properties) {
+		return new PersistenceProvider().getEntityManagerFactoryBuilder(
+				persistenceUnit, 
+				properties);
 	}
 	
 }
