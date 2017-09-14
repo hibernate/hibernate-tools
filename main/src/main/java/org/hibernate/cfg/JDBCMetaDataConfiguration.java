1e8b337368d93948152c2be5309f51e35acc1eb2
Koen Aers <koen.aers@gmail.com>
2017-09-14 09:58:47 +0200

    HBX-1467: Move functionality of JDBCMetadataConfiguration into JdbcMetadataSources - Clean up the remainder of JDBCMetaDataConfiguration

--- main/src/main/java/org/hibernate/cfg/JDBCMetaDataConfiguration.java
+++ main/src/main/java/org/hibernate/cfg/JDBCMetaDataConfiguration.java
@@ -4,29 +4,10 @@
  */
 package org.hibernate.cfg;
 
-import org.dom4j.Element;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * @author max
  *
  */
 public class JDBCMetaDataConfiguration extends Configuration {
-
-    private static final Logger log = LoggerFactory.getLogger(JDBCMetaDataConfiguration.class);
-	
-	private boolean ignoreconfigxmlmapppings = true;
-	// set to true and fk's that are part of a primary key will just be mapped as the raw value and as a readonly property. if false, it will be <many-to-one-key-property
-	
-    protected void parseMappingElement(Element subelement, String name) {
-        if(!ignoreconfigxmlmapppings ) {  
-        	//FIXME the method is private
-           // super.parseMappingElement(subelement, name);
-        } 
-        else {
-            log.info("Ignoring " + name + " mapping");
-        }
-    }
 
 }
