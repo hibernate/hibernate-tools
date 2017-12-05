57ce1c037dccbdd1a8994e7f7df8b493e295fb08
Koen Aers <koen.aers@gmail.com>
2017-12-05 09:20:29 +0100

    HBX-1510: Update version identifier to 5.2.8.Final
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.2.8-SNAPSHOT";
+	public static final String VERSION = "5.2.8.Final";
 	
 	private static final Version instance = new Version();
 	
