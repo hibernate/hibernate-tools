f5f684dc3cbae0b836ba6fb35f65dd0cfb162d96
Koen Aers <koen.aers@gmail.com>
2018-03-14 12:28:50 +0100

    HBX-1533: Update version identifier to 5.3.0.Beta1
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- main/src/main/java/org/hibernate/tool/Version.java
+++ main/src/main/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.3.0-SNAPSHOT";
+	public static final String VERSION = "5.3.0.Beta1";
 	
 	private static final Version instance = new Version();
 	
