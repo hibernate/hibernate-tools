3af360fd39a2a103755495cfe87b3c400e42af84
Koen Aers <koen.aers@gmail.com>
2018-03-24 17:23:55 +0100

    HBX-1404: Guard against connection leaks - Initial implementation of a connection leak detection class
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ test/utils/src/main/java/org/hibernate/tools/test/util/ConnectionLeakUtil.java
@@ -0,0 +1,65 @@
+package org.hibernate.tools.test.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectionLeakUtil {
+	
+	public static ConnectionLeakUtil forH2() {
+		ConnectionLeakUtil result = new ConnectionLeakUtil();
+		result.idleConnectionCounter = new H2IdleConnectionCounter();
+		return result;
+	}
+	
+	private IdleConnectionCounter idleConnectionCounter = null;
+	
+	private int connectionCount = 0;
+	
+	public void initialize() {
+		connectionCount = idleConnectionCounter.countConnections();
+	}
+	
+	public int getLeakedConnectionCount() {
+		int previousCount = connectionCount;
+		connectionCount = idleConnectionCounter.countConnections();
+		return connectionCount - previousCount;
+	}
+	
+	private static interface IdleConnectionCounter {
+		int countConnections();
+	}
+	
+	private static class H2IdleConnectionCounter implements IdleConnectionCounter {
+		private Connection newConnection() {
+			try {
+				return DriverManager.getConnection("jdbc:h2:mem:test", "sa", "");
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		public int countConnections() {		
+			try {
+				int result = 0;
+				Connection connection = newConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery(
+						"SELECT COUNT(*) " +
+						"FROM information_schema.sessions " + 
+						"WHERE statement IS NULL");
+				while (resultSet.next()) {
+					result = resultSet.getInt(1);
+				}
+				statement.close();
+				connection.close();
+				return result;
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		
+	}
+
+}
