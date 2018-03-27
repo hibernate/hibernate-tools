b908e2db3be7d421121bf2b17cba334c247e07e7
Jacques Stadler <jacques.stadler@gmail.com>
2018-03-27 22:08:39 +0200

    HBX-1537 Add description for the individual properties of the mojo

--- maven-plugin/src/main/java/org/hibernate/mvn/Hbm2JavaMojo.java
+++ maven-plugin/src/main/java/org/hibernate/mvn/Hbm2JavaMojo.java
@@ -17,12 +17,20 @@ import static org.apache.maven.plugins.annotations.LifecyclePhase.GENERATE_SOURC
 @Mojo(name = "hbm2java", defaultPhase = GENERATE_SOURCES)
 public class Hbm2JavaMojo extends AbstractHbm2xMojo {
 
+    /** The directory into which the JPA entities will be generated. */
     @Parameter(defaultValue = "${project.build.directory}/generated-sources/")
     private File outputDirectory;
+
+    /** Code will contain EJB 3 features, e.g. using annotations from javax.persistence
+     * and org.hibernate.annotations. */
     @Parameter(defaultValue = "false")
     private boolean ejb3;
+
+    /** Code will contain JDK 5 constructs such as generics and static imports. */
     @Parameter(defaultValue = "false")
     private boolean jdk5;
+
+    /** A path used for looking up user-edited templates. */
     @Parameter
     private String templatePath;
 
