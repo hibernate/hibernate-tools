214fe4b5c6d131ea5b576c609f853b4af5c6dfb1
Koen Aers <koen.aers@gmail.com>
2018-03-28 12:45:58 +0200

    HBX-1538: 'formula' attribute is erroneously generated in addition to the <formula> nested element - Add test proving the problem
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ test/nodb/src/test/java/org/hibernate/tool/hbm2x/hbm2hbmxml/FormulaTest/Customer.java
@@ -0,0 +1,34 @@
+package org.hibernate.tool.hbm2x.hbm2hbmxml.FormulaTest;
+
+public class Customer {
+
+	private String name;
+	private String customerId;
+	private Address billingAddress;
+	private Address shippingAddress;
+
+	public Address getBillingAddress() {
+		return billingAddress;
+	}
+	public void setBillingAddress(Address billingAddress) {
+		this.billingAddress = billingAddress;
+	}
+	public String getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Address getShippingAddress() {
+		return shippingAddress;
+	}
+	public void setShippingAddress(Address shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
+}
