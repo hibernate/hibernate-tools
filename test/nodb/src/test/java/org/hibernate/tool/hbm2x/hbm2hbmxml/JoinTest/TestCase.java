8c602d666402eaef739344f6e0ee1fb91d5a31d0
Koen Aers <koen.aers@gmail.com>
2018-04-02 11:05:31 +0200

    HBX-1541: <join> element is generated at wrong location - add a test that shows the problem
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ test/nodb/src/test/java/org/hibernate/tool/hbm2x/hbm2hbmxml/JoinTest/TestCase.java
@@ -0,0 +1,82 @@
+/*
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.hibernate.tool.hbm2x.hbm2hbmxml.JoinTest;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.tool.api.metadata.MetadataDescriptor;
+import org.hibernate.tool.api.metadata.MetadataDescriptorFactory;
+import org.hibernate.tool.hbm2x.Exporter;
+import org.hibernate.tool.hbm2x.HibernateMappingExporter;
+import org.hibernate.tools.test.util.HibernateUtil;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+/**
+ * @author koen
+ */
+public class TestCase {
+
+	private static final String[] HBM_XML_FILES = new String[] {
+			"Parent.hbm.xml"
+	};
+	
+	@Rule
+	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+	
+	private File outputDir = null;
+	private File resourcesDir = null;
+	
+	private Exporter hbmexporter = null;
+
+	@Before
+	public void setUp() throws Exception {
+		outputDir = new File(temporaryFolder.getRoot(), "output");
+		outputDir.mkdir();
+		resourcesDir = new File(temporaryFolder.getRoot(), "resources");
+		resourcesDir.mkdir();
+		MetadataDescriptor metadataDescriptor = HibernateUtil
+				.initializeMetadataDescriptor(this, HBM_XML_FILES, resourcesDir);
+		hbmexporter = new HibernateMappingExporter();
+		hbmexporter.setMetadataDescriptor(metadataDescriptor);
+		hbmexporter.setOutputDirectory(outputDir);
+		hbmexporter.start();
+	}
+	
+	@Test
+	public void testReadable() {
+        ArrayList<File> files = new ArrayList<File>(4); 
+        files.add(new File(
+        		outputDir, 
+        		"/org/hibernate/tool/hbm2x/hbm2hbmxml/JoinTest/Parent.hbm.xml"));
+        files.add(new File(
+        		outputDir, 
+        		"/org/hibernate/tool/hbm2x/hbm2hbmxml/JoinTest/Child.hbm.xml"));
+		Properties properties = new Properties();
+		properties.setProperty(AvailableSettings.DIALECT, HibernateUtil.Dialect.class.getName());
+		MetadataDescriptor metadataDescriptor = MetadataDescriptorFactory
+				.createNativeDescriptor(null, files.toArray(new File[2]), properties);
+        Assert.assertNotNull(metadataDescriptor.createMetadata());
+    }
+	
+}
