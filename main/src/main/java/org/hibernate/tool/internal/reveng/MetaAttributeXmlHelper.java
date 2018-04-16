da37ce9f24df7efd26c332aac65149a8d7c286b0
Koen Aers <koen.aers@gmail.com>
2018-04-16 19:11:09 +0200

    HBX-1558: Create class 'org.hibernate.tool.internal.reveng.MetaAttributeXmlHelper' and implement a version of 'org.hibernate.tool.internal.reveng.MetaAttributeHelper#loadMetaMap(Element)' using 'org.w3c.dom.Element'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ main/src/main/java/org/hibernate/tool/internal/reveng/MetaAttributeXmlHelper.java
@@ -0,0 +1,38 @@
+package org.hibernate.tool.internal.reveng;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.collections.MultiMap;
+import org.apache.commons.collections.map.MultiValueMap;
+import org.hibernate.tool.internal.reveng.MetaAttributeHelper.SimpleMetaAttribute;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class MetaAttributeXmlHelper {
+
+	 static MultiMap loadMetaMap(Element element) {
+		MultiMap result = new MultiValueMap();
+		List<Element> metaAttributeList = new ArrayList<Element>();
+		NodeList nodeList = element.getElementsByTagName("meta");
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			metaAttributeList.add((Element)nodeList.item(i));
+		}
+		for (Iterator<Element> iter = metaAttributeList.iterator(); iter.hasNext();) {
+			Element metaAttribute = iter.next();
+			String attribute = metaAttribute.getAttributeNode("attribute").getValue();
+			String value = metaAttribute.getTextContent();
+			String inheritStr= metaAttribute.getAttributeNode("inherit").getValue();
+			boolean inherit = true;
+			if(inheritStr!=null) {
+				inherit = Boolean.valueOf(inheritStr).booleanValue(); 
+			}			
+			SimpleMetaAttribute ma = new SimpleMetaAttribute(value, inherit);
+			result.put(attribute, ma);
+		}
+		return result;
+
+	}
+
+}
