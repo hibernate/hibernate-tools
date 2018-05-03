1f99b931350b62f1c2c5195afa3651dfbe53443d
Koen Aers <koen.aers@gmail.com>
2018-05-03 17:44:30 +0200

    HBX-1576: Update the version to 5.3.0.CR2
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- main/src/main/java/org/hibernate/tool/Version.java
+++ main/src/main/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.3.0-SNAPSHOT";
+	public static final String VERSION = "5.3.0.CR2";
 	
 	private static final Version instance = new Version();
 	
