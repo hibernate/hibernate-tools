9e02b1eab358e267ff1e4c6b6ce6fd3cd29a9c31
Koen Aers <koen.aers@gmail.com>
2018-05-29 17:58:51 +0200

    HBX-1635: Update version identifier to 5.3.2-SNAPSHOT
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- main/src/main/java/org/hibernate/tool/Version.java
+++ main/src/main/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.3.1.Final";
+	public static final String VERSION = "5.3.2-SNAPSHOT";
 	
 	private static final Version instance = new Version();
 	
