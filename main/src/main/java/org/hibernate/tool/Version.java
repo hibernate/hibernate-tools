3ea93c51291c99a3a2cfa73c0ecb9f346c597a80
Koen Aers <koen.aers@gmail.com>
2018-09-10 17:50:21 +0200

    HBX-1727: Update version identifier to 5.3.5.Final
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- main/src/main/java/org/hibernate/tool/Version.java
+++ main/src/main/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.3.5-SNAPSHOT";
+	public static final String VERSION = "5.3.5.Final";
 	
 	private static final Version instance = new Version();
 	
