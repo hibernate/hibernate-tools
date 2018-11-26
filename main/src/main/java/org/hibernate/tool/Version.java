72a0eb0a15501b01fbc66e1dc52f0e51c9c138de
Koen Aers <koen.aers@gmail.com>
2018-11-26 21:09:12 +0100

    HBX-1759: Update version identifier to 5.4.0-SNAPSHOT
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- main/src/main/java/org/hibernate/tool/Version.java
+++ main/src/main/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.4.0.CR1";
+	public static final String VERSION = "5.4.0-SNAPSHOT";
 	
 	private static final Version instance = new Version();
 	
