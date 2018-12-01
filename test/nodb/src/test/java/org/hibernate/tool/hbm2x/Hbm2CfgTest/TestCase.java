aac01717d45740345224d0bce611d26b49fc43e6
Koen Aers <koen.aers@gmail.com>
2018-12-01 01:16:37 +0100

    Remove unsupported '@SuppressWarnings("el-syntax")' from class 'org.hibernate.tool.hbm2x.Hbm2CfgTest.TestCase'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/nodb/src/test/java/org/hibernate/tool/hbm2x/Hbm2CfgTest/TestCase.java
+++ test/nodb/src/test/java/org/hibernate/tool/hbm2x/Hbm2CfgTest/TestCase.java
@@ -112,7 +112,6 @@ public class TestCase {
 		Assert.assertEquals(1, cfgexporter.getArtifactCollector().getFileCount("cfg.xml"));
 	}
 	
-	@SuppressWarnings("el-syntax")
 	@Test
 	public void testNoVelocityLeftOvers() {
         Assert.assertNull(FileUtil.findFirstString("${",new File(outputDir, "hibernate.cfg.xml")));
