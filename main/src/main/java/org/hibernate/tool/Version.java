698bbd8e3018ea0fa7a0124afd8146dabf2e6dc6
Koen Aers <koen.aers@gmail.com>
2019-01-28 17:13:12 +0100

    HBX-1794: Update version identifier to 5.4.2-SNAPSHOT
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- main/src/main/java/org/hibernate/tool/Version.java
+++ main/src/main/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.4.1.Final";
+	public static final String VERSION = "5.4.2-SNAPSHOT";
 	
 	private static final Version instance = new Version();
 	
