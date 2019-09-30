3b688310560ec1426edb6aac46dd1a5bf03d39a4
Koen Aers <koen.aers@gmail.com>
2019-09-30 15:27:04 +0200

    HBX-1856: Rename 'org.hibernate.tool.util.ReflectHelper' to 'org.hibernate.tool.util.ReflectionUtil'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- utils/src/test/java/org/hibernate/tool/utils/ReflectionUtilTest.java
+++ utils/src/test/java/org/hibernate/tool/utils/ReflectionUtilTest.java
@@ -3,21 +3,21 @@ package org.hibernate.tool.utils;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
-import org.hibernate.tool.util.ReflectHelper;
+import org.hibernate.tool.util.ReflectionUtil;
 import org.junit.jupiter.api.Test;
 
-public class ReflectHelperTest {
+public class ReflectionUtilTest {
 	
 	@Test
 	public void testClassForName() throws Exception {
 		ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
 		assertNotNull(contextClassLoader);
 		try {
-			Class<?> clazz = ReflectHelper.classForName("org.hibernate.tool.util.ReflectHelper");
-			assertSame(ReflectHelper.class, clazz);
+			Class<?> clazz = ReflectionUtil.classForName("org.hibernate.tool.util.ReflectionUtil");
+			assertSame(ReflectionUtil.class, clazz);
 			Thread.currentThread().setContextClassLoader(null);
-			clazz = ReflectHelper.classForName("org.hibernate.tool.util.ReflectHelper");
-			assertSame(ReflectHelper.class, clazz);
+			clazz = ReflectionUtil.classForName("org.hibernate.tool.util.ReflectionUtil");
+			assertSame(ReflectionUtil.class, clazz);
 		} finally {
 			Thread.currentThread().setContextClassLoader(contextClassLoader);
 		}
