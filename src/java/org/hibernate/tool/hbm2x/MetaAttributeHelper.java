be00bce8854839ce1b622caef59b1f55da52c6fd
Koen Aers <koen.aers@gmail.com>
2019-10-16 14:56:56 +0200

    HBX-1885: Replace the use of the deprecated classes 'MultiMap' and 'MultiValueMap' by 'MultiValuedMap' and 'HashSetValuedHashMap'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- src/java/org/hibernate/tool/hbm2x/MetaAttributeHelper.java
+++ src/java/org/hibernate/tool/hbm2x/MetaAttributeHelper.java
@@ -3,7 +3,8 @@ package org.hibernate.tool.hbm2x;
 import java.util.Collection;
 import java.util.Iterator;
 
-import org.apache.commons.collections4.MultiMap;
+import org.apache.commons.collections4.MultiValuedMap;
+import org.hibernate.cfg.reveng.SimpleMetaAttribute;
 import org.hibernate.mapping.MetaAttribute;
 
 /**
@@ -62,27 +63,18 @@ public final class MetaAttributeHelper {
 		return getMetaAsString(c, "");
 	}
 
-    /**
-     * Copies all the values from one MultiMap to another.
-     * This method is needed because the (undocumented) behaviour of 
-     * MultiHashMap.putAll in versions of Commons Collections prior to 3.0
-     * was to replace the collection in the destination, whereas in 3.0
-     * it adds the collection from the source as an _element_ of the collection
-     * in the destination.  This method makes no assumptions about the implementation
-     * of the MultiMap, and should work with all versions.
-     * 
-     * @param destination
-     * @param specific
-     */
-     public static void copyMultiMap(MultiMap destination, MultiMap specific) {
-        for (Iterator<?> keyIterator = specific.keySet().iterator(); keyIterator.hasNext(); ) {
-            Object key = keyIterator.next();
-            Collection<?> c = (Collection<?>)specific.get(key);
-            for (Iterator<?> valueIterator = c.iterator(); valueIterator.hasNext(); ) 
+    public static void copyMultiMap(
+    		MultiValuedMap<String, SimpleMetaAttribute> destination, 
+    		MultiValuedMap<String, SimpleMetaAttribute> specific) {
+        for (Iterator<String> keyIterator = specific.keySet().iterator(); keyIterator.hasNext(); ) {
+            String key = keyIterator.next();
+            Collection<SimpleMetaAttribute> c = specific.get(key);
+            for (Iterator<SimpleMetaAttribute> valueIterator = c.iterator(); valueIterator.hasNext();) { 
                 destination.put(key, valueIterator.next() );
+            }
         }
     }
-
+    
 	public static boolean getMetaAsBool(org.hibernate.mapping.MetaAttribute metaAttribute, boolean defaultValue) {
 		return getMetaAsBool(metaAttribute==null?null:metaAttribute.getValues(), defaultValue);
 	}
