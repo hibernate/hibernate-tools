88a1b005a242a27eb3aab1a6e73c04575b2bbd29
Koen Aers <koen.aers@gmail.com>
2019-10-17 10:21:34 +0200

    HBX-1886: HBX-1886: Replace commons-logging with java logging
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- src/test/org/hibernate/tool/hbm2x/Hbm2EJBDaoTest.java
+++ src/test/org/hibernate/tool/hbm2x/Hbm2EJBDaoTest.java
@@ -12,7 +12,6 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.persistence.Persistence;
 
-import org.apache.commons.logging.Log;
 import org.hibernate.tool.NonReflectiveTestCase;
 import org.hibernate.tool.test.TestHelper;
 
@@ -51,7 +50,6 @@ public class Hbm2EJBDaoTest extends NonReflectiveTestCase {
 		
 		ArrayList<String> list = new ArrayList<String>();
 		List<File> jars = new ArrayList<File>();
-		jars.add(TestHelper.findJarFileFor(Log.class)); // for commons logging
 		jars.add(TestHelper.findJarFileFor(Persistence.class)); // for jpa api
 		jars.add(TestHelper.findJarFileFor(EJB.class)); // for javaee api
 		TestHelper.compile(getOutputDir(), file, TestHelper.visitAllFiles(getOutputDir(), list), "1.5", TestHelper.buildClasspathFromFileList(jars) );
