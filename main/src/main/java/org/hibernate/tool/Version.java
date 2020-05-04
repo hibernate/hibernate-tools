c3df9c72b5c95173ba8f4ead5bfc2fe682c0dac5
Koen Aers <koen.aers@gmail.com>
2020-05-04 20:02:08 +0200

    HBX-2025: Update version identifier to 5.3.18-SNAPSHOT
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- main/src/main/java/org/hibernate/tool/Version.java
+++ main/src/main/java/org/hibernate/tool/Version.java
@@ -4,7 +4,7 @@ import java.util.logging.Logger;
 
 final public class Version {
 
-	public static final String VERSION = "5.3.17.Final";
+	public static final String VERSION = "5.3.18-SNAPSHOT";
 	
 	private static final Version instance = new Version();
 	
