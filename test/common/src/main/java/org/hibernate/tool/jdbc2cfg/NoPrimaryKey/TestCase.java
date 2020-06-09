ffedbd80e1e3b0249a0533ea9e17a13edfab36e3
Koen Aers <koen.aers@gmail.com>
2020-06-09 19:41:30 +0200

    HBX-2035: Investigate en reenable failing tests for 6.0.0.x - Add 'org.hibernate.tool.jdbc2cfg.NoPrimaryKey.TestCase.testMe()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/common/src/main/java/org/hibernate/tool/jdbc2cfg/NoPrimaryKey/TestCase.java
+++ test/common/src/main/java/org/hibernate/tool/jdbc2cfg/NoPrimaryKey/TestCase.java
@@ -9,7 +9,6 @@ import org.hibernate.tools.test.util.JdbcUtil;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 
@@ -30,8 +29,6 @@ public class TestCase {
 		JdbcUtil.dropDatabase(this);
 	}
 
-	// TODO HBX-2035: Investigate and reenable
-	@Ignore
 	@Test
 	public void testMe() {
 		Assert.assertNotNull(
