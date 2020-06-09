4d02890fa5850a1f7c4edb6b062418cce6ace613
Koen Aers <koen.aers@gmail.com>
2020-06-09 14:45:04 +0200

    HBX-2035: Investigate en reenable failing tests for 6.0.0.x - Add 'org.hibernate.tool.jdbc2cfg.ManyToMany.TestCase.testFalsePositive()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/common/src/main/java/org/hibernate/tool/jdbc2cfg/ManyToMany/TestCase.java
+++ test/common/src/main/java/org/hibernate/tool/jdbc2cfg/ManyToMany/TestCase.java
@@ -103,8 +103,6 @@ public class TestCase {
 		
 	}
 
-	// TODO HBX-2035: Investigate and reenable
-	@Ignore
 	@Test
 	public void testFalsePositive() {
 		Metadata metadata = MetadataDescriptorFactory
