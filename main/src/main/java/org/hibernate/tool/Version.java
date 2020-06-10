0b20c990092f1d10980b51a7dbaf704b73c4b935
Koen Aers <koen.aers@gmail.com>
2020-06-10 11:27:42 +0200

    HBX-2043: Set up 5.5 branch
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- main/src/main/java/org/hibernate/tool/Version.java
+++ main/src/main/java/org/hibernate/tool/Version.java
@@ -4,7 +4,7 @@ import java.util.logging.Logger;
 
 final public class Version {
 
-	public static final String VERSION = "5.4.18-SNAPSHOT";
+	public static final String VERSION = "5.5.0-SNAPSHOT";
 	
 	private static final Version instance = new Version();
 	
