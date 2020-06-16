cba4214e43864eb1f1ddebfe739d154804a3cdc4
Koen Aers <koen.aers@gmail.com>
2020-06-16 10:38:50 +0200

    HBX-2035: Investigate en reenable failing tests for 6.0.0.x - Add 'org.hibernate.tool.jdbc2cfg.RevEngForeignKey.TestCase.testSetAndManyToOne()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/common/src/main/java/org/hibernate/tool/jdbc2cfg/RevEngForeignKey/TestCase.java
+++ test/common/src/main/java/org/hibernate/tool/jdbc2cfg/RevEngForeignKey/TestCase.java
@@ -65,8 +65,6 @@ public class TestCase {
 		Assert.assertEquals("id", worksOn.getIdentifierProperty().getName());	
 	}
 
-	// TODO HBX-2035: Investigate and reenable
-	@Ignore
 	@Test
 	public void testSetAndManyToOne() {
 		OverrideRepository or = new OverrideRepository();
