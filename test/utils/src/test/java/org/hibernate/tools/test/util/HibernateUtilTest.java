5790f2430d1ff9e6a2cf9d7226a45c955f820aa5
Koen Aers <koen.aers@gmail.com>
2020-10-05 14:46:00 +0200

    HBX-2084: Update dependency on Hibernate ORM to version 5.4.22.Final
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/utils/src/test/java/org/hibernate/tools/test/util/HibernateUtilTest.java
+++ test/utils/src/test/java/org/hibernate/tools/test/util/HibernateUtilTest.java
@@ -26,7 +26,7 @@ public class HibernateUtilTest {
 		Table table = new Table();
 		Assert.assertNull(HibernateUtil.getForeignKey(table, "foo"));
 		Assert.assertNull(HibernateUtil.getForeignKey(table, "bar"));
-		table.createForeignKey("foo", Collections.emptyList(), null, null);
+		table.createForeignKey("foo", Collections.emptyList(), "bar", null);
 		Assert.assertNotNull(HibernateUtil.getForeignKey(table, "foo"));
 		Assert.assertNull(HibernateUtil.getForeignKey(table, "bar"));
 	}
