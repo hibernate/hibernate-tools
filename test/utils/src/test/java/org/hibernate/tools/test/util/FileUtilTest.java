634e300d4e540b9481c08ad71e158a392f3cb208
Koen Aers <koen.aers@gmail.com>
2021-01-20 14:51:44 +0100

    HBX-2124: Replace JUnit4 tests with JUnit Jupiter tests in 'org.hibernate.tools.test.util.FileUtilTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/utils/src/test/java/org/hibernate/tools/test/util/FileUtilTest.java
+++ test/utils/src/test/java/org/hibernate/tools/test/util/FileUtilTest.java
@@ -19,36 +19,38 @@
  */
 package org.hibernate.tools.test.util;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.File;
 
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 public class FileUtilTest {
 	
-	@Rule
-	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+	@TempDir
+	public File outputFolder = new File("output");
 	
 	@Test
 	public void testFindFirstString() throws Exception {
 		String resource = "/org/hibernate/tools/test/util/FileUtilTest.resource";
 		File file = new File(getClass().getResource(resource).toURI());
-		Assert.assertEquals("or would it be a bar test?", FileUtil.findFirstString("bar", file));
-		Assert.assertNull(FileUtil.findFirstString("foobar", file));
-		Assert.assertEquals("This is a foo test", FileUtil.findFirstString("test", file));
-		Assert.assertEquals("And a third line...", FileUtil.findFirstString("third", file));
+		assertEquals("or would it be a bar test?", FileUtil.findFirstString("bar", file));
+		assertNull(FileUtil.findFirstString("foobar", file));
+		assertEquals("This is a foo test", FileUtil.findFirstString("test", file));
+		assertEquals("And a third line...", FileUtil.findFirstString("third", file));
 	}
 	
 	@Test
 	public void testGenerateNoopComparator() throws Exception {
-		FileUtil.generateNoopComparator(temporaryFolder.getRoot());
-		File pkg = new File(temporaryFolder.getRoot(), "comparator");
-		Assert.assertTrue(pkg.isDirectory() && pkg.exists());
+		FileUtil.generateNoopComparator(outputFolder);
+		File pkg = new File(outputFolder, "comparator");
+		assertTrue(pkg.isDirectory() && pkg.exists());
 		File comp = new File(pkg, "NoopComparator.java");
-		Assert.assertTrue(comp.isFile() && comp.exists());
-		Assert.assertEquals(
+		assertTrue(comp.isFile() && comp.exists());
+		assertEquals(
 				"public class NoopComparator implements Comparator {", 
 				FileUtil.findFirstString("NoopComparator", comp));
 	}
