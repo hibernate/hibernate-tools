bab60f219c4f6900472f767a2793e20804bdae63
Koen Aers <koen.aers@gmail.com>
2021-01-25 11:37:53 +0100

    HBX-2124: Replace JUnit4 tests with JUnit Jupiter tests in 'org.hibernate.tool.hbm2x..hbm2hbmxml.TypeParamsTest.TestCase'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/nodb/src/test/java/org/hibernate/tool/hbm2x/hbm2hbmxml/TypeParamsTest/TestCase.java
+++ test/nodb/src/test/java/org/hibernate/tool/hbm2x/hbm2hbmxml/TypeParamsTest/TestCase.java
@@ -1,7 +1,7 @@
 /*
  * Hibernate Tools, Tooling for your Hibernate Projects
  * 
- * Copyright 2004-2020 Red Hat, Inc.
+ * Copyright 2004-2021 Red Hat, Inc.
  *
  * Licensed under the GNU Lesser General Public License (LGPL), 
  * version 2.1 or later (the "License").
@@ -19,6 +19,10 @@
  */
 
 package org.hibernate.tool.hbm2x.hbm2hbmxml.TypeParamsTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.util.HashMap;
@@ -41,12 +45,10 @@ import org.hibernate.tool.api.metadata.MetadataDescriptorFactory;
 import org.hibernate.tool.internal.export.hbm.HbmExporter;
 import org.hibernate.tools.test.util.HibernateUtil;
 import org.hibernate.tools.test.util.JUnitUtil;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 /**
  * @author Dmitry Geraskov
@@ -58,58 +60,58 @@ public class TestCase {
 			"Order.hbm.xml",
 	};
 	
-	@Rule
-	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+	@TempDir
+	public File outputFolder = new File("output");
 	
-	private File outputDir = null;
+	private File srcDir = null;
 	private File resourcesDir = null;
 
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
-		outputDir = new File(temporaryFolder.getRoot(), "output");
-		outputDir.mkdir();
-		resourcesDir = new File(temporaryFolder.getRoot(), "resources");
+		srcDir = new File(outputFolder, "output");
+		srcDir.mkdir();
+		resourcesDir = new File(outputFolder, "resources");
 		resourcesDir.mkdir();
 		MetadataDescriptor metadataDescriptor = HibernateUtil
 				.initializeMetadataDescriptor(this, HBM_XML_FILES, resourcesDir);
 		Exporter hbmexporter = new HbmExporter();
 		hbmexporter.getProperties().put(ExporterConstants.METADATA_DESCRIPTOR, metadataDescriptor);
-		hbmexporter.getProperties().put(ExporterConstants.DESTINATION_FOLDER, outputDir);
+		hbmexporter.getProperties().put(ExporterConstants.DESTINATION_FOLDER, srcDir);
 		hbmexporter.start();
 	}
 
 	// TODO HBX-2062: Investigate and reenable
-	@Ignore
+	@Disabled
 	@Test
 	public void testAllFilesExistence() {
 		JUnitUtil.assertIsNonEmptyFile(
 				new File(
-						outputDir,  
+						srcDir,  
 						"org/hibernate/tool/hbm2x/hbm2hbmxml/TypeParamsTest/Order.hbm.xml"));
 	}
 
 	// TODO HBX-2062: Investigate and reenable
-	@Ignore
+	@Disabled
 	@Test
 	public void testReadable() {
 		File orderHbmXml =
         		new File(
-        				outputDir, 
+        				srcDir, 
         				"org/hibernate/tool/hbm2x/hbm2hbmxml/TypeParamsTest/Order.hbm.xml");
 		Properties properties = new Properties();
 		properties.setProperty(AvailableSettings.DIALECT, HibernateUtil.Dialect.class.getName());
 		File[] files = new File[] { orderHbmXml };
 		MetadataDescriptor metadataDescriptor = MetadataDescriptorFactory
 				.createNativeDescriptor(null, files, properties);
-        Assert.assertNotNull(metadataDescriptor.createMetadata());
+        assertNotNull(metadataDescriptor.createMetadata());
     }
 
 	// TODO HBX-2062: Investigate and reenable
-	@Ignore
+	@Disabled
 	@Test
 	public void testTypeParamsElements() throws DocumentException {
 		File outputXml = new File(
-				outputDir,  
+				srcDir,  
 				"org/hibernate/tool/hbm2x/hbm2hbmxml/TypeParamsTest/Order.hbm.xml");
 		JUnitUtil.assertIsNonEmptyFile(outputXml);
 		SAXReader xmlReader =  new SAXReader();
@@ -117,7 +119,7 @@ public class TestCase {
 		Document document = xmlReader.read(outputXml);
 		XPath xpath = DocumentHelper.createXPath("//hibernate-mapping/class/property");
 		List<?> list = xpath.selectNodes(document);
-		Assert.assertEquals("Expected to get one property element", 2, list.size());
+		assertEquals(2, list.size(), "Expected to get one property element");
 		Element statusElement = (Element) list.get(0);
 		Element nameElement = (Element) list.get(1);
 		if(!statusElement.attribute( "name" ).getText().equals("status")) {
@@ -125,42 +127,42 @@ public class TestCase {
 			nameElement = statusElement;
 			statusElement = temp;
 		}
-		Assert.assertEquals(statusElement.attribute( "name" ).getText(),"status");
+		assertEquals(statusElement.attribute( "name" ).getText(),"status");
 		list = statusElement.elements("type");
-		Assert.assertEquals("Expected to get one type element", 1, list.size());
+		assertEquals(1, list.size(), "Expected to get one type element");
 		list =  ((Element) list.get(0)).elements("param");		
-		Assert.assertEquals("Expected to get 5 params elements", list.size(), 5);
+		assertEquals(list.size(), 5, "Expected to get 5 params elements");
 		Map<String, String> params = new HashMap<String, String>();
 		for (int i = 0; i < list.size(); i++) {
 			Element param = (Element) list.get(i);
 			params.put(param.attribute( "name" ).getText(), param.getText());
 		}
 		Set<String> set = params.keySet();
-		Assert.assertEquals("Expected to get 5 different params elements", params.size(), 5);
-		Assert.assertTrue(
-				"Can't find 'catalog' param", 
-				set.contains("catalog"));
-		Assert.assertEquals("", params.get("catalog"));
-		Assert.assertTrue(
-				"Can't find 'column' param", 
-				set.contains("column"));
-		Assert.assertEquals("STATUS", params.get("column"));
-		Assert.assertTrue(
-				"Can't find 'table' param", 
-				set.contains("table"));
-		Assert.assertEquals("ORDERS", params.get("table"));
-		Assert.assertTrue(
-				"Can't find 'schema' param", 
-				set.contains("schema"));
-		Assert.assertEquals("", params.get("schema"));
-		Assert.assertTrue(
-				"Can't find 'enumClass' param", 
-				set.contains("enumClass"));
-		Assert.assertEquals(
+		assertEquals(params.size(), 5, "Expected to get 5 different params elements");
+		assertTrue(
+				set.contains("catalog"),
+				"Can't find 'catalog' param");
+		assertEquals("", params.get("catalog"));
+		assertTrue(
+				set.contains("column"),
+				"Can't find 'column' param");
+		assertEquals("STATUS", params.get("column"));
+		assertTrue(
+				set.contains("table"),
+				"Can't find 'table' param");
+		assertEquals("ORDERS", params.get("table"));
+		assertTrue(
+				set.contains("schema"),
+				"Can't find 'schema' param");
+		assertEquals("", params.get("schema"));
+		assertTrue(
+				set.contains("enumClass"),
+				"Can't find 'enumClass' param");
+		assertEquals(
 				"org.hibernate.tool.hbm2x.hbm2hbmxml.Order$Status", 
 				params.get("enumClass"));
-		Assert.assertTrue("property name should not have any type element",nameElement.elements("type").isEmpty());
-		Assert.assertEquals(nameElement.attribute("type").getText(), "string");
+		assertTrue(nameElement.elements("type").isEmpty(), "property name should not have any type element");
+		assertEquals(nameElement.attribute("type").getText(), "string");
 	}
 
 }
