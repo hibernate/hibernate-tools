2189e96033e5c673bfc478febf951139065a00f6
Koen Aers <koen.aers@gmail.com>
2021-02-01 12:55:05 +0100

    HBX-2124: Replace JUnit4 tests with JUnit Jupiter tests in 'org.hibernate.tool.ant.HbmLint'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/common/src/main/java/org/hibernate/tool/test/db/JupiterCommonTestSuite.java
+++ test/common/src/main/java/org/hibernate/tool/test/db/JupiterCommonTestSuite.java
@@ -14,5 +14,6 @@ public class JupiterCommonTestSuite {
 	@Nested public class GenericExport extends org.hibernate.tool.ant.GenericExport.TestCase {}
 	@Nested public class Hbm2JavaConfiguration extends org.hibernate.tool.ant.Hbm2JavaConfiguration.TestCase {}
 	@Nested public class Hbm2JavaEJB3Configuration extends org.hibernate.tool.ant.Hbm2JavaEJB3Configuration.TestCase {}
+	@Nested public class HbmLint extends org.hibernate.tool.ant.HbmLint.TestCase {}
 
 }
