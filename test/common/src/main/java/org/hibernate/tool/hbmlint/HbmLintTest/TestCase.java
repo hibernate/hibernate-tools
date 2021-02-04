4acb2c101373d9c0b7faea3b2b01c27492bac4ea
Koen Aers <koen.aers@gmail.com>
2021-02-04 10:41:37 +0100

    HBX-2124: Replace JUnit4 tests with JUnit Jupiter tests in 'org.hibernate.tool.hbmlint.HbmLintTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/common/src/main/java/org/hibernate/tool/hbmlint/HbmLintTest/TestCase.java
+++ test/common/src/main/java/org/hibernate/tool/hbmlint/HbmLintTest/TestCase.java
@@ -1,4 +1,25 @@
+/*
+ * Hibernate Tools, Tooling for your Hibernate Projects
+ * 
+ * Copyright 2004-2021 Red Hat, Inc.
+ *
+ * Licensed under the GNU Lesser General Public License (LGPL), 
+ * version 2.1 or later (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may read the licence in the 'lgpl.txt' file in the root folder of 
+ * project or obtain a copy at
+ *
+ *     http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.hibernate.tool.hbmlint.HbmLintTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
 
@@ -11,11 +32,9 @@ import org.hibernate.tool.internal.export.lint.HbmLintExporter;
 import org.hibernate.tool.internal.export.lint.InstrumentationDetector;
 import org.hibernate.tool.internal.export.lint.ShadowedIdentifierDetector;
 import org.hibernate.tools.test.util.HibernateUtil;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 public class TestCase {
 
@@ -25,19 +44,16 @@ public class TestCase {
 			"BrokenLazy.hbm.xml"
 	};
 	
-	@Rule
-	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+	@TempDir
+	public File outputDir = new File("output");
 	
-	private File outputDir = null;
 	private File resourcesDir = null;
 	
 	private MetadataDescriptor metadataDescriptor = null;
 	
-	@Before
+	@BeforeEach
 	public void setUp() {
-		outputDir = new File(temporaryFolder.getRoot(), "output");
-		outputDir.mkdir();
-		resourcesDir = new File(temporaryFolder.getRoot(), "resources");
+		resourcesDir = new File(outputDir, "resources");
 		resourcesDir.mkdir();
 		metadataDescriptor = HibernateUtil.initializeMetadataDescriptor(this, HBM_XML_FILES, resourcesDir);
 	}
@@ -54,21 +70,21 @@ public class TestCase {
 	public void testValidateCache() {	
 		HbmLint analyzer = new HbmLint(new Detector[] { new BadCachingDetector() });		
 		analyzer.analyze(metadataDescriptor.createMetadata());
-		Assert.assertEquals(1,analyzer.getResults().size());		
+		assertEquals(1,analyzer.getResults().size());		
 	}
 
 	@Test
 	public void testValidateIdentifier() {		
 		HbmLint analyzer = new HbmLint(new Detector[] { new ShadowedIdentifierDetector() });		
 		analyzer.analyze(metadataDescriptor.createMetadata());
-		Assert.assertEquals(1,analyzer.getResults().size());
+		assertEquals(1,analyzer.getResults().size());
 	}
 	
 	@Test
 	public void testBytecodeRestrictions() {		
 		HbmLint analyzer = new HbmLint(new Detector[] { new InstrumentationDetector() });		
 		analyzer.analyze(metadataDescriptor.createMetadata());
-		Assert.assertEquals(analyzer.getResults().toString(), 2,analyzer.getResults().size());
+		assertEquals(2,analyzer.getResults().size(), analyzer.getResults().toString());
 	}
 	
 }
--- test/common/src/main/java/org/hibernate/tool/test/db/JupiterCommonTestSuite.java
+++ test/common/src/main/java/org/hibernate/tool/test/db/JupiterCommonTestSuite.java
@@ -34,5 +34,6 @@ public class JupiterCommonTestSuite {
 	@Nested public class IncrementalSchemaReading extends org.hibernate.tool.hbm2x.IncrementalSchemaReading.TestCase {}
 	@Nested public class JdbcHbm2JavaEjb3 extends org.hibernate.tool.hbm2x.JdbcHbm2JavaEjb3.TestCase {}
 	@Nested public class QueryExporterTest extends org.hibernate.tool.hbm2x.query.QueryExporterTest.TestCase {}
+	@Nested public class HbmLintTest extends org.hibernate.tool.hbmlint.HbmLintTest.TestCase {}
 
 }
