64f11175d5798752115386dc220572f56d1c4b15
Koen Aers <koen.aers@gmail.com>
2021-02-10 10:08:55 +0100

    HBX-2124: Replace JUnit4 tests with JUnit Jupiter tests - Use the JUnit Jupiter test suite in the 'hsql' module
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/hsql/src/test/java/org/hibernate/tool/test/db/hsql/TestSuite.java
+++ test/hsql/src/test/java/org/hibernate/tool/test/db/hsql/TestSuite.java
@@ -1,11 +1,11 @@
 package org.hibernate.tool.test.db.hsql;
 
-import org.hibernate.tools.test.util.DbSuite;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite.SuiteClasses;
+import org.hibernate.tool.test.db.JupiterCommonTestSuite;
+import org.junit.jupiter.api.Nested;
 
-@RunWith(DbSuite.class)
-@SuiteClasses({ 
-	org.hibernate.tool.test.db.CommonTestSuite.class 
-})
-public class TestSuite {}
+public class TestSuite {
+	
+	@Nested
+	public class H2TestSuite extends JupiterCommonTestSuite {}
+
+}
