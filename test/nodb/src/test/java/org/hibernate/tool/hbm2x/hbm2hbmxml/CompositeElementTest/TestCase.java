3da1fe0b34a01c1ad42c67bd984174cc943bcc1e
Koen Aers <koen.aers@gmail.com>
2021-06-28 21:33:18 +0200

    HBX-2184: Replace JUnit4 tests with JUnit Jupiter tests - Refactor test class 'org.hibernate.tool.hbm2x.hbm2hbmxml.CompositeElementTest.TestCase'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/nodb/src/test/java/org/hibernate/tool/hbm2x/hbm2hbmxml/CompositeElementTest/TestCase.java
+++ test/nodb/src/test/java/org/hibernate/tool/hbm2x/hbm2hbmxml/CompositeElementTest/TestCase.java
@@ -1,20 +1,27 @@
 /*
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
+ * Hibernate Tools, Tooling for your Hibernate Projects
+ * 
+ * Copyright 2004-2021 Red Hat, Inc.
  *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * Licensed under the GNU Lesser General Public License (LGPL), 
+ * version 2.1 or later (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may read the licence in the 'lgpl.txt' file in the root folder of 
+ * project or obtain a copy at
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *     http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package org.hibernate.tool.hbm2x.hbm2hbmxml.CompositeElementTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -33,11 +40,9 @@ import org.hibernate.tool.hbm2x.Exporter;
 import org.hibernate.tool.hbm2x.HibernateMappingExporter;
 import org.hibernate.tools.test.util.HibernateUtil;
 import org.hibernate.tools.test.util.JUnitUtil;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -53,35 +58,35 @@ public class TestCase {
 			"Glarch.hbm.xml"
 	};
 	
-	@Rule
-	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+	@TempDir
+	public File outputFolder = new File("output");
 	
-	private File outputDir = null;
+	private File srcDir = null;
 	private File resourcesDir = null;
 	
 	private Exporter hbmexporter = null;
 
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
-		outputDir = new File(temporaryFolder.getRoot(), "output");
-		outputDir.mkdir();
-		resourcesDir = new File(temporaryFolder.getRoot(), "resources");
+		srcDir = new File(outputFolder, "output");
+		srcDir.mkdir();
+		resourcesDir = new File(outputFolder, "resources");
 		resourcesDir.mkdir();
 		MetadataDescriptor metadataDescriptor = HibernateUtil
 				.initializeMetadataDescriptor(this, HBM_XML_FILES, resourcesDir);
 		hbmexporter = new HibernateMappingExporter();
 		hbmexporter.setMetadataDescriptor(metadataDescriptor);
-		hbmexporter.setOutputDirectory(outputDir);
+		hbmexporter.setOutputDirectory(srcDir);
 		hbmexporter.start();
 	}
 
 	@Test
 	public void testAllFilesExistence() {
 		JUnitUtil.assertIsNonEmptyFile(new File(
-				outputDir,  
+				srcDir,  
 				"org/hibernate/tool/hbm2x/hbm2hbmxml/CompositeElementTest/Fee.hbm.xml") );
 		JUnitUtil.assertIsNonEmptyFile(new File(
-				outputDir,  
+				srcDir,  
 				"org/hibernate/tool/hbm2x/hbm2hbmxml/CompositeElementTest/Glarch.hbm.xml") );
 	}
 
@@ -89,22 +94,22 @@ public class TestCase {
 	public void testReadable() {
         ArrayList<File> files = new ArrayList<File>(4); 
         files.add(new File(
-        		outputDir, 
+        		srcDir, 
         		"org/hibernate/tool/hbm2x/hbm2hbmxml/CompositeElementTest/Fee.hbm.xml"));
         files.add(new File(
-        		outputDir, 
+        		srcDir, 
         		"org/hibernate/tool/hbm2x/hbm2hbmxml/CompositeElementTest/Glarch.hbm.xml"));
 		Properties properties = new Properties();
 		properties.setProperty(AvailableSettings.DIALECT, HibernateUtil.Dialect.class.getName());
 		MetadataDescriptor metadataDescriptor = MetadataDescriptorFactory
 				.createNativeDescriptor(null, files.toArray(new File[2]), properties);
-        Assert.assertNotNull(metadataDescriptor.createMetadata());
+        assertNotNull(metadataDescriptor.createMetadata());
     }
 
 	@Test
 	public void testCompositeElementNode() throws Exception {
 		File outputXml = new File(
-        		outputDir, 
+        		srcDir, 
         		"org/hibernate/tool/hbm2x/hbm2hbmxml/CompositeElementTest/Glarch.hbm.xml");
         JUnitUtil.assertIsNonEmptyFile(outputXml);
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -116,7 +121,7 @@ public class TestCase {
 				.evaluate(document, XPathConstants.NODESET);
 		Element secondList = (Element)nodeList.item(1);
 		NodeList compositeElementList = secondList.getElementsByTagName("composite-element");
-		Assert.assertEquals("Expected to get one composite-element element", 1, compositeElementList.getLength());
+		assertEquals(1, compositeElementList.getLength(), "Expected to get one composite-element element");
 		Element compositeElement = (Element)compositeElementList.item(0);
 		NodeList compositeElementChildNodes = compositeElement.getChildNodes();
 		int amountOfProperties = 0;
@@ -124,17 +129,17 @@ public class TestCase {
 			Node node = compositeElementChildNodes.item(i);
 			if ("property".equals(node.getNodeName())) amountOfProperties++;
 		}
-		Assert.assertEquals("Expected to get two property element", 2, amountOfProperties);
+		assertEquals(2, amountOfProperties, "Expected to get two property element");
 		NodeList manyToOneList = secondList.getElementsByTagName("many-to-one");
-		Assert.assertEquals(1, manyToOneList.getLength());
+		assertEquals(1, manyToOneList.getLength());
 		Element manyToOneElement = (Element)manyToOneList.item(0);
-		Assert.assertEquals("fee", manyToOneElement.getAttribute("name"));
-		Assert.assertEquals("all", manyToOneElement.getAttribute("cascade"));
+		assertEquals("fee", manyToOneElement.getAttribute("name"));
+		assertEquals("all", manyToOneElement.getAttribute("cascade"));
 		NodeList nestedCompositeElementList = compositeElement.getElementsByTagName("nested-composite-element");
-		Assert.assertEquals(1, nestedCompositeElementList.getLength());
+		assertEquals(1, nestedCompositeElementList.getLength());
 		Element nestedCompositeElement = (Element)nestedCompositeElementList.item(0);
-		Assert.assertEquals("subcomponent", nestedCompositeElement.getAttribute("name"));
-		Assert.assertEquals(
+		assertEquals("subcomponent", nestedCompositeElement.getAttribute("name"));
+		assertEquals(
 				"org.hibernate.tool.hbm2x.hbm2hbmxml.CompositeElementTest.FooComponent", 
 				nestedCompositeElement.getAttribute("class"));
 	}
