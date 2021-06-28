296bbfef69c45188c6b4d9eff331b18b76d8e8df
Koen Aers <koen.aers@gmail.com>
2021-06-28 12:36:58 +0200

    HBX-2184: Replace JUnit4 tests with JUnit Jupiter tests - Refactor test class 'org.hibernate.tool.hbm2x.HashcodeEqualsTest.TestCase'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/nodb/src/test/java/org/hibernate/tool/hbm2x/HashcodeEqualsTest/TestCase.java
+++ test/nodb/src/test/java/org/hibernate/tool/hbm2x/HashcodeEqualsTest/TestCase.java
@@ -1,8 +1,27 @@
 /*
- * Created on 2004-12-01
+ * Hibernate Tools, Tooling for your Hibernate Projects
+ * 
+ * Copyright 2004-2021 Red Hat, Inc.
  *
+ * Licensed under the GNU Lesser General Public License (LGPL), 
+ * version 2.1 or later (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may read the licence in the 'lgpl.txt' file in the root folder of 
+ * project or obtain a copy at
+ *
+ *     http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package org.hibernate.tool.hbm2x.HashcodeEqualsTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.io.File;
 
@@ -14,11 +33,9 @@ import org.hibernate.tools.test.util.FileUtil;
 import org.hibernate.tools.test.util.HibernateUtil;
 import org.hibernate.tools.test.util.JUnitUtil;
 import org.hibernate.tools.test.util.JavaUtil;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 /**
  * @author max
@@ -30,25 +47,25 @@ public class TestCase {
 			"HashEquals.hbm.xml"				
 	};
 	
-	@Rule
-	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+	@TempDir
+	public File outputFolder = new File("output");
 
-	private File outputDir = null;
+	private File srcDir = null;
 	private File resourcesDir = null;
 	private ArtifactCollector artifactCollector = null;
 	private MetadataDescriptor metadataDescriptor = null;
 	
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
-		outputDir = new File(temporaryFolder.getRoot(), "output");
-		outputDir.mkdir();
-		resourcesDir = new File(temporaryFolder.getRoot(), "resources");
+		srcDir = new File(outputFolder, "output");
+		srcDir.mkdir();
+		resourcesDir = new File(outputFolder, "resources");
 		resourcesDir.mkdir();
 		metadataDescriptor = HibernateUtil
 				.initializeMetadataDescriptor(this, HBM_XML_FILES, resourcesDir);
 		Exporter exporter = new POJOExporter();
 		exporter.setMetadataDescriptor(metadataDescriptor);
-		exporter.setOutputDirectory(outputDir);
+		exporter.setOutputDirectory(srcDir);
 		artifactCollector = new ArtifactCollector();
 		exporter.setArtifactCollector(artifactCollector);
 		exporter.start();
@@ -58,7 +75,7 @@ public class TestCase {
 	public void testJDK5FailureExpectedOnJDK4() {
 		POJOExporter exporter = new POJOExporter();
 		exporter.setMetadataDescriptor(metadataDescriptor);
-		exporter.setOutputDirectory(outputDir);
+		exporter.setOutputDirectory(srcDir);
 		exporter.getProperties().setProperty("jdk5", "true");
 		artifactCollector = new ArtifactCollector();
 		exporter.setArtifactCollector(artifactCollector);
@@ -71,17 +88,17 @@ public class TestCase {
 	@Test
 	public void testFileExistence() {
 		JUnitUtil.assertIsNonEmptyFile(new File(
-				outputDir, "org/hibernate/tool/hbm2x/HashEquals.java"));
+				srcDir, "org/hibernate/tool/hbm2x/HashEquals.java"));
 		JUnitUtil.assertIsNonEmptyFile(new File(
-				outputDir, "org/hibernate/tool/hbm2x/Address.java"));
-		Assert.assertEquals(2, artifactCollector.getFileCount("java"));
+				srcDir, "org/hibernate/tool/hbm2x/Address.java"));
+		assertEquals(2, artifactCollector.getFileCount("java"));
 	}
 	
 	@Test
 	public void testCompilable() {
-		File compiled = new File(temporaryFolder.getRoot(), "compiled");
+		File compiled = new File(outputFolder, "compiled");
 		compiled.mkdir();
-		JavaUtil.compile(outputDir, compiled);
+		JavaUtil.compile(srcDir, compiled);
 		JUnitUtil.assertIsNonEmptyFile(new File(
 				compiled, "org/hibernate/tool/hbm2x/HashEquals.class"));
 		JUnitUtil.assertIsNonEmptyFile(new File(
@@ -90,17 +107,17 @@ public class TestCase {
 
 	@Test
 	public void testNoVelocityLeftOvers() {
-		Assert.assertNull(FileUtil
+		assertNull(FileUtil
 				.findFirstString(
 						"$",
 						new File(
-								outputDir, 
+								srcDir, 
 								"org/hibernate/tool/hbm2x/HashEquals.java")));
-        Assert.assertNull(FileUtil
+       assertNull(FileUtil
         		.findFirstString(
         				"$",
         				new File(
-        						outputDir, 
+        						srcDir, 
         						"org/hibernate/tool/hbm2x/Address.java")));
 	}
 
