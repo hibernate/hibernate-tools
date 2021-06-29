5fd7a9b0bc4df5387d89e1ad80428f01759e6e97
Koen Aers <koen.aers@gmail.com>
2021-06-29 18:15:04 +0200

    HBX-2184: Replace JUnit4 tests with JUnit Jupiter tests - Refactor test class 'org.hibernate.tool.hbm2x.Hbm2JavaEqualsTest.TestCase'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/nodb/src/test/java/org/hibernate/tool/hbm2x/Hbm2JavaEqualsTest/TestCase.java
+++ test/nodb/src/test/java/org/hibernate/tool/hbm2x/Hbm2JavaEqualsTest/TestCase.java
@@ -1,4 +1,27 @@
+/*
+ * Hibernate Tools, Tooling for your Hibernate Projects
+ * 
+ * Copyright 2004-2021 Red Hat, Inc.
+ *
+ * Licensed under the GNU Lesser General Public License (LGPL), 
+ * version 2.1 or later (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may read the licence in the 'lgpl.txt' file in the root folder of 
+ * project or obtain a copy at
+ *
+ *     http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.hibernate.tool.hbm2x.Hbm2JavaEqualsTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -16,11 +39,9 @@ import org.hibernate.tool.hbm2x.Exporter;
 import org.hibernate.tool.hbm2x.POJOExporter;
 import org.hibernate.tools.test.util.HibernateUtil;
 import org.hibernate.tools.test.util.JavaUtil;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 public class TestCase {
 	
@@ -44,18 +65,18 @@ public class TestCase {
 			"  int getId();                                  "+ System.lineSeparator() +
 	        "}                                               ";
 	
-	@Rule
-	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+	@TempDir
+	public File outputFolder = new File("output");
 	
-	private File outputDir = null;
+	private File srcDir = null;
 	private File resourcesDir = null;
 
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
 		// create output folder
-		outputDir = new File(temporaryFolder.getRoot(), "output");
-		outputDir.mkdir();
-		resourcesDir = new File(temporaryFolder.getRoot(), "resources");
+		srcDir = new File(outputFolder, "output");
+		srcDir.mkdir();
+		resourcesDir = new File(outputFolder, "resources");
 		resourcesDir.mkdir();
 		// export class ProxiedTestEntity.java and UnProxiedTestEntity
 		File hbmXml = new File(resourcesDir, "testEntity.hbm.xml");
@@ -69,21 +90,21 @@ public class TestCase {
 				.createNativeDescriptor(null, new File[] { hbmXml }, properties);
 		Exporter exporter = new POJOExporter();
 		exporter.setMetadataDescriptor(metadataDescriptor);
-		exporter.setOutputDirectory(outputDir);
+		exporter.setOutputDirectory(srcDir);
 		exporter.start();
 		// copy interface EntityProxy.java
-		File file = new File(outputDir, "org/hibernate/tool/hbm2x/Hbm2JavaEquals/TestEntityProxy.java");
+		File file = new File(srcDir, "org/hibernate/tool/hbm2x/Hbm2JavaEquals/TestEntityProxy.java");
 		FileWriter writer = new FileWriter(file);
 		writer.write(TEST_ENTITY_PROXY_JAVA);
 		writer.close();
 		// compile the source files
-		JavaUtil.compile(outputDir);
+		JavaUtil.compile(srcDir);
 	}	
 	
 	@Test
 	public void testEqualsWithoutProxy() throws Exception {
 		// load the entity class and lookup the setId method
-        URL[] urls = new URL[] { outputDir.toURI().toURL() };
+        URL[] urls = new URL[] { srcDir.toURI().toURL() };
         ClassLoader oldLoader = Thread.currentThread().getContextClassLoader();
 		URLClassLoader ucl = new URLClassLoader(urls, oldLoader );
         Class<?> entityClass = ucl.loadClass("org.hibernate.tool.hbm2x.Hbm2JavaEquals.UnProxiedTestEntity");
@@ -95,20 +116,20 @@ public class TestCase {
         // - 'false' when comparing against an object of a different class
         Object firstEntity = entityClass.newInstance();
         setId.invoke(firstEntity, new Object[] { Integer.MAX_VALUE });
-        Assert.assertTrue(firstEntity.equals(firstEntity));
-        Assert.assertFalse(firstEntity.equals(null));
-        Assert.assertFalse(firstEntity.equals(new Object()));
+        assertTrue(firstEntity.equals(firstEntity));
+        assertFalse(firstEntity.equals(null));
+        assertFalse(firstEntity.equals(new Object()));
 
         // create a second entity and check the 'normal behavior
         // - 'true' if the id property is the same
         // - 'false' if the id property is different
         Object secondEntity = entityClass.newInstance();
         setId.invoke(secondEntity, new Object[] { Integer.MAX_VALUE });
-        Assert.assertTrue(firstEntity.equals(secondEntity));
-        Assert.assertTrue(secondEntity.equals(firstEntity));
+        assertTrue(firstEntity.equals(secondEntity));
+        assertTrue(secondEntity.equals(firstEntity));
         setId.invoke(secondEntity, new Object[] { Integer.MIN_VALUE });
-        Assert.assertFalse(firstEntity.equals(secondEntity));
-        Assert.assertFalse(secondEntity.equals(firstEntity));
+        assertFalse(firstEntity.equals(secondEntity));
+        assertFalse(secondEntity.equals(firstEntity));
 
         ucl.close();
 	}
@@ -117,7 +138,7 @@ public class TestCase {
 	public void testEqualsWithProxy() throws Exception {
 
 		// load the entity and proxy classes, lookup the setId method and create a proxy object
-        URL[] urls = new URL[] { outputDir.toURI().toURL() };
+        URL[] urls = new URL[] { srcDir.toURI().toURL() };
         ClassLoader oldLoader = Thread.currentThread().getContextClassLoader();
 		URLClassLoader ucl = new URLClassLoader(urls, oldLoader );
         Class<?> entityClass = ucl.loadClass("org.hibernate.tool.hbm2x.Hbm2JavaEquals.ProxiedTestEntity");
@@ -135,28 +156,28 @@ public class TestCase {
         // - 'false' when comparing against an object of a different class (that is not the proxy class)
         Object firstEntity = entityClass.newInstance();
         setId.invoke(firstEntity, new Object[] { Integer.MAX_VALUE });
-        Assert.assertTrue(firstEntity.equals(firstEntity));
-        Assert.assertFalse(firstEntity.equals(null));
-        Assert.assertFalse(firstEntity.equals(new Object()));
+        assertTrue(firstEntity.equals(firstEntity));
+        assertFalse(firstEntity.equals(null));
+        assertFalse(firstEntity.equals(new Object()));
 
         // create a second proxied entity and check the 'normal behavior
         // - 'true' if the id property is the same
         // - 'false' if the id property is different
         Object secondEntity = entityClass.newInstance();
         setId.invoke(secondEntity, new Object[] { Integer.MAX_VALUE });
-        Assert.assertTrue(firstEntity.equals(secondEntity));
-        Assert.assertTrue(secondEntity.equals(firstEntity));
+        assertTrue(firstEntity.equals(secondEntity));
+        assertTrue(secondEntity.equals(firstEntity));
         setId.invoke(secondEntity, new Object[] { Integer.MIN_VALUE });
-        Assert.assertFalse(firstEntity.equals(secondEntity));
-        Assert.assertFalse(secondEntity.equals(firstEntity));
+        assertFalse(firstEntity.equals(secondEntity));
+        assertFalse(secondEntity.equals(firstEntity));
 
         // compare both proxied entities with the proxy
         handler.id = Integer.MAX_VALUE;
-        Assert.assertTrue(firstEntity.equals(testEntityProxy));
-        Assert.assertFalse(secondEntity.equals(testEntityProxy));        
+        assertTrue(firstEntity.equals(testEntityProxy));
+        assertFalse(secondEntity.equals(testEntityProxy));        
         handler.id = Integer.MIN_VALUE;
-        Assert.assertFalse(firstEntity.equals(testEntityProxy));
-        Assert.assertTrue(secondEntity.equals(testEntityProxy));
+        assertFalse(firstEntity.equals(testEntityProxy));
+        assertTrue(secondEntity.equals(testEntityProxy));
         
         ucl.close();
 	}
