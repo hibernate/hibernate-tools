615f89ec26f3bdba72f1655aac6216c6be7fe948
Koen Aers <koen.aers@gmail.com>
2021-07-02 16:31:17 +0200

    HBX-2184: Replace JUnit4 tests with JUnit Jupiter tests - Refactor test class 'org.hibernate.tools.test.util.JdbcUtilTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/utils/src/test/java/org/hibernate/tools/test/util/JdbcUtilTest.java
+++ test/utils/src/test/java/org/hibernate/tools/test/util/JdbcUtilTest.java
@@ -1,4 +1,29 @@
+/*
+ * Hibernate Tools, Tooling for your Hibernate Projects
+ * 
+ * Copyright 2017-2020 Red Hat, Inc.
+ *
+ * Licensed under the GNU Lesser General Public License (LGPL), 
+ * version 2.1 or later (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may read the licence in the 'lgpl.txt' file in the root folder of 
+ * project or obtain a copy at
+ *
+ *     http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.hibernate.tools.test.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -13,26 +38,24 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.util.Properties;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 public class JdbcUtilTest {
 	
-	@Rule
-	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+	@TempDir
+	public File outputFolder = new File("output");
 	
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
 		clearConnectionTable();
 		createHibernateProperties("sa", "", "jdbc:h2:mem:test");
 		setUpClassLoader();
 	}
 	
-	@After
+	@AfterEach
 	public void tearDown() throws Exception {
 		clearConnectionTable();
 		restoreClassLoader();
@@ -41,17 +64,17 @@ public class JdbcUtilTest {
 	@Test
 	public void testGetConnectionProperties() throws Exception {
 		Properties properties = JdbcUtil.getConnectionProperties();
-		Assert.assertEquals("jdbc:h2:mem:test", properties.get("url"));
-		Assert.assertEquals("sa", properties.get("user"));
-		Assert.assertEquals("", properties.get("password"));
+		assertEquals("jdbc:h2:mem:test", properties.get("url"));
+		assertEquals("sa", properties.get("user"));
+		assertEquals("", properties.get("password"));
 	}
 	
 	@Test
 	public void testEstablishJdbcConnection() throws Exception {
-		Assert.assertNull(JdbcUtil.CONNECTION_TABLE.get(this));
+		assertNull(JdbcUtil.CONNECTION_TABLE.get(this));
 		JdbcUtil.establishJdbcConnection(this);
 		Connection connection = JdbcUtil.CONNECTION_TABLE.get(this);
-		Assert.assertFalse(connection.isClosed());
+		assertFalse(connection.isClosed());
 	}
 	
 	@Test
@@ -59,11 +82,11 @@ public class JdbcUtilTest {
 		Connection connection = 
 				DriverManager.getConnection("jdbc:h2:mem:test;USER=sa");
 		JdbcUtil.CONNECTION_TABLE.put(this, connection);
-		Assert.assertNotNull(JdbcUtil.CONNECTION_TABLE.get(this));
-		Assert.assertFalse(connection.isClosed());
+		assertNotNull(JdbcUtil.CONNECTION_TABLE.get(this));
+		assertFalse(connection.isClosed());
 		JdbcUtil.releaseJdbcConnection(this);
-		Assert.assertNull(JdbcUtil.CONNECTION_TABLE.get(this));
-		Assert.assertTrue(connection.isClosed());
+		assertNull(JdbcUtil.CONNECTION_TABLE.get(this));
+		assertTrue(connection.isClosed());
 	}
 	
 	@Test
@@ -74,7 +97,7 @@ public class JdbcUtilTest {
 		ResultSet resultSet = connection
 				.getMetaData()
 				.getTables(null, null, "FOO", null);
-		Assert.assertFalse(resultSet.next());
+		assertFalse(resultSet.next());
 		String[] sqls = new String[] {
 				"CREATE TABLE FOO (BAR INT)"
 		};
@@ -82,7 +105,7 @@ public class JdbcUtilTest {
 		resultSet = connection
 				.getMetaData()
 				.getTables(null, null, "FOO", null);
-		Assert.assertTrue(resultSet.next());
+		assertTrue(resultSet.next());
 	}
 	
 	@Test
@@ -96,19 +119,19 @@ public class JdbcUtilTest {
 				connectionInvocationHandler );
 		JdbcUtil.CONNECTION_TABLE.put(this, connection);
 		metaDataInvocationHandler.whichCase = "MIXED";
-		Assert.assertEquals("Foo", JdbcUtil.toIdentifier(this, "Foo"));
+		assertEquals("Foo", JdbcUtil.toIdentifier(this, "Foo"));
 		metaDataInvocationHandler.whichCase = "UPPER";
-		Assert.assertEquals("FOO", JdbcUtil.toIdentifier(this, "Foo"));
+		assertEquals("FOO", JdbcUtil.toIdentifier(this, "Foo"));
 		metaDataInvocationHandler.whichCase = "LOWER";
-		Assert.assertEquals("foo", JdbcUtil.toIdentifier(this, "Foo"));
+		assertEquals("foo", JdbcUtil.toIdentifier(this, "Foo"));
 	}
 	
 	@Test
 	public void testIsDatabaseOnline() throws Exception {
-			Assert.assertTrue(JdbcUtil.isDatabaseOnline());
-			new File(temporaryFolder.getRoot(), "hibernate.properties").delete();
+			assertTrue(JdbcUtil.isDatabaseOnline());
+			new File(outputFolder, "hibernate.properties").delete();
 			createHibernateProperties("foo", "bar", "jdbc:sqlserver://org.foo.bar:1433");
-			Assert.assertFalse(JdbcUtil.isDatabaseOnline());
+			assertFalse(JdbcUtil.isDatabaseOnline());
 	}
 	
 	private void clearConnectionTable() throws Exception {
@@ -127,7 +150,6 @@ public class JdbcUtilTest {
 		properties.put("hibernate.connection.username", user);
 		properties.put("hibernate.connection.password", password);
 		properties.put("hibernate.connection.url", url);
-		File outputFolder = temporaryFolder.getRoot(); 
 		File propertiesFile = new File(outputFolder, "hibernate.properties");
 		FileWriter writer = new FileWriter(propertiesFile);
 		properties.store(writer, null);
@@ -138,7 +160,7 @@ public class JdbcUtilTest {
 		ClassLoader currentClassLoader = Thread.currentThread().getContextClassLoader();
 		Thread.currentThread().setContextClassLoader(
 				new URLClassLoader(
-					new URL[] { temporaryFolder.getRoot().toURI().toURL() }, 
+					new URL[] { outputFolder.toURI().toURL() }, 
 					currentClassLoader));
 	}
 	
