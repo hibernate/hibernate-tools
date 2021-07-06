09fd6170077af4aca6f8b602b9a5f2a7487ba575
Koen Aers <koen.aers@gmail.com>
2021-07-06 21:42:55 +0200

    HBX-2184: Replace JUnit4 tests with JUnit Jupiter tests - Refactor test class 'org.hibernate.tool.jdbc2cfg.AutoQuote.TestCase'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/common/src/main/java/org/hibernate/tool/jdbc2cfg/AutoQuote/TestCase.java
+++ test/common/src/main/java/org/hibernate/tool/jdbc2cfg/AutoQuote/TestCase.java
@@ -1,8 +1,27 @@
 /*
- * Created on 2004-11-24
+ * Hibernate Tools, Tooling for your Hibernate Projects
+ * 
+ * Copyright 2004-2021 Red Hat, Inc.
  *
+ * Licensed under the GNU Lesser General Public License (LGPL), 
+ * version 2.1 or later (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may read the licence in the 'lgpl.txt' file in the root folder of 
+ * project or obtain a copy at
+ *
+ *     http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.hibernate.tool.jdbc2cfg.AutoQuote;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.hibernate.boot.Metadata;
 import org.hibernate.mapping.PersistentClass;
@@ -11,10 +30,9 @@ import org.hibernate.mapping.Table;
 import org.hibernate.tool.api.metadata.MetadataDescriptorFactory;
 import org.hibernate.tools.test.util.HibernateUtil;
 import org.hibernate.tools.test.util.JdbcUtil;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author max
@@ -24,7 +42,7 @@ public class TestCase {
 
 	private Metadata metadata = null;
 
-	@Before
+	@BeforeEach
 	public void setUp() {
 		JdbcUtil.createDatabase(this);
 		metadata = MetadataDescriptorFactory
@@ -32,7 +50,7 @@ public class TestCase {
 				.createMetadata();
 	}
 
-	@After
+	@AfterEach
 	public void tearDown() {
 		JdbcUtil.dropDatabase(this);
 	}
@@ -40,13 +58,13 @@ public class TestCase {
 	@Test
 	public void testForQuotes() {
 		Table table = HibernateUtil.getTable(metadata, "us-ers");
-		Assert.assertNotNull(table);
-		Assert.assertTrue(table.isQuoted());		
-		Assert.assertEquals(2, table.getColumnSpan());		
+		assertNotNull(table);
+		assertTrue(table.isQuoted());		
+		assertEquals(2, table.getColumnSpan());		
 		PersistentClass classMapping = metadata.getEntityBinding("Worklogs");
-		Assert.assertNotNull(classMapping);
+		assertNotNull(classMapping);
 		Property property = classMapping.getProperty("usErs");
-		Assert.assertNotNull(property);	
+		assertNotNull(property);	
 	}
 	
 }
