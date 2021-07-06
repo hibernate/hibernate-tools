6a624549b1559cdf02cf4faf8be017a6a36420b7
Koen Aers <koen.aers@gmail.com>
2021-07-07 01:42:56 +0200

    HBX-2184: Replace JUnit4 tests with JUnit Jupiter tests - Refactor test class 'org.hibernate.tool.jdbc2cfg.Identity.TestCase'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/common/src/main/java/org/hibernate/tool/jdbc2cfg/Identity/TestCase.java
+++ test/common/src/main/java/org/hibernate/tool/jdbc2cfg/Identity/TestCase.java
@@ -1,8 +1,26 @@
 /*
- * Created on 2004-11-23
+ * Hibernate Tools, Tooling for your Hibernate Projects
+ * 
+ * Copyright 2004-2021 Red Hat, Inc.
  *
+ * Licensed under the GNU Lesser General Public License (LGPL), 
+ * version 2.1 or later (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may read the licence in the 'lgpl.txt' file in the root folder of 
+ * project or obtain a copy at
+ *
+ *     http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.hibernate.tool.jdbc2cfg.Identity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.sql.SQLException;
 
@@ -11,10 +29,9 @@ import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.SimpleValue;
 import org.hibernate.tool.api.metadata.MetadataDescriptorFactory;
 import org.hibernate.tools.test.util.JdbcUtil;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author max
@@ -24,7 +41,7 @@ public class TestCase {
 
 	private Metadata metadata = null;
 
-	@Before
+	@BeforeEach
 	public void setUp() {
 		JdbcUtil.createDatabase(this);
 		metadata = MetadataDescriptorFactory
@@ -32,7 +49,7 @@ public class TestCase {
 				.createMetadata();
 	}
 
-	@After
+	@AfterEach
 	public void tearDown() {
 		JdbcUtil.dropDatabase(this);
 	}
@@ -41,8 +58,8 @@ public class TestCase {
 	public void testIdentity() throws SQLException {
 
 		PersistentClass classMapping = metadata.getEntityBinding("Autoinc");
-		Assert.assertNotNull(classMapping);
-		Assert.assertEquals(
+		assertNotNull(classMapping);
+		assertEquals(
 				"identity", 
 				((SimpleValue)classMapping
 						.getIdentifierProperty()
@@ -50,7 +67,7 @@ public class TestCase {
 					.getIdentifierGeneratorStrategy());
 		
 		classMapping = metadata.getEntityBinding("Noautoinc");
-		Assert.assertEquals(
+		assertEquals(
 				"assigned", 
 				((SimpleValue)classMapping
 						.getIdentifierProperty()
