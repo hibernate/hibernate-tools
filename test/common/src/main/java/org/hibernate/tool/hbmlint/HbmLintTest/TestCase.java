e0aea1679e0eb83dcc46dff68d242649f661bb21
Koen Aers <koen.aers@gmail.com>
2021-07-06 21:07:43 +0200

    HBX-2184: Replace JUnit4 tests with JUnit Jupiter tests - Refactor test class 'org.hibernate.tool.hbmlint.HbmLintTest.TestCase'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/common/src/main/java/org/hibernate/tool/hbmlint/HbmLintTest/TestCase.java
+++ test/common/src/main/java/org/hibernate/tool/hbmlint/HbmLintTest/TestCase.java
@@ -1,4 +1,25 @@
+/*
+ * Hibernate Tools, Tooling for your Hibernate Projects
+ * 
+ * Copyright 2004-2021 Red Hat, Inc.
+ *
+ * Licensed under the GNU Lesser General Public License (LGPL), 
+ * version 2.1 or later (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may read the licence in the 'lgpl.txt' file in the root folder of 
+ * project or obtain a copy at
+ *
+ *     http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.hibernate.tool.hbmlint.HbmLintTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
 
@@ -10,11 +31,9 @@ import org.hibernate.tool.hbmlint.detector.BadCachingDetector;
 import org.hibernate.tool.hbmlint.detector.InstrumentationDetector;
 import org.hibernate.tool.hbmlint.detector.ShadowedIdentifierDetector;
 import org.hibernate.tools.test.util.HibernateUtil;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 public class TestCase {
 
@@ -24,19 +43,19 @@ public class TestCase {
 			"BrokenLazy.hbm.xml"
 	};
 	
-	@Rule
-	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+	@TempDir
+	public File temporaryFolder = new File("temp");
 	
 	private File outputDir = null;
 	private File resourcesDir = null;
 	
 	private MetadataDescriptor metadataDescriptor = null;
 	
-	@Before
+	@BeforeEach
 	public void setUp() {
-		outputDir = new File(temporaryFolder.getRoot(), "output");
+		outputDir = new File(temporaryFolder, "output");
 		outputDir.mkdir();
-		resourcesDir = new File(temporaryFolder.getRoot(), "resources");
+		resourcesDir = new File(temporaryFolder, "resources");
 		resourcesDir.mkdir();
 		metadataDescriptor = HibernateUtil.initializeMetadataDescriptor(this, HBM_XML_FILES, resourcesDir);
 	}
@@ -53,21 +72,21 @@ public class TestCase {
 	public void testValidateCache() {	
 		HbmLint analyzer = new HbmLint(new Detector[] { new BadCachingDetector() });		
 		analyzer.analyze(metadataDescriptor.createMetadata());
-		Assert.assertEquals(1,analyzer.getResults().size());		
+		assertEquals(1,analyzer.getResults().size());		
 	}
 
 	@Test
 	public void testValidateIdentifier() {		
 		HbmLint analyzer = new HbmLint(new Detector[] { new ShadowedIdentifierDetector() });		
 		analyzer.analyze(metadataDescriptor.createMetadata());
-		Assert.assertEquals(1,analyzer.getResults().size());
+		assertEquals(1,analyzer.getResults().size());
 	}
 	
 	@Test
 	public void testBytecodeRestrictions() {		
 		HbmLint analyzer = new HbmLint(new Detector[] { new InstrumentationDetector() });		
 		analyzer.analyze(metadataDescriptor.createMetadata());
-		Assert.assertEquals(analyzer.getResults().toString(), 2,analyzer.getResults().size());
+		assertEquals(2,analyzer.getResults().size(), analyzer.getResults().toString());
 	}
 	
 }
