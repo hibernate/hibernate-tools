05c405b81b856124cdf2eb729ebf149adb619aa1
Koen Aers <koen.aers@gmail.com>
2021-10-01 09:18:11 +0200

    HBX-2225: Update version identifier to 5.6.0.CR1
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- main/src/main/java/org/hibernate/tool/Version.java
+++ main/src/main/java/org/hibernate/tool/Version.java
@@ -4,7 +4,7 @@ import java.util.logging.Logger;
 
 final public class Version {
 
-	public static final String VERSION = "5.6.0-SNAPSHOT";
+	public static final String VERSION = "5.6.0.CR1";
 	
 	private static final Version instance = new Version();
 	
