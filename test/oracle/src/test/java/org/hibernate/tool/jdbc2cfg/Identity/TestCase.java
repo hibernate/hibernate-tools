7b6f9bb637ab0157f163260593039963d5435fad
Koen Aers <koen.aers@gmail.com>
2021-10-12 22:08:15 +0200

    HBX-2184: Replace JUnit4 tests with JUnit Jupiter tests - Cleanup of 'org.hibernate.tool.jdbc2cfg.Identity.TestCase'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/oracle/src/test/java/org/hibernate/tool/jdbc2cfg/Identity/TestCase.java
+++ test/oracle/src/test/java/org/hibernate/tool/jdbc2cfg/Identity/TestCase.java
@@ -31,7 +31,6 @@ import org.hibernate.tool.api.metadata.MetadataDescriptorFactory;
 import org.hibernate.tools.test.util.JdbcUtil;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
