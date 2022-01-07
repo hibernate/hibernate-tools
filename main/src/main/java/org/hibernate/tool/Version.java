88cd791301a977092b6eb6c8b00e36b3b2ae6a90
Koen Aers <koen.aers@gmail.com>
2022-01-07 14:11:21 +0100

    HBX-2268: Update version identifier to 5.5.9.Final
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- main/src/main/java/org/hibernate/tool/Version.java
+++ main/src/main/java/org/hibernate/tool/Version.java
@@ -4,7 +4,7 @@ import java.util.logging.Logger;
 
 final public class Version {
 
-	public static final String VERSION = "5.5.9-SNAPSHOT";
+	public static final String VERSION = "5.5.9.Final";
 	
 	private static final Version instance = new Version();
 	
