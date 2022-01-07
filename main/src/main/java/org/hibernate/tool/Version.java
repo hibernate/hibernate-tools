d080fbc06d6086d03aa26454219356760ff2c4bb
Koen Aers <koen.aers@gmail.com>
2022-01-07 13:18:16 +0100

    HBX-2265: Update version identifier to 5.6.3-SNAPSHOT
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- main/src/main/java/org/hibernate/tool/Version.java
+++ main/src/main/java/org/hibernate/tool/Version.java
@@ -4,7 +4,7 @@ import java.util.logging.Logger;
 
 final public class Version {
 
-	public static final String VERSION = "5.6.2.Final";
+	public static final String VERSION = "5.6.3-SNAPSHOT";
 	
 	private static final Version instance = new Version();
 	
