f44f6855ec754c20fccf968fde9497adb2ff4bc2
Koen Aers <koen.aers@gmail.com>
2022-01-12 13:03:26 +0100

    HBX-2280: Update version identifier to 5.1.13-SNAPSHOT
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -4,7 +4,7 @@ import java.util.logging.Logger;
 
 final public class Version {
 
-	public static final String VERSION = "5.1.12.Final";
+	public static final String VERSION = "5.1.13-SNAPSHOT";
 	
 	private static final Version instance = new Version();
 	
