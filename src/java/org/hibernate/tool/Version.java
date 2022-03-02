628443e05adb41bee33788166f2c6b8c9f32486a
Koen Aers <koen.aers@gmail.com>
2022-03-02 11:36:40 +0100

    HBX-2308: Update version identifier to 3.6.2.Final
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -4,7 +4,7 @@ import freemarker.log.Logger;
 
 final public class Version {
 
-	public static final String VERSION = "3.6.2-SNAPSHOT";
+	public static final String VERSION = "3.6.2.Final";
 	
 	private static final Version instance = new Version();
 	
