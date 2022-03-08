0a3366f63d1c49f5f467367e687a250a3f83b433
Koen Aers <koen.aers@gmail.com>
2022-03-08 10:13:00 +0100

    HBX-2314: Update version identifier to 3.5.2.Final
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "3.5.2-SNAPSHOT";
+	public static final String VERSION = "3.5.2.Final";
 	
 	private static final Version instance = new Version();
 	
