15dc278bb2471197eefe0990141e61739fb8d126
Koen Aers <koen.aers@gmail.com>
2022-03-10 09:12:06 +0100

    HBX-2320: Update version identifier to 3.5.4-SNAPSHOT
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "3.5.3.Final";
+	public static final String VERSION = "3.5.4-SNAPSHOT";
 	
 	private static final Version instance = new Version();
 	
