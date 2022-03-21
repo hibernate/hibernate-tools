f4c3a4ac43c1658d0b116e04b2baa2778d0ab609
Koen Aers <koen.aers@gmail.com>
2022-03-21 19:26:04 +0100

    HBX-2325: Replace the API calls that were deprecated since Hibernate Core 6.0 - Remove unused import 'org.junit.jupiter.api.Disabled' from 'org.hibernate.tool.jdbc2cfg.SearchEscapeString.TestCase'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/common/src/main/java/org/hibernate/tool/jdbc2cfg/SearchEscapeString/TestCase.java
+++ test/common/src/main/java/org/hibernate/tool/jdbc2cfg/SearchEscapeString/TestCase.java
@@ -32,7 +32,6 @@ import org.hibernate.tools.test.util.JUnitUtil;
 import org.hibernate.tools.test.util.JdbcUtil;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
