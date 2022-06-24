f772b9d60d333345821c4c07abcd9f0dfd67d286
Koen Aers <koen.aers@gmail.com>
2022-06-24 13:57:11 +0200

    HBX-2375: Fix the problem while publishing the test results with 'scacap/action-surefire-report' - Test build failure
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- src/java/org/hibernate/tool/Version.java
+++ src/java/org/hibernate/tool/Version.java
@@ -4,7 +4,7 @@ import java.util.logging.Logger;
 
 final public class Version {
 
-	public static final String VERSION = "5.0.8-SNAPSHOT";
+	public static final String VERSION = "0.0.8-SNAPSHOT";
 	
 	private static final Version instance = new Version();
 	
