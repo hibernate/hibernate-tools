98628354c502b859852a822cf92c8a477da2314d
blafond <blafond@redhat.com>
2022-08-24 10:07:38 -0500

    HBX-2386: Update version identifier to 5.6.11-SNAPSHOT
    Signed-off-by: blafond <blafond@redhat.com>

--- main/src/main/java/org/hibernate/tool/Version.java
+++ main/src/main/java/org/hibernate/tool/Version.java
@@ -4,7 +4,7 @@ import java.util.logging.Logger;
 
 final public class Version {
 
-	public static final String VERSION = "5.6.10.Final";
+	public static final String VERSION = "5.6.11-SNAPSHOT";
 	
 	private static final Version instance = new Version();
 	
