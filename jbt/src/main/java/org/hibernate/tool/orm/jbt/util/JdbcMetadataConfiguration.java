9acff2eede8faeab4fc02d8772e3ef4b57d7a875
Koen Aers <koen.aers@gmail.com>
2022-09-28 10:12:45 +0200

    HBX-2390: Create a JBoss Tools adaptation layer in Hibernate Tools - Add test class 'org.hibernate.tool.orm.jbt.util.JdbcMetadataConfigurationTest' and implementation class 'org.hibernate.tool.orm.jbt.util.JdbcMetadataConfiguration
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/util/JdbcMetadataConfiguration.java
@@ -0,0 +1,65 @@
+package org.hibernate.tool.orm.jbt.util;
+
+import java.util.Properties;
+
+import org.hibernate.boot.Metadata;
+import org.hibernate.tool.api.metadata.MetadataConstants;
+import org.hibernate.tool.api.metadata.MetadataDescriptorFactory;
+import org.hibernate.tool.api.reveng.RevengStrategy;
+
+public class JdbcMetadataConfiguration {
+
+	Properties properties = new Properties();
+	RevengStrategy revengStrategy;
+	Metadata metadata;
+
+	public Properties getProperties() {
+		return properties;
+	}
+	
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+	
+	public Object getProperty(String key) {
+		return this.properties.get(key);
+	}
+	
+	public void setProperty(String key, String value) {
+		properties.put(key, value);
+	}
+
+	public void addProperties(Properties properties) {
+		this.properties.putAll(properties);
+	}
+
+	public Object getReverseEngineeringStrategy() {
+		return revengStrategy;
+	}
+
+	public void setReverseEngineeringStrategy(RevengStrategy strategy) {
+		this.revengStrategy = strategy;
+	}
+
+	public boolean preferBasicCompositeIds() {
+		Object preferBasicCompositeIds = properties.get(MetadataConstants.PREFER_BASIC_COMPOSITE_IDS);
+		return preferBasicCompositeIds == null ? true : ((Boolean)preferBasicCompositeIds).booleanValue();
+	}
+
+	public void setPreferBasicCompositeIds(boolean preferBasicCompositeIds) {
+		properties.put(
+				MetadataConstants.PREFER_BASIC_COMPOSITE_IDS, 
+				Boolean.valueOf(preferBasicCompositeIds));
+	}
+
+	public Metadata getMetadata() {
+		return metadata;
+	}
+
+	public void readFromJdbc() {
+		metadata = MetadataDescriptorFactory
+				.createReverseEngineeringDescriptor(revengStrategy, properties)
+				.createMetadata();
+	}
+		
+}
---
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/util/JdbcMetadataConfigurationTest.java
@@ -0,0 +1,154 @@
+package org.hibernate.tool.orm.jbt.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Properties;
+
+import org.h2.Driver;
+import org.hibernate.boot.Metadata;
+import org.hibernate.tool.api.metadata.MetadataConstants;
+import org.hibernate.tool.api.reveng.RevengStrategy;
+import org.hibernate.tool.internal.reveng.strategy.DefaultStrategy;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class JdbcMetadataConfigurationTest {
+
+	private JdbcMetadataConfiguration jdbcMetadataConfiguration = null;
+	
+	@BeforeAll
+	public static void beforeAll() throws Exception {
+		DriverManager.registerDriver(new Driver());		
+	}
+
+	@BeforeEach
+	public void beforeEach() {
+		jdbcMetadataConfiguration = new JdbcMetadataConfiguration();
+	}
+	
+	@Test
+	public void testInstance() {
+		assertNotNull(jdbcMetadataConfiguration);
+	}
+	
+	@Test
+	public void testGetProperties() {
+		Properties properties = new Properties();
+		assertNotNull(jdbcMetadataConfiguration.properties);
+		assertNotSame(properties,  jdbcMetadataConfiguration.getProperties());
+		jdbcMetadataConfiguration.properties = properties;
+		assertSame(properties, jdbcMetadataConfiguration.getProperties());
+	}
+	
+	@Test
+	public void testSetProperties() {
+		Properties properties = new Properties();
+		assertNotNull(jdbcMetadataConfiguration.properties);
+		assertNotSame(properties,  jdbcMetadataConfiguration.getProperties());
+		jdbcMetadataConfiguration.setProperties(properties);
+		assertSame(properties, jdbcMetadataConfiguration.getProperties());
+	}
+	
+	@Test
+	public void testGetProperty() {
+		assertNull(jdbcMetadataConfiguration.getProperty("foo"));
+		jdbcMetadataConfiguration.properties.put("foo", "bar");
+		assertEquals("bar", jdbcMetadataConfiguration.getProperty("foo"));
+	}
+
+	@Test
+	public void testSetProperty() {
+		assertNull(jdbcMetadataConfiguration.properties.get("foo"));
+		jdbcMetadataConfiguration.setProperty("foo", "bar");
+		assertEquals("bar", jdbcMetadataConfiguration.properties.get("foo"));
+	}
+	
+	@Test
+	public void testAddProperties() {
+		Properties properties = new Properties();
+		properties.put("foo", "bar");
+		assertNull(jdbcMetadataConfiguration.properties.get("foo"));
+		jdbcMetadataConfiguration.addProperties(properties);
+		assertEquals("bar", jdbcMetadataConfiguration.properties.get("foo"));
+	}
+	
+	@Test
+	public void testGetReverseEngineeringStrategy() {
+		RevengStrategy strategy = new DefaultStrategy();
+		assertNull(jdbcMetadataConfiguration.getReverseEngineeringStrategy());
+		jdbcMetadataConfiguration.revengStrategy = strategy;
+		assertSame(strategy, jdbcMetadataConfiguration.getReverseEngineeringStrategy());
+	}
+	
+	@Test
+	public void testSetReverseEngineeringStrategy() {
+		RevengStrategy strategy = new DefaultStrategy();
+		assertNull(jdbcMetadataConfiguration.revengStrategy);
+		jdbcMetadataConfiguration.setReverseEngineeringStrategy(strategy);
+		assertSame(strategy, jdbcMetadataConfiguration.revengStrategy);
+	}
+	
+	@Test
+	public void testPreferBasicCompositeIds() {
+		assertTrue(jdbcMetadataConfiguration.preferBasicCompositeIds());
+		jdbcMetadataConfiguration.properties.put(MetadataConstants.PREFER_BASIC_COMPOSITE_IDS, false);
+		assertFalse(jdbcMetadataConfiguration.preferBasicCompositeIds());
+	}
+	
+	@Test
+	public void testSetPreferBasicCompositeIds() {
+		assertNull(
+				jdbcMetadataConfiguration.properties.get(
+						MetadataConstants.PREFER_BASIC_COMPOSITE_IDS));
+		jdbcMetadataConfiguration.setPreferBasicCompositeIds(true);
+		assertEquals(
+				true, 
+				jdbcMetadataConfiguration.properties.get(
+						MetadataConstants.PREFER_BASIC_COMPOSITE_IDS));
+	}
+	
+	@Test
+	public void testGetMetadata() {
+		Metadata metadata = (Metadata)Proxy.newProxyInstance(
+				getClass().getClassLoader(), 
+				new Class[] { Metadata.class }, 
+				new InvocationHandler() {					
+					@Override
+					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+						return null;
+					}
+				});
+		assertNull(jdbcMetadataConfiguration.getMetadata());
+		jdbcMetadataConfiguration.metadata = metadata;
+		assertSame(metadata, jdbcMetadataConfiguration.getMetadata());
+	}
+	
+	@Test
+	public void testReadFromJdbc() throws Exception {
+		Connection connection = DriverManager.getConnection("jdbc:h2:mem:test");
+		Statement statement = connection.createStatement();
+		statement.execute("CREATE TABLE FOO(id int primary key, bar varchar(255))");
+		jdbcMetadataConfiguration.properties.put("hibernate.connection.url", "jdbc:h2:mem:test");
+		jdbcMetadataConfiguration.revengStrategy = new DefaultStrategy();
+		assertNull(jdbcMetadataConfiguration.metadata);
+		jdbcMetadataConfiguration.readFromJdbc();
+		assertNotNull(jdbcMetadataConfiguration.metadata);
+		statement.execute("DROP TABLE FOO");
+		statement.close();
+		connection.close();
+	}
+
+}
