2f103be8b012fb07c79bcd2a52e99cc790f1bfea
Koen Aers <koen.aers@gmail.com>
2022-10-29 08:14:46 +0200

    HBX-2390: Create a JBoss Tools adaptation layer in Hibernate Tools - Add new test 'WrapperFactoryTest#testCreateNamingStrategyWrapper()' and implementation of 'WrapperFactory#createNamingStrategyWrapper()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/WrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/WrapperFactory.java
@@ -1,5 +1,6 @@
 package org.hibernate.tool.orm.jbt.wrp;
 
+import org.hibernate.cfg.DefaultNamingStrategy;
 import org.hibernate.tool.internal.export.common.DefaultArtifactCollector;
 import org.hibernate.tool.internal.export.hbm.Cfg2HbmTool;
 
@@ -11,6 +12,10 @@ public class WrapperFactory {
 	
 	public Object createCfg2HbmWrapper() {
 		return new Cfg2HbmTool();
+	}
+
+	public Object createNamingStrategyWrapper() {
+		return new DefaultNamingStrategy();
 	}
 
 }
--- jbt/src/test/java/org/hibernate/tool/orm/jbt/wrp/WrapperFactoryTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/wrp/WrapperFactoryTest.java
@@ -3,6 +3,7 @@ package org.hibernate.tool.orm.jbt.wrp;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.hibernate.cfg.DefaultNamingStrategy;
 import org.hibernate.tool.internal.export.common.DefaultArtifactCollector;
 import org.hibernate.tool.internal.export.hbm.Cfg2HbmTool;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,6 +30,13 @@ public class WrapperFactoryTest {
 		Object cfg2HbmWrapper = wrapperFactory.createCfg2HbmWrapper();
 		assertNotNull(cfg2HbmWrapper);
 		assertTrue(cfg2HbmWrapper instanceof Cfg2HbmTool);
+	}
+	
+	@Test
+	public void testCreateNamingStrategyWrapper() {
+		Object namingStrategyWrapper = wrapperFactory.createNamingStrategyWrapper();
+		assertNotNull(namingStrategyWrapper);
+		assertTrue(namingStrategyWrapper instanceof DefaultNamingStrategy);
 	}
 
 }
