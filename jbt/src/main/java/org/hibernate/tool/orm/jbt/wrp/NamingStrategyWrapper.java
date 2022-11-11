a0c85b0905e891a591a4552db341a79d556d7a5f
Koen Aers <koen.aers@gmail.com>
2022-11-11 18:28:29 +0100

    HBX-2390: Create a JBoss Tools adaptation layer in Hibernate Tools - Add wrapper class for NamingStrategy and associated test class
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/NamingStrategyWrapper.java
@@ -0,0 +1,43 @@
+package org.hibernate.tool.orm.jbt.wrp;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.hibernate.cfg.NamingStrategy;
+import org.hibernate.tool.orm.jbt.util.ReflectUtil;
+
+public class NamingStrategyWrapper {
+	
+	public static Object create(String className) {
+		Object delegate = ReflectUtil.createInstance(className);
+		return Proxy.newProxyInstance( 
+				NamingStrategyWrapper.class.getClassLoader(), 
+				new Class[] { NamingStrategy.class , ClassNameProvieer.class }, 
+				new NamingStrategyInvocationHandler(delegate));
+	}
+	
+	private static class NamingStrategyInvocationHandler implements InvocationHandler {
+		
+		private Object delegate = null;
+		
+		private NamingStrategyInvocationHandler(Object delegate) {
+			this.delegate = delegate;
+		}
+
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			if (method.getName().equals("getClassName")) {
+				return delegate.getClass().getName();
+			} else {
+			    return method.invoke(delegate, args);
+			}
+		}
+		
+	}
+	
+	private static interface ClassNameProvieer {
+		public String getClassName();
+	}
+	
+}
---
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/wrp/NamingStrategyWrapperTest.java
@@ -0,0 +1,30 @@
+package org.hibernate.tool.orm.jbt.wrp;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.lang.reflect.Method;
+
+import org.hibernate.cfg.DefaultNamingStrategy;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class NamingStrategyWrapperTest {
+	
+	private Object namingStrategyWrapper = null;
+	
+	@BeforeEach
+	public void beforeEach() {
+		namingStrategyWrapper = NamingStrategyWrapper.create(DefaultNamingStrategy.class.getName());
+	}
+	
+	@Test
+	public void testGetClassName() throws Exception {
+		Method getClassNameMethod = namingStrategyWrapper
+				.getClass()
+				.getDeclaredMethod("getClassName", new Class[] {});
+		assertEquals(
+				DefaultNamingStrategy.class.getName(),
+				getClassNameMethod.invoke(namingStrategyWrapper, new Object[] {}));
+	}
+
+}
