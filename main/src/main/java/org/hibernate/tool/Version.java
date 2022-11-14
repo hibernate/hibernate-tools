50c69f94230080389c18d1b9ae1218aa739d866c
blafond <blafond@redhat.com>
2022-11-14 12:38:20 -0600

    HBX-2426: Update version identifier to 5.3.28.Final
    Signed-off-by: blafond <blafond@redhat.com>

--- main/src/main/java/org/hibernate/tool/Version.java
+++ main/src/main/java/org/hibernate/tool/Version.java
@@ -4,7 +4,7 @@ import java.util.logging.Logger;
 
 final public class Version {
 
-	public static final String VERSION = "5.3.28-SNAPSHOT";
+	public static final String VERSION = "5.3.28.Final";
 	
 	private static final Version instance = new Version();
 	
