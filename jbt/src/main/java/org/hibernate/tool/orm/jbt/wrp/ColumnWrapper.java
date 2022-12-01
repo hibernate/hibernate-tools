ef86a75772e978cc72e244447a605638166dab25
Koen Aers <koen.aers@gmail.com>
2022-12-01 08:48:40 +0100

    HBX-2390: Create a JBoss Tools adaptation layer in Hibernate Tools - Revert ColumnWrapper class to be a subclass of Column
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/ColumnWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/ColumnWrapper.java
@@ -10,51 +10,36 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.engine.jdbc.dialect.spi.DialectFactory;
 import org.hibernate.mapping.Column;
-import org.hibernate.mapping.Value;
 import org.hibernate.tool.orm.jbt.util.MetadataHelper;
 import org.hibernate.type.spi.TypeConfiguration;
 
-public class ColumnWrapper {
+public class ColumnWrapper extends Column {
 	
 	private static final int DEFAULT_LENGTH = 255;
 	private static final int DEFAULT_PRECISION = 19;
 	private static final int DEFAULT_SCALE = 2;
 	
-	private Column wrappedColumn = null;
-	
 	public ColumnWrapper(String name) {
-		wrappedColumn = new Column(name);
+		super(name);
 	}
 	
-	public String getName() {
-		return wrappedColumn.getName();
-	}
-
-	public Integer getSqlTypeCode() {
-		return wrappedColumn.getSqlTypeCode();
-	}
-
-	public String getSqlType() {
-		return wrappedColumn.getSqlType();
-	}
-
 	public String getSqlType(Configuration configuration) {
 		Metadata metadata = MetadataHelper.getMetadata(configuration);
 		TypeConfiguration tc = ((MetadataImpl)metadata).getTypeConfiguration();
-		return wrappedColumn.getSqlType(tc, buildDialect(configuration), metadata);
+		return super.getSqlType(tc, buildDialect(configuration), metadata);
 	}
 	
-	public int getLength() {
-		Long length = wrappedColumn.getLength();
-		return length == null ? Integer.MIN_VALUE : length.intValue();
+	public Long getLength() {
+		Long length = super.getLength();
+		return length == null ? Integer.MIN_VALUE : length.longValue();
 	}
 	
 	public int getDefaultLength() {
 		return DEFAULT_LENGTH;
 	}
 	
-	public int getPrecision() {
-		Integer precision = wrappedColumn.getPrecision();
+	public Integer getPrecision() {
+		Integer precision = super.getPrecision();
 		return precision == null ? Integer.MIN_VALUE : precision.intValue();
 	}
 
@@ -63,28 +48,12 @@ public class ColumnWrapper {
 	}
 
 	public Integer getScale() {
-		Integer scale = wrappedColumn.getScale();
+		Integer scale = super.getScale();
 		return scale == null ? Integer.MIN_VALUE : scale.intValue();
 	}
 
 	public int getDefaultScale() {
 		return DEFAULT_SCALE;
-	}
-
-	public boolean isNullable() {
-		return wrappedColumn.isNullable();
-	}
-
-	public Value getValue() {
-		return wrappedColumn.getValue();
-	}
-
-	public boolean isUnique() {
-		return wrappedColumn.isUnique();
-	}
-
-	public void setSqlType(String string) {
-		wrappedColumn.setSqlType(string);
 	}
 
 	private Dialect buildDialect(Configuration configuration) {
--- jbt/src/test/java/org/hibernate/tool/orm/jbt/wrp/ColumnWrapperTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/wrp/ColumnWrapperTest.java
@@ -1,11 +1,7 @@
 package org.hibernate.tool.orm.jbt.wrp;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
@@ -14,7 +10,6 @@ import java.lang.reflect.Proxy;
 
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.mapping.Column;
 import org.hibernate.mapping.Value;
 import org.hibernate.tool.orm.jbt.type.IntegerType;
 import org.hibernate.tool.orm.jbt.util.MockConnectionProvider;
@@ -25,47 +20,29 @@ import org.junit.jupiter.api.Test;
 public class ColumnWrapperTest {
 	
 	private ColumnWrapper columnWrapper = null;
-	private Column wrappedColumn = null;
 	
 	@BeforeEach
 	public void beforeEach() throws Exception {
 		columnWrapper = new ColumnWrapper(null);
-		Field columnField = ColumnWrapper.class.getDeclaredField("wrappedColumn");
-		columnField.setAccessible(true);
-		wrappedColumn = (Column)columnField.get(columnWrapper);
-	}
-	
-	@Test
-	public void testGetName() {
-		assertNull(columnWrapper.getName());
-		wrappedColumn.setName("foobar");
-		assertEquals("foobar", columnWrapper.getName());
-	}
-	
-	@Test
-	public void testGetSqlTypeCode() {
-		assertNull(columnWrapper.getSqlTypeCode());
-		wrappedColumn.setSqlTypeCode(Integer.MAX_VALUE);
-		assertEquals(Integer.MAX_VALUE, columnWrapper.getSqlTypeCode().intValue());
 	}
 	
 	@Test
 	public void testGetSqlType() {
 		assertNull(columnWrapper.getSqlType());
-		wrappedColumn.setSqlType("foobar");
+		columnWrapper.setSqlType("foobar");
 		assertEquals("foobar", columnWrapper.getSqlType());
-		wrappedColumn.setSqlType(null);
+		columnWrapper.setSqlType(null);
 		Configuration cfg = new Configuration();
 		cfg.setProperty(AvailableSettings.DIALECT, MockDialect.class.getName());
 		cfg.setProperty(AvailableSettings.CONNECTION_PROVIDER, MockConnectionProvider.class.getName());
-		wrappedColumn.setValue(createValue());
+		columnWrapper.setValue(createValue());
 		assertEquals("integer", columnWrapper.getSqlType(cfg));
 	}
 	
 	@Test
 	public void testGetLength() {
 		assertEquals(Integer.MIN_VALUE, columnWrapper.getLength());
-		wrappedColumn.setLength(Integer.MAX_VALUE);
+		columnWrapper.setLength(Integer.MAX_VALUE);
 		assertEquals(Integer.MAX_VALUE, columnWrapper.getLength());
 	}
 	
@@ -79,7 +56,7 @@ public class ColumnWrapperTest {
 	@Test
 	public void testGetPrecision() {
 		assertEquals(Integer.MIN_VALUE, columnWrapper.getPrecision());
-		wrappedColumn.setPrecision(Integer.MAX_VALUE);
+		columnWrapper.setPrecision(Integer.MAX_VALUE);
 		assertEquals(Integer.MAX_VALUE, columnWrapper.getPrecision());
 	}
 	
@@ -93,7 +70,7 @@ public class ColumnWrapperTest {
 	@Test
 	public void testGetScale() {
 		assertEquals(Integer.MIN_VALUE, columnWrapper.getScale());
-		wrappedColumn.setScale(Integer.MAX_VALUE);
+		columnWrapper.setScale(Integer.MAX_VALUE);
 		assertEquals(Integer.MAX_VALUE, columnWrapper.getScale());
 	}
 	
@@ -102,43 +79,6 @@ public class ColumnWrapperTest {
 		Field defaultScaleField = ColumnWrapper.class.getDeclaredField("DEFAULT_SCALE");
 		defaultScaleField.setAccessible(true);
 		assertEquals(defaultScaleField.get(null), columnWrapper.getDefaultScale());
-	}
-	
-	@Test
-	public void testIsNullable() {
-		wrappedColumn.setNullable(true);
-		assertTrue(columnWrapper.isNullable());
-		wrappedColumn.setNullable(false);
-		assertFalse(columnWrapper.isNullable());
-	}
-	
-	@Test
-	public void testGetValue() {
-		Value v = createValue();
-		assertNull(columnWrapper.getValue());
-		wrappedColumn.setValue(v);
-		Value value = columnWrapper.getValue();
-		assertNotNull(value);
-		assertSame(v, value);
-		wrappedColumn.setValue(null);
-		value = columnWrapper.getValue();
-		assertNull(value);
-	}
-	
-	@Test
-	public void testIsUnique() {
-		assertFalse(columnWrapper.isUnique());
-		wrappedColumn.setUnique(true);
-		assertTrue(columnWrapper.isUnique());
-		wrappedColumn.setUnique(false);
-		assertFalse(columnWrapper.isUnique());
-	}
-	
-	@Test
-	public void testSetSqlType() {
-		assertNull(wrappedColumn.getSqlType());
-		columnWrapper.setSqlType("blah");
-		assertEquals("blah", wrappedColumn.getSqlType());
 	}
 	
 	private Value createValue() {
