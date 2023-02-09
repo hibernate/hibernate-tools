c43d36d7f5917ba7f445a2929589b3aa47826943
Koen Aers <koen.aers@gmail.com>
2023-02-09 09:05:54 +0100

    HBX-2390: Create a JBoss Tools adaptation layer in Hibernate Tools   - Add new test case 'org.hibernate.tool.orm.jbt.wrp.CriteriaWrapperFactoryTest#testSetMaxResults()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/test/java/org/hibernate/tool/orm/jbt/wrp/CriteriaWrapperFactoryTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/wrp/CriteriaWrapperFactoryTest.java
@@ -1,5 +1,7 @@
 package org.hibernate.tool.orm.jbt.wrp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.lang.reflect.InvocationHandler;
@@ -27,14 +29,23 @@ public class CriteriaWrapperFactoryTest {
 		assertNotNull(queryWrapper);
 	}
 	
+	@Test
+	public void testSetMaxResults() {
+		assertNotEquals(queryTarget.getMaxResults(), Integer.MAX_VALUE);
+		queryWrapper.setMaxResults(Integer.MAX_VALUE);
+		assertEquals(queryTarget.getMaxResults(), Integer.MAX_VALUE);
+	}
+	
 	private Query createQueryTarget() {
 		return (Query)Proxy.newProxyInstance(
 				getClass().getClassLoader(), 
 				new Class[] { Query.class }, 
 				new InvocationHandler () {
+					private int maxResults = 0;
 					@Override
 					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-						// TODO Auto-generated method stub
+						if (method.getName().equals("getMaxResults")) { return maxResults; }
+						else if (method.getName().equals("setMaxResults")) { maxResults = (int)args[0]; }
 						return null;
 					}				
 				});
