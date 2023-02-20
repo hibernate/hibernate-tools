640efa144d19d7108dc8e2f6c0cd06ff83adc950
Koen Aers <koen.aers@gmail.com>
2023-02-20 19:52:05 +0100

    HBX-2390: Create a JBoss Tools adaptation layer in Hibernate Tools   - Add new method 'org.hibernate.tool.orm.jbt.wrp.PersistentClassWrapperFactory#createSingleTableSubclassWrapper(PersistentClassWrapper)'   - Adapt the test class 'org.hibernate.tool.orm.jbt.wrp.PersistentClassWrapperFactoryTest' to account for the new factory method
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/PersistentClassWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/PersistentClassWrapperFactory.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Proxy;
 
 import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.RootClass;
+import org.hibernate.mapping.SingleTableSubclass;
 import org.hibernate.tool.orm.jbt.util.DummyMetadataBuildingContext;
 
 public class PersistentClassWrapperFactory {
@@ -15,6 +16,14 @@ public class PersistentClassWrapperFactory {
 				PersistentClassWrapperFactory.class.getClassLoader(), 
 				new Class[] { PersistentClassWrapper.class }, 
 				new PersistentClassWrapperInvocationHandler(new RootClassWrapperImpl()));
+	}
+	
+	public static Object createSingleTableSubclassWrapper(PersistentClassWrapper superClassWrapper) {
+		return Proxy.newProxyInstance(
+				PersistentClassWrapperFactory.class.getClassLoader(), 
+				new Class[] { PersistentClassWrapper.class }, 
+				new PersistentClassWrapperInvocationHandler(
+						new SingleTableSubclassWrapperImpl(superClassWrapper.getWrappedObject())));
 	}
 	
 	static class PersistentClassWrapperInvocationHandler implements InvocationHandler {
@@ -42,6 +51,22 @@ public class PersistentClassWrapperFactory {
 		public PersistentClass getWrappedObject() {
 			return this;
 		}
+
+	}
+	
+	static class SingleTableSubclassWrapperImpl 
+			extends SingleTableSubclass 
+			implements PersistentClassWrapper {
+
+		public SingleTableSubclassWrapperImpl(PersistentClass superclass) {
+			super(superclass, DummyMetadataBuildingContext.INSTANCE);
+		}
+
+		@Override
+		public PersistentClass getWrappedObject() {
+			return this;
+		}
+		
 	}
 	
 }
--- jbt/src/test/java/org/hibernate/tool/orm/jbt/wrp/PersistentClassWrapperFactoryTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/wrp/PersistentClassWrapperFactoryTest.java
@@ -18,41 +18,56 @@ public class PersistentClassWrapperFactoryTest {
 	
 	private PersistentClass rootClassTarget = null;
 	private PersistentClassWrapper rootClassWrapper = null;
+	private PersistentClass singleTableSubclassTarget = null;
+	private PersistentClassWrapper singleTableSubclassWrapper = null;
 	
 	@BeforeEach
 	public void beforeEach() throws Exception {
-		rootClassWrapper = (PersistentClassWrapper)PersistentClassWrapperFactory
-				.createRootClassWrapper();
-		InvocationHandler invocationHandler = Proxy.getInvocationHandler(rootClassWrapper);
+		InvocationHandler invocationHandler = null;
 		Field wrapperField = PersistentClassWrapperInvocationHandler.class.getDeclaredField("wrapper");
 		wrapperField.setAccessible(true);
+		rootClassWrapper = (PersistentClassWrapper)PersistentClassWrapperFactory
+				.createRootClassWrapper();
+		invocationHandler = Proxy.getInvocationHandler(rootClassWrapper);
 		rootClassTarget = (PersistentClass)wrapperField.get(invocationHandler);
-		
-;	}
+		singleTableSubclassWrapper = (PersistentClassWrapper)PersistentClassWrapperFactory
+				.createSingleTableSubclassWrapper(rootClassWrapper);
+		invocationHandler = Proxy.getInvocationHandler(singleTableSubclassWrapper);
+		singleTableSubclassTarget = (PersistentClass)wrapperField.get(invocationHandler);
+	}
 	
 	@Test
 	public void testConstruction() {
 		assertNotNull(rootClassWrapper);
 		assertNotNull(rootClassTarget);
+		assertNotNull(singleTableSubclassWrapper);
+		assertNotNull(singleTableSubclassTarget);
 	}
 	
 	@Test
 	public void testGetWrappedObject() {
 		assertSame(rootClassTarget, rootClassWrapper.getWrappedObject());
+		assertSame(singleTableSubclassTarget, singleTableSubclassWrapper.getWrappedObject());
 	}
 	
 	@Test
 	public void testGetEntityName() {
 		assertNotEquals("foo", rootClassWrapper.getEntityName());
+		assertNotEquals("bar", singleTableSubclassWrapper.getEntityName());
 		rootClassTarget.setEntityName("foo");
+		singleTableSubclassTarget.setEntityName("bar");
 		assertEquals("foo", rootClassWrapper.getEntityName());
+		assertEquals("bar", singleTableSubclassWrapper.getEntityName());
 	}
 	
 	@Test
 	public void testGetClassName() {
 		assertNotEquals("foo", rootClassWrapper.getClassName());
+		assertNotEquals("bar", singleTableSubclassWrapper.getClassName());
 		rootClassTarget.setClassName("foo");
+		singleTableSubclassTarget.setClassName("bar");
 		assertEquals("foo", rootClassWrapper.getClassName());
+		assertEquals("bar", singleTableSubclassWrapper.getClassName());
 	}
 	
 }
