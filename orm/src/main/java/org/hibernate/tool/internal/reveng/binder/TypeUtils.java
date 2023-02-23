323ee5256ef648fae76c1ff166dbab07b54a1e07
Koen Aers <koen.aers@gmail.com>
2023-02-23 17:11:05 +0100

    HBX-2464: Update Hibernate ORM Dependency to Version 6.1.7.Final
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/src/main/java/org/hibernate/tool/internal/reveng/binder/TypeUtils.java
+++ orm/src/main/java/org/hibernate/tool/internal/reveng/binder/TypeUtils.java
@@ -1,5 +1,6 @@
 package org.hibernate.tool.internal.reveng.binder;
 
+import java.lang.reflect.Field;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -70,7 +71,7 @@ public class TypeUtils {
 				LOGGER.log(
 						Level.INFO,
 						"Sql type mismatch for " + location + " between DB and wanted hibernate type. Sql type set to " + typeCodeName( sqlTypeCode.intValue() ) + " instead of " + typeCodeName(wantedSqlType) );
-				column.setSqlTypeCode(Integer.valueOf(wantedSqlType));
+				forceSqlTypeCode(column, wantedSqlType);
 			}
 			
 		}
@@ -96,5 +97,17 @@ public class TypeUtils {
 		return sqlTypeCode + "(" + JdbcToHibernateTypeHelper.getJDBCTypeName(sqlTypeCode) + ")";
 	}
 
+	private static void forceSqlTypeCode(Column column, int sqlCode) {
+		try {
+			Field sqlCodeField = Column.class.getDeclaredField("sqlTypeCode");
+			sqlCodeField.setAccessible(true);
+			sqlCodeField.set(column, Integer.valueOf(sqlCode));
+		} catch (NoSuchFieldException |
+				 SecurityException | 
+				 IllegalArgumentException | 
+				 IllegalAccessException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 }
