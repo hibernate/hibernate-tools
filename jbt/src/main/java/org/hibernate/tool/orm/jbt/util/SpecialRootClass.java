c1c9e2fb416b4b9eba65f6404a8f3ccb6beb2305
Koen Aers <koen.aers@gmail.com>
2023-02-27 20:28:07 +0100

    HBX-2390: Create a JBoss Tools adaptation layer in Hibernate Tools   - Complete the implementation of 'org.hibernate.tool.orm.jbt.util.SpecialRootClass'   - Complete the tests in 'org.hibernate.tool.orm.jbt.util.SpecialRootClassTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/util/SpecialRootClass.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/util/SpecialRootClass.java
@@ -1,23 +1,70 @@
 package org.hibernate.tool.orm.jbt.util;
 
 import java.lang.reflect.Field;
+import java.util.Iterator;
 
 import org.hibernate.boot.spi.MetadataBuildingContext;
+import org.hibernate.mapping.Collection;
+import org.hibernate.mapping.Component;
 import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.Property;
 import org.hibernate.mapping.RootClass;
+import org.hibernate.mapping.Value;
 
 public class SpecialRootClass extends RootClass {
 	
 	private Property property;
+	private Property parentProperty;
 
 	public SpecialRootClass(Property property) {
 		super(getMetadataBuildingContext(property));
 		this.property = property;
+		initialize();
 	}
 	
 	public Property getProperty() {
 		return property;
+	}
+	
+	public Property getParentProperty() {
+		return parentProperty;
+	}
+	
+	private void initialize() {
+		Component component = getComponent();
+		if (component != null) {
+			setClassName(component.getComponentClassName());
+			setEntityName(component.getComponentClassName());
+			PersistentClass ownerClass = component.getOwner();
+			if (component.getParentProperty() != null) {
+				parentProperty = new Property();
+				parentProperty.setName(component.getParentProperty());
+				parentProperty.setPersistentClass(ownerClass);
+			}
+			Iterator<Property> iterator = component.getProperties().iterator();
+			while (iterator.hasNext()) {
+				Property property = iterator.next();
+				if (property != null) {
+					addProperty(property);
+				}
+			}
+		}
+	}
+	
+	private Component getComponent() {
+		Component result = null;
+		if (property != null) {
+			Value v = property.getValue();
+			if (v != null) {
+				if (Collection.class.isAssignableFrom(v.getClass())) {
+					v = ((Collection)v).getElement();
+				}
+				if (v != null && Component.class.isAssignableFrom(v.getClass())) {
+					result = (Component)v;
+				}
+			}
+		}
+		return result;
 	}
 	
 	private static MetadataBuildingContext getMetadataBuildingContext(Property property) {
@@ -25,9 +72,11 @@ public class SpecialRootClass extends RootClass {
 		try {
 			if (property != null) {
 				PersistentClass pc = property.getPersistentClass();
-				Field field = PersistentClass.class.getDeclaredField("metadataBuildingContext");
-				field.setAccessible(true);
-				result = (MetadataBuildingContext)field.get(pc);
+				if (pc != null) {
+					Field field = PersistentClass.class.getDeclaredField("metadataBuildingContext");
+					field.setAccessible(true);
+					result = (MetadataBuildingContext)field.get(pc);
+				}
 			}
 		} catch (NoSuchFieldException | 
 				SecurityException | 
--- jbt/src/test/java/org/hibernate/tool/orm/jbt/util/SpecialRootClassTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/util/SpecialRootClassTest.java
@@ -1,43 +1,108 @@
 package org.hibernate.tool.orm.jbt.util;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.lang.reflect.Field;
 
+import org.hibernate.mapping.BasicValue;
+import org.hibernate.mapping.Component;
 import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.Property;
 import org.hibernate.mapping.RootClass;
-import org.junit.jupiter.api.BeforeEach;
+import org.hibernate.mapping.Set;
 import org.junit.jupiter.api.Test;
 
 public class SpecialRootClassTest {
 	
 	private SpecialRootClass specialRootClass = null;
 	
-	private Property property = new Property();
-	
-	@BeforeEach 
-	public void beforeEach() {
-		PersistentClass pc = new RootClass(DummyMetadataBuildingContext.INSTANCE);
-		property.setPersistentClass(pc);
-		specialRootClass = new SpecialRootClass(property);
+	@Test
+	public void testPropertyIsNull() {
+		specialRootClass = new SpecialRootClass(null);
+		assertNotNull(specialRootClass);
+		assertNull(specialRootClass.getProperty());
+		assertNull(specialRootClass.getParentProperty());
 	}
 	
 	@Test
-	public void testConstruction() throws Exception {
-		// Normal case
+	public void testPropertyIsNotNull() throws Exception {
+		PersistentClass pc = new RootClass(DummyMetadataBuildingContext.INSTANCE);
+		Property property = new Property();
+		property.setPersistentClass(pc);
+		specialRootClass = new SpecialRootClass(property);
 		assertNotNull(specialRootClass);
 		Field field = PersistentClass.class.getDeclaredField("metadataBuildingContext");
 		field.setAccessible(true);
 		assertSame(DummyMetadataBuildingContext.INSTANCE, field.get(specialRootClass));
-		// Property is null
-		assertNotNull(new SpecialRootClass(null));
+		assertSame(property, specialRootClass.getProperty());
 	}
 	
 	@Test
-	public void testGetProperty() {
+	public void testPropertyIsComponent() {
+		PersistentClass pc = new RootClass(DummyMetadataBuildingContext.INSTANCE);
+		Component component = new Component(DummyMetadataBuildingContext.INSTANCE, pc);
+		Property property = new Property();
+		property.setValue(component);
+		property.setPersistentClass(pc);
+		specialRootClass = new SpecialRootClass(property);
 		assertSame(property, specialRootClass.getProperty());
+		assertNull(specialRootClass.getParentProperty());
+		assertTrue(specialRootClass.getProperties().isEmpty());
+		component.setParentProperty("foo");
+		specialRootClass = new SpecialRootClass(property);
+		assertSame(property, specialRootClass.getProperty());
+		Property parentProperty = specialRootClass.getParentProperty();
+		assertNotNull(parentProperty);
+		assertEquals("foo", parentProperty.getName());
+		assertTrue(specialRootClass.getProperties().isEmpty());
+		Property p = new Property();
+		p.setName("bar");
+		component.addProperty(p);
+		specialRootClass = new SpecialRootClass(property);
+		assertFalse(specialRootClass.getProperties().isEmpty());
+		assertEquals("bar", specialRootClass.getProperties().get(0).getName());
 	}
-
+	
+	@Test
+	public void testPropertyIsCollection() {
+		PersistentClass pc = new RootClass(DummyMetadataBuildingContext.INSTANCE);
+		Set set = new Set(DummyMetadataBuildingContext.INSTANCE, pc);
+		Property property = new Property();
+		property.setValue(set);
+		property.setPersistentClass(pc);
+		specialRootClass = new SpecialRootClass(property);
+		assertSame(property, specialRootClass.getProperty());
+		assertNull(specialRootClass.getParentProperty());
+		assertTrue(specialRootClass.getProperties().isEmpty());
+		set.setElement(new BasicValue(DummyMetadataBuildingContext.INSTANCE));
+		specialRootClass = new SpecialRootClass(property);
+		assertSame(property, specialRootClass.getProperty());
+		assertNull(specialRootClass.getParentProperty());
+		assertTrue(specialRootClass.getProperties().isEmpty());
+		Component component = new Component(DummyMetadataBuildingContext.INSTANCE, pc);
+		set.setElement(component);
+		specialRootClass = new SpecialRootClass(property);
+		assertSame(property, specialRootClass.getProperty());
+		assertNull(specialRootClass.getParentProperty());
+		assertTrue(specialRootClass.getProperties().isEmpty());
+		component.setParentProperty("foo");
+		specialRootClass = new SpecialRootClass(property);
+		assertSame(property, specialRootClass.getProperty());
+		Property parentProperty = specialRootClass.getParentProperty();
+		assertNotNull(parentProperty);
+		assertEquals("foo", parentProperty.getName());
+		assertTrue(specialRootClass.getProperties().isEmpty());
+		Property p = new Property();
+		p.setName("bar");
+		component.addProperty(p);
+		specialRootClass = new SpecialRootClass(property);
+		assertFalse(specialRootClass.getProperties().isEmpty());
+		assertEquals("bar", specialRootClass.getProperties().get(0).getName());
+	}
+	
 }
