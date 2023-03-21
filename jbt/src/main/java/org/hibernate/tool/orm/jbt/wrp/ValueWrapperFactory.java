79d1766322420fa726831bcce3ded524508ed629
Koen Aers <koen.aers@gmail.com>
2023-03-21 19:02:19 +0100

    HBX-2390: Create a JBoss Tools adaptation layer in Hibernate Tools   - Add interface 'org.hibernate.tool.orm.jbt.wrp.ValueWrapperFactory.ValueWrapper'   - Add implementations of above interface as inner classes to ValueWrapperFactory for all the different Value kinds that are created by the factory   - Adapt the factory methods of ValueWrapperFactory to return instances of the above inner classes
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/ValueWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/ValueWrapperFactory.java
@@ -3,6 +3,7 @@ package org.hibernate.tool.orm.jbt.wrp;
 import org.hibernate.mapping.Array;
 import org.hibernate.mapping.Bag;
 import org.hibernate.mapping.BasicValue;
+import org.hibernate.mapping.Collection;
 import org.hibernate.mapping.List;
 import org.hibernate.mapping.ManyToOne;
 import org.hibernate.mapping.Map;
@@ -17,46 +18,109 @@ import org.hibernate.tool.orm.jbt.util.DummyMetadataBuildingContext;
 public class ValueWrapperFactory {
 	
 	public static Value createArrayWrapper(PersistentClassWrapper persistentClassWrapper) {
-		return new Array(DummyMetadataBuildingContext.INSTANCE, persistentClassWrapper.getWrappedObject());
+		return new ArrayWrapperImpl(persistentClassWrapper);
 	}
 
 	public static Value createBagWrapper(PersistentClassWrapper persistentClassWrapper) {
-		return new Bag(DummyMetadataBuildingContext.INSTANCE, persistentClassWrapper.getWrappedObject());
+		return new BagWrapperImpl(persistentClassWrapper);
 	}
 
 	public static Value createListWrapper(PersistentClassWrapper persistentClassWrapper) {
-		return new List(DummyMetadataBuildingContext.INSTANCE, persistentClassWrapper.getWrappedObject());
+		return new ListWrapperImpl(persistentClassWrapper);
 	}
 
 	public static Value createManyToOneWrapper(Table table) {
-		return new ManyToOne(DummyMetadataBuildingContext.INSTANCE, table);
+		return new ManyToOneWrapperImpl(table);
 	}
 
 	public static Value createMapWrapper(PersistentClassWrapper persistentClassWrapper) {
-		return new Map(DummyMetadataBuildingContext.INSTANCE, persistentClassWrapper.getWrappedObject());
+		return new MapWrapperImpl(persistentClassWrapper);
 	}
 
 	public static Value createOneToManyWrapper(PersistentClassWrapper persistentClassWrapper) {
-		return new OneToMany(DummyMetadataBuildingContext.INSTANCE, persistentClassWrapper.getWrappedObject());
+		return new OneToManyWrapperImpl(persistentClassWrapper);
 	}
 
 	public static Value createOneToOneWrapper(PersistentClassWrapper persistentClassWrapper) {
-		return new OneToOne(
-				DummyMetadataBuildingContext.INSTANCE, 
-				persistentClassWrapper.getWrappedObject().getTable(),
-				persistentClassWrapper.getWrappedObject());
+		return new OneToOneWrapperImpl(persistentClassWrapper);
 	}
 
 	public static Value createPrimitiveArrayWrapper(PersistentClassWrapper persistentClassWrapper) {
-		return new PrimitiveArray(DummyMetadataBuildingContext.INSTANCE, persistentClassWrapper.getWrappedObject());
+		return new PrimitiveArrayWrapperImpl(persistentClassWrapper);
 	}
 
 	public static Value createSetWrapper(PersistentClassWrapper persistentClassWrapper) {
-		return new Set(DummyMetadataBuildingContext.INSTANCE, persistentClassWrapper.getWrappedObject());
+		return new SetWrapperImpl(persistentClassWrapper);
 	}
 
 	public static Value createSimpleValueWrapper() {
-		return new BasicValue(DummyMetadataBuildingContext.INSTANCE);
+		return new SimpleValueWrapperImpl();
+	}
+	
+	static interface ValueWrapper extends Value {
+		default boolean isCollection() { return Collection.class.isAssignableFrom(getClass()); }
+	}
+	
+	private static class ArrayWrapperImpl extends Array implements ValueWrapper {
+		protected ArrayWrapperImpl(PersistentClassWrapper persistentClassWrapper) {
+			super(DummyMetadataBuildingContext.INSTANCE, persistentClassWrapper.getWrappedObject());
+		}		
+	}
+
+	private static class BagWrapperImpl extends Bag implements ValueWrapper {
+		protected BagWrapperImpl(PersistentClassWrapper persistentClassWrapper) {
+			super(DummyMetadataBuildingContext.INSTANCE, persistentClassWrapper.getWrappedObject());
+		}		
+	}
+
+	private static class ListWrapperImpl extends List implements ValueWrapper {
+		protected ListWrapperImpl(PersistentClassWrapper persistentClassWrapper) {
+			super(DummyMetadataBuildingContext.INSTANCE, persistentClassWrapper.getWrappedObject());
+		}		
+	}
+
+	private static class ManyToOneWrapperImpl extends ManyToOne implements ValueWrapper {
+		protected ManyToOneWrapperImpl(Table table) {
+			super(DummyMetadataBuildingContext.INSTANCE, table);
+		}		
+	}
+
+	private static class MapWrapperImpl extends Map implements ValueWrapper {
+		protected MapWrapperImpl(PersistentClassWrapper persistentClassWrapper) {
+			super(DummyMetadataBuildingContext.INSTANCE, persistentClassWrapper.getWrappedObject());
+		}		
+	}
+
+	private static class OneToManyWrapperImpl extends OneToMany implements ValueWrapper {
+		protected OneToManyWrapperImpl(PersistentClassWrapper persistentClassWrapper) {
+			super(DummyMetadataBuildingContext.INSTANCE, persistentClassWrapper.getWrappedObject());
+		}		
+	}
+
+	private static class OneToOneWrapperImpl extends OneToOne implements ValueWrapper {
+		protected OneToOneWrapperImpl(PersistentClassWrapper persistentClassWrapper) {
+			super(DummyMetadataBuildingContext.INSTANCE, 
+					persistentClassWrapper.getWrappedObject().getTable(),
+					persistentClassWrapper.getWrappedObject());
+		}		
+	}
+
+	private static class PrimitiveArrayWrapperImpl extends PrimitiveArray implements ValueWrapper {
+		protected PrimitiveArrayWrapperImpl(PersistentClassWrapper persistentClassWrapper) {
+			super(DummyMetadataBuildingContext.INSTANCE, persistentClassWrapper.getWrappedObject());
+		}		
+	}
+
+	private static class SetWrapperImpl extends Set implements ValueWrapper {
+		protected SetWrapperImpl(PersistentClassWrapper persistentClassWrapper) {
+			super(DummyMetadataBuildingContext.INSTANCE, persistentClassWrapper.getWrappedObject());
+		}		
+	}
+
+	private static class SimpleValueWrapperImpl extends BasicValue implements ValueWrapper {
+		protected SimpleValueWrapperImpl() {
+			super(DummyMetadataBuildingContext.INSTANCE);
+		}		
 	}
 
 }
