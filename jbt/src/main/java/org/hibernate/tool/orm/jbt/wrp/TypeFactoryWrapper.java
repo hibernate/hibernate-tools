6c3c33c658dd76076dea232dc5c0265eb9707ecb
Koen Aers <koen.aers@gmail.com>
2023-04-15 09:20:29 +0200

    HBX-2390: Create a JBoss Tools adaptation layer in Hibernate Tools   - Add new test case 'org.hibernate.tool.orm.jbt.wrp.TypeFactoryWrapperTest#testGetBooleanType()'   - Implement new method 'org.hibernate.tool.orm.jbt.wrp.TypeFactoryWrapper#getBooleanType()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/TypeFactoryWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/TypeFactoryWrapper.java
@@ -1,9 +1,18 @@
 package org.hibernate.tool.orm.jbt.wrp;
 
+import org.hibernate.tool.orm.jbt.type.TypeFactory;
+import org.hibernate.tool.orm.jbt.wrp.TypeWrapperFactory.TypeWrapper;
+
 public class TypeFactoryWrapper {
 
 	public static final TypeFactoryWrapper INSTANCE = new TypeFactoryWrapper();
 	
+	static final TypeWrapper BOOLEAN_TYPE = TypeWrapperFactory.createTypeWrapper(TypeFactory.BOOLEAN_TYPE);
+	
 	private TypeFactoryWrapper() {}
+
+	public TypeWrapper getBooleanType() {
+		return BOOLEAN_TYPE;
+	}
 
 }
--- jbt/src/test/java/org/hibernate/tool/orm/jbt/wrp/TypeFactoryWrapperTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/wrp/TypeFactoryWrapperTest.java
@@ -1,7 +1,10 @@
 package org.hibernate.tool.orm.jbt.wrp;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
+import org.hibernate.tool.orm.jbt.type.TypeFactory;
+import org.hibernate.tool.orm.jbt.wrp.TypeWrapperFactory.TypeWrapper;
 import org.junit.jupiter.api.Test;
 
 public class TypeFactoryWrapperTest {
@@ -9,6 +12,12 @@ public class TypeFactoryWrapperTest {
 	@Test
 	public void testConstruction() {
 		assertNotNull(TypeFactoryWrapper.INSTANCE);
+	}
+	
+	@Test
+	public void testGetBooleanType() {
+		TypeWrapper booleanTypeWrapper = TypeFactoryWrapper.INSTANCE.getBooleanType();
+		assertSame(booleanTypeWrapper.getWrappedObject(), TypeFactory.BOOLEAN_TYPE);
 	}
 
 }
