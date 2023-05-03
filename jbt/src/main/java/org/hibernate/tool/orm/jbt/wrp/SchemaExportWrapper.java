185a67268d86224c67d167f6ec83dfa2c92ce06f
Koen Aers <koen.aers@gmail.com>
2023-05-03 14:00:06 +0200

    HBX-2390: Create a JBoss Tools adaptation layer in Hibernate Tools   - Add new test case 'org.hibernate.tool.orm.jbt.wrp.SchemaExportWrapperTest#testCreate()'   - Create new implementation method 'org.hibernate.tool.orm.jbt.wrp.SchemaExportWrapper#create()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/SchemaExportWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/SchemaExportWrapper.java
@@ -1,7 +1,11 @@
 package org.hibernate.tool.orm.jbt.wrp;
 
+import java.util.EnumSet;
+
 import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.hibernate.tool.orm.jbt.util.MetadataHelper;
+import org.hibernate.tool.schema.TargetType;
 
 public class SchemaExportWrapper extends SchemaExport {
 	
@@ -9,6 +13,12 @@ public class SchemaExportWrapper extends SchemaExport {
 	
 	public SchemaExportWrapper(Configuration configuration) {
 		this.configuration = configuration;
+	}
+
+	public void create() {
+		create(EnumSet.of(
+				TargetType.DATABASE), 
+				MetadataHelper.getMetadata(configuration));
 	}
 
 }
