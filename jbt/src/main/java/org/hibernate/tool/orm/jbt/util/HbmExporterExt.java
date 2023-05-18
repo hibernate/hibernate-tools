4d3c624e7a516f28de6fb1c89f2b9a892fa473ff
Koen Aers <koen.aers@gmail.com>
2023-05-18 11:47:48 +0200

    HBX-2390: Create a JBoss Tools adaptation layer in Hibernate Tools   - Add new test case 'org.hibernate.tool.orm.jbt.util.HbmExporterExtTest#testExportPOJO()'   - Add new implementation method 'org.hibernate.tool.orm.jbt.util.HbmExporterExt#exportPOJO(Map,POJOClass)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/util/HbmExporterExt.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/util/HbmExporterExt.java
@@ -1,9 +1,12 @@
 package org.hibernate.tool.orm.jbt.util;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 
 import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.internal.export.hbm.HbmExporter;
+import org.hibernate.tool.internal.export.java.POJOClass;
 
 public class HbmExporterExt extends HbmExporter {
 	
@@ -22,4 +25,30 @@ public class HbmExporterExt extends HbmExporter {
 		delegateExporter = delegate;
 	}
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	protected void exportPOJO(Map map, POJOClass pojoClass) {
+		if (delegateExporter == null) {
+			super.exportPOJO(map, pojoClass);
+		} else {
+			delegateExporterExportPOJO(
+					(Map<Object, Object>)map, 
+					pojoClass,
+					pojoClass.getQualifiedDeclarationName());
+		}
+	}
+	
+	private void delegateExporterExportPOJO (
+			Map<Object, Object> map, 
+			POJOClass pojoClass, 
+			String qualifiedDeclarationName) {
+		try {
+			delegateExporter
+				.getClass()
+				.getMethod("exportPojo", Map.class, Object.class, String.class)
+				.invoke(delegateExporter, map, pojoClass, qualifiedDeclarationName);
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
--- jbt/src/test/java/org/hibernate/tool/orm/jbt/util/HbmExporterExtTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/util/HbmExporterExtTest.java
@@ -1,5 +1,6 @@
 package org.hibernate.tool.orm.jbt.util;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -7,10 +8,20 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
+import java.io.Writer;
 import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.hibernate.cfg.Configuration;
+import org.hibernate.mapping.PersistentClass;
+import org.hibernate.mapping.RootClass;
 import org.hibernate.tool.api.export.ExporterConstants;
+import org.hibernate.tool.internal.export.common.AbstractExporter;
+import org.hibernate.tool.internal.export.common.TemplateHelper;
+import org.hibernate.tool.internal.export.java.Cfg2JavaTool;
+import org.hibernate.tool.internal.export.java.EntityPOJOClass;
+import org.hibernate.tool.internal.export.java.POJOClass;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -20,6 +31,9 @@ public class HbmExporterExtTest {
 	private HbmExporterExt hbmExporterExt = null;
 	private Configuration cfg = null;
 	private File f = null;
+	private boolean templateProcessed = false;
+	private boolean delegateHasExported = false;
+
 
 	@TempDir private File tempFolder;
 	
@@ -52,6 +66,47 @@ public class HbmExporterExtTest {
 		assertNull(delegateField.get(hbmExporterExt));
 		hbmExporterExt.setDelegate(delegate);
 		assertSame(delegate, delegateField.get(hbmExporterExt));
+	}
+	
+	@Test
+	public void testExportPOJO() throws Exception {
+		// first without a delegate exporter
+		Map<Object, Object> context = new HashMap<>();
+		PersistentClass pc = new RootClass(DummyMetadataBuildingContext.INSTANCE);
+		pc.setEntityName("foo");
+		pc.setClassName("foo");
+		POJOClass pojoClass = new EntityPOJOClass(pc, new Cfg2JavaTool());
+		TemplateHelper templateHelper = new TemplateHelper() {
+		    public void processTemplate(String templateName, Writer output, String rootContext) {
+		    	templateProcessed = true;
+		    }
+		};
+		templateHelper.init(null, new String[] {});
+		Field templateHelperField = AbstractExporter.class.getDeclaredField("vh");
+		templateHelperField.setAccessible(true);
+		templateHelperField.set(hbmExporterExt, templateHelper);
+		assertFalse(templateProcessed);
+		assertFalse(delegateHasExported);
+		hbmExporterExt.exportPOJO(context, pojoClass);
+		assertTrue(templateProcessed);
+		assertFalse(delegateHasExported);
+		// now with a delegate exporter
+		templateProcessed = false;
+		Object delegateExporter = new Object() {
+			@SuppressWarnings("unused")
+			public void exportPojo(Map<Object, Object> map, Object object, String string) {
+				assertSame(map, context);
+				assertSame(object, pojoClass);
+				assertEquals(string, pojoClass.getQualifiedDeclarationName());
+				delegateHasExported = true;
+			}
+		};
+		hbmExporterExt.setDelegate(delegateExporter);
+		assertFalse(templateProcessed);
+		assertFalse(delegateHasExported);
+		hbmExporterExt.exportPOJO(context, pojoClass);
+		assertFalse(templateProcessed);
+		assertTrue(delegateHasExported);
 	}
 
 }
