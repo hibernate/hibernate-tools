6ffdbacea4e5fec4a4307042979a47c2c6913a6b
Koen Aers <koen.aers@gmail.com>
2023-05-30 09:48:43 +0200

    HBX-2390: Create a JBoss Tools adaptation layer in Hibernate Tools   - Add new test case 'org.hibernate.tool.orm.jbt.wrp.DdlExporterWrapperTest#testSetExport()'   - Add new implementation method 'org.hibernate.tool.orm.jbt.wrp.DdlExporterWrapper#setExport(boolean)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/test/java/org/hibernate/tool/orm/jbt/wrp/DdlExporterWrapperTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/wrp/DdlExporterWrapperTest.java
@@ -1,8 +1,11 @@
 package org.hibernate.tool.orm.jbt.wrp;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.hibernate.tool.api.export.ExporterConstants;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -21,4 +24,13 @@ public class DdlExporterWrapperTest {
 		assertTrue(ddlExporterWrapper instanceof DdlExporterWrapper);
 	}
 	
+	@Test
+	public void testSetExport() {
+		assertNull(ddlExporterWrapper.getProperties().get(ExporterConstants.EXPORT_TO_DATABASE));
+		ddlExporterWrapper.setExport(false);
+		assertFalse((Boolean)ddlExporterWrapper.getProperties().get(ExporterConstants.EXPORT_TO_DATABASE));
+		ddlExporterWrapper.setExport(true);
+		assertTrue((Boolean)ddlExporterWrapper.getProperties().get(ExporterConstants.EXPORT_TO_DATABASE));
+	}
+
 }
