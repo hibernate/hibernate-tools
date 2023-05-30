282e64141a98e62b83308f66e8bbfed96e3c1c26
Koen Aers <koen.aers@gmail.com>
2023-05-30 12:17:19 +0200

    HBX-2390: Create a JBoss Tools adaptation layer in Hibernate Tools   - Add new test case 'org.hibernate.tool.orm.jbt.wrp.QueryExporterWrapperTest#testSetFileName()'   - Add new implementation method 'org.hibernate.tool.orm.jbt.wrp.setFilename(String)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/QueryExporterWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/QueryExporterWrapper.java
@@ -1,7 +1,12 @@
 package org.hibernate.tool.orm.jbt.wrp;
 
+import org.hibernate.tool.api.export.ExporterConstants;
 import org.hibernate.tool.internal.export.query.QueryExporter;
 
 public class QueryExporterWrapper extends QueryExporter {
+
+	public void setFilename(String fileName) {
+		getProperties().put(ExporterConstants.OUTPUT_FILE_NAME, fileName);
+	}
 
 }
