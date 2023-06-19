b441b63c0535cd382cf0fa4764c98402f1baa606
Koen Aers <koen.aers@gmail.com>
2023-06-19 13:12:30 +0200

    HBX-2390: Create a JBoss Tools adaptation layer in Hibernate Tools   - Add new initial test class 'org.hibernate.tool.orm.jbt.wrp.QueryWrapperFactoryTest'   - Create initial implementation of class 'org.hibernate.tool.orm.jbt.wrp.QueryWrapperFactory'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/QueryWrapperFactory.java
@@ -0,0 +1,34 @@
+package org.hibernate.tool.orm.jbt.wrp;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.hibernate.query.Query;
+
+public class QueryWrapperFactory {
+	
+	public static QueryWrapper<?> createQueryWrapper(Query<?> query) {
+		return (QueryWrapper<?>)Proxy.newProxyInstance(
+				QueryWrapperFactory.class.getClassLoader(), 
+				new Class[] { QueryWrapper.class }, 
+				new QueryWrapperInvocationHandler(query));
+	}
+	
+    static interface QueryWrapper<T> extends Query<T>, Wrapper {
+		
+	}
+    
+    private static class QueryWrapperInvocationHandler implements InvocationHandler {
+    	
+    	private QueryWrapperInvocationHandler(Query<?> q) {
+    	}
+
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			return null;
+		}
+    	
+    }
+
+}
