1be4a54cb3e575331a112bc6371a4bd170d26882
Koen Aers <koen.aers@gmail.com>
2023-06-30 13:58:47 +0200

    HBX-2390: Create a JBoss Tools adaptation layer in Hibernate Tools   - Add new test case 'org.hibernate.tool.orm.jbt.wrp.PropertyWrapperTest#testGetValue()'   - Override implementation of 'getValue()' in 'org.hibernate.tool.orm.jbt.wrp.PropertyWrapper' to wrap the resulting value
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/PropertyWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/PropertyWrapper.java
@@ -1,7 +1,14 @@
 package org.hibernate.tool.orm.jbt.wrp;
 
 import org.hibernate.mapping.Property;
+import org.hibernate.mapping.Value;
 
 public class PropertyWrapper extends Property implements Wrapper {
+	
+	@Override
+	public Value getValue() {
+		Value v = super.getValue();
+		return v == null ? null : ValueWrapperFactory.createValueWrapper(v);
+	}
 
 }
--- jbt/src/test/java/org/hibernate/tool/orm/jbt/wrp/PropertyWrapperTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/wrp/PropertyWrapperTest.java
@@ -1,7 +1,11 @@
 package org.hibernate.tool.orm.jbt.wrp;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import org.hibernate.mapping.BasicValue;
+import org.hibernate.mapping.Value;
+import org.hibernate.tool.orm.jbt.util.DummyMetadataBuildingContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +21,14 @@ public class PropertyWrapperTest {
 	@Test
 	public void testConstruction() {
 		assertSame(propertyWrapper, propertyWrapper.getWrappedObject());
+	}
+	
+	@Test
+	public void testGetValue() {
+		Value v = new BasicValue(DummyMetadataBuildingContext.INSTANCE);
+		assertNull(propertyWrapper.getValue());
+		propertyWrapper.setValue(v);
+		assertSame(v, ((Wrapper)propertyWrapper.getValue()).getWrappedObject());
 	}
 
 }
