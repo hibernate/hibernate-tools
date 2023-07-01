2bafd230a987c01d965dee0d7d4b9a508931463b
Koen Aers <koen.aers@gmail.com>
2023-07-01 11:25:13 +0200

    HBX-2390: Create a JBoss Tools adaptation layer in Hibernate Tools   - Add new test case 'org.hibernate.tool.orm.jbt.wrp.TableWrapperTest#testGetIdentifierValue()'   - Override method 'getIdentifierValue()' in class 'org.hibernate.tool.orm.jbt.wrp.TableWrapper' to return a wrapped instance of Value
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/TableWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/TableWrapper.java
@@ -1,11 +1,18 @@
 package org.hibernate.tool.orm.jbt.wrp;
 
+import org.hibernate.mapping.KeyValue;
 import org.hibernate.mapping.Table;
 
 public class TableWrapper extends Table implements Wrapper {
 	
 	public TableWrapper(String name) {
 		super("HibernateTools", name);
+	}
+	
+	@Override
+	public KeyValue getIdentifierValue() {
+		KeyValue result = super.getIdentifierValue();
+		return result == null ? null : (KeyValue)ValueWrapperFactory.createValueWrapper(result);
 	}
 
 }
--- jbt/src/test/java/org/hibernate/tool/orm/jbt/wrp/TableWrapperTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/wrp/TableWrapperTest.java
@@ -2,7 +2,11 @@ package org.hibernate.tool.orm.jbt.wrp;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
+import org.hibernate.mapping.BasicValue;
+import org.hibernate.mapping.KeyValue;
+import org.hibernate.tool.orm.jbt.util.DummyMetadataBuildingContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,12 +17,21 @@ public class TableWrapperTest {
 	@BeforeEach
 	public void beforeEach() {
 		tableWrapper = new TableWrapper("foo");
+		KeyValue v = new BasicValue(DummyMetadataBuildingContext.INSTANCE);
+		tableWrapper.setIdentifierValue(v);
 	}
 	
 	@Test
 	public void testConstruction() {
 		assertNotNull(tableWrapper);
 		assertEquals("foo", tableWrapper.getName());
+	}
+	
+	@Test
+	public void testGetIdentifierValue() {
+		KeyValue v = new BasicValue(DummyMetadataBuildingContext.INSTANCE);
+		tableWrapper.setIdentifierValue(v);
+		assertSame(v,  ((Wrapper)tableWrapper.getIdentifierValue()).getWrappedObject());
 	}
 
 }
