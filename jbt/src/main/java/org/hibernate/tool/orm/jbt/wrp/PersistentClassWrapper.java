0b86b8ee22a5690185cf527b2f6506b6965cc011
Koen Aers <koen.aers@gmail.com>
2023-07-03 09:01:58 +0200

    HBX-2390: Create a JBoss Tools adaptation layer in Hibernate Tools   - Create new class 'org.hibernate.tool.orm.jbt.wrp.DelegatingPersistentClassWrapperImpl' as a subclass of 'org.hibernate.mapping.RootClass' that implements 'org.hibernate.tool.orm.jbt.wrp.PersistentClassWrapper' that delegates to an actual instance of 'org.hibernate.mapping.PersistentClass' and wraps the resulting Value and Property instances   - Reorganize interface 'org.hibernate.tool.orm.jbt.wrp.PersistentClassWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/PersistentClassWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/wrp/PersistentClassWrapper.java
@@ -17,21 +17,30 @@ import org.hibernate.tool.orm.jbt.wrp.PersistentClassWrapperFactory.RootClassWra
 public interface PersistentClassWrapper extends Wrapper {
 
 	default PersistentClass getWrappedObject() { return (PersistentClass)this; }
-	String getEntityName();
-	String getClassName();
 	default boolean isAssignableToRootClass() { return isInstanceOfRootClass(); }
 	default boolean isRootClass() { return getWrappedObject().getClass() == RootClassWrapperImpl.class; }
-	Property getIdentifierProperty();
-	boolean hasIdentifierProperty();
 	default boolean isInstanceOfRootClass() { return RootClass.class.isAssignableFrom(getWrappedObject().getClass()); }
 	default boolean isInstanceOfSubclass() { return Subclass.class.isAssignableFrom(getWrappedObject().getClass()); }
 	default boolean isInstanceOfJoinedSubclass() { return JoinedSubclass.class.isAssignableFrom(getWrappedObject().getClass()); }
+	default Property getProperty() { throw new RuntimeException("getProperty() is only allowed on SpecialRootClass"); }
+	default void setTable(Table table) { throw new RuntimeException("Method 'setTable(Table)' is not supported."); }
+	default void setIdentifier(KeyValue value) { throw new RuntimeException("Method 'setIdentifier(KeyValue)' can only be called on RootClass instances"); }
+	default void setKey(KeyValue value) { throw new RuntimeException("setKey(KeyValue) is only allowed on JoinedSubclass"); }
+	default boolean isInstanceOfSpecialRootClass() { return SpecialRootClass.class.isAssignableFrom(getWrappedObject().getClass()); }
+	default Property getParentProperty() { throw new RuntimeException("getParentProperty() is only allowed on SpecialRootClass"); }
+	default void setIdentifierProperty(Property property) { throw new RuntimeException("setIdentifierProperty(Property) is only allowed on RootClass instances"); }
+	default void setDiscriminator(Value value) { throw new RuntimeException("Method 'setDiscriminator(Value)' can only be called on RootClass instances"); }
+	default boolean isLazyPropertiesCacheable() { throw new RuntimeException("Method 'isLazyPropertiesCacheable()' can only be called on RootClass instances"); }
+
+	String getEntityName();
+	String getClassName();
+	Property getIdentifierProperty();
+	boolean hasIdentifierProperty();
 	PersistentClass getRootClass();
 	Iterator<Property> getPropertyClosureIterator();
 	PersistentClass getSuperclass();
 	Iterator<Property> getPropertyIterator();
 	Property getProperty(String name);
-	default Property getProperty() { throw new RuntimeException("getProperty() is only allowed on SpecialRootClass"); }
 	Table getTable();
 	Boolean isAbstract();
 	Value getDiscriminator();
@@ -43,13 +52,6 @@ public interface PersistentClassWrapper extends Wrapper {
 	void setDiscriminatorValue(String str);
 	void setAbstract(Boolean b);
 	void addProperty(Property p);
-	default void setTable(Table table) { throw new RuntimeException("Method 'setTable(Table)' is not supported."); }
-	default void setIdentifier(KeyValue value) { throw new RuntimeException("Method 'setIdentifier(KeyValue)' can only be called on RootClass instances"); }
-	default void setKey(KeyValue value) { throw new RuntimeException("setKey(KeyValue) is only allowed on JoinedSubclass"); }
-	default boolean isInstanceOfSpecialRootClass() { return SpecialRootClass.class.isAssignableFrom(getWrappedObject().getClass()); }
-	default Property getParentProperty() { throw new RuntimeException("getParentProperty() is only allowed on SpecialRootClass"); }
-	default void setIdentifierProperty(Property property) { throw new RuntimeException("setIdentifierProperty(Property) is only allowed on RootClass instances"); }
-	default void setDiscriminator(Value value) { throw new RuntimeException("Method 'setDiscriminator(Value)' can only be called on RootClass instances"); }
 	void setProxyInterfaceName(String name);
 	void setLazy(boolean b);
 	Iterator<Subclass> getSubclassIterator();
@@ -64,7 +66,6 @@ public interface PersistentClassWrapper extends Wrapper {
 	boolean isInherited();
 	boolean isJoinedSubclass();
 	boolean isLazy();
-	default boolean isLazyPropertiesCacheable() { throw new RuntimeException("Method 'isLazyPropertiesCacheable()' can only be called on RootClass instances"); }
 	boolean isMutable();
 	boolean isPolymorphic();
 	boolean isVersioned();
