e4deeaf0a1bd96a5e8fb2155f24b8739c1078b7f
Koen Aers <koen.aers@gmail.com>
2024-01-02 16:43:34 +0100

    HBX-2665: Update version identifier to 6.2.16-SNAPSHOT
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/src/main/java/org/hibernate/tool/api/version/Version.java
+++ orm/src/main/java/org/hibernate/tool/api/version/Version.java
@@ -2,6 +2,6 @@ package org.hibernate.tool.api.version;
 
 public interface Version {
 	
-	final static String CURRENT_VERSION = "6.2.15.Final";
+	final static String CURRENT_VERSION = "6.2.16-SNAPSHOT";
 
 }
