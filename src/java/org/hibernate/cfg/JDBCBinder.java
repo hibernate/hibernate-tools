4b0ad0fceb8348178676c5b9e5ce983e2cc7bd30
Adrien Ferrand <ferrand.ad@gmail.com>
2024-01-08 09:41:38 +0100

    HBX-2716 - Remove floating point types from types advertised to support precision & scale
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- src/java/org/hibernate/cfg/JDBCBinder.java
+++ src/java/org/hibernate/cfg/JDBCBinder.java
@@ -24,7 +24,7 @@ import org.hibernate.cfg.binder.PropertyBinder;
 import org.hibernate.cfg.reveng.AssociationInfo;
 import org.hibernate.cfg.reveng.DatabaseCollector;
 import org.hibernate.cfg.reveng.JDBCReader;
-import org.hibernate.cfg.reveng.JDBCToHibernateTypeHelper;
+import org.hibernate.cfg.reveng.JdbcToHibernateTypeHelper;
 import org.hibernate.cfg.reveng.MappingsDatabaseCollector;
 import org.hibernate.cfg.reveng.RevEngUtils;
 import org.hibernate.cfg.reveng.ReverseEngineeringStrategy;
@@ -900,7 +900,7 @@ public class JDBCBinder {
 	}
 
 	private String typeCodeName(int sqlTypeCode) {
-		return sqlTypeCode + "(" + JDBCToHibernateTypeHelper.getJDBCTypeName(sqlTypeCode) + ")";
+		return sqlTypeCode + "(" + JdbcToHibernateTypeHelper.getJDBCTypeName(sqlTypeCode) + ")";
 	}
 
 	/**
