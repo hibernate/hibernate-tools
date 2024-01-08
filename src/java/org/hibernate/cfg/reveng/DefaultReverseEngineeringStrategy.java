bf3b0d4fe9ef5629cdb3149f32b42e4ee0ea6bb5
Adrien Ferrand <ferrand.ad@gmail.com>
2024-01-08 09:41:38 +0100

    HBX-2716 - Remove floating point types from types advertised to support precision & scale
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- src/java/org/hibernate/cfg/reveng/DefaultReverseEngineeringStrategy.java
+++ src/java/org/hibernate/cfg/reveng/DefaultReverseEngineeringStrategy.java
@@ -105,11 +105,11 @@ public class DefaultReverseEngineeringStrategy implements ReverseEngineeringStra
     }
 	
 	public String columnToHibernateTypeName(TableIdentifier table, String columnName, int sqlType, int length, int precision, int scale, boolean nullable, boolean generatedIdentifier) {
-		String preferredHibernateType = JDBCToHibernateTypeHelper.getPreferredHibernateType(sqlType, length, precision, scale, nullable, generatedIdentifier);
+		String preferredHibernateType = JdbcToHibernateTypeHelper.getPreferredHibernateType(sqlType, length, precision, scale, nullable, generatedIdentifier);
 		
 		String location = "<no info>";
 		if(log.isDebugEnabled()) {
-			String info = " t:" + JDBCToHibernateTypeHelper.getJDBCTypeName( sqlType ) + " l:" + length + " p:" + precision + " s:" + scale + " n:" + nullable + " id:" + generatedIdentifier;
+			String info = " t:" + JdbcToHibernateTypeHelper.getJDBCTypeName( sqlType ) + " l:" + length + " p:" + precision + " s:" + scale + " n:" + nullable + " id:" + generatedIdentifier;
 			if(table!=null) {
 				location = TableNameQualifier.qualify(table.getCatalog(), table.getSchema(), table.getName() ) + "." + columnName + info;
 			} else {
