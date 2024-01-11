504a3f7d9d66dc4ee8ecd0ba76c17e01e5deae95
Koen Aers <koen.aers@gmail.com>
2024-01-11 09:53:02 +0100

    HBX-2686: Update Hibernate ORM Dependency to Version 6.2.19-SNAPSHOT
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/src/main/java/org/hibernate/tool/api/version/Version.java
+++ orm/src/main/java/org/hibernate/tool/api/version/Version.java
@@ -2,6 +2,6 @@ package org.hibernate.tool.api.version;
 
 public interface Version {
 	
-	final static String CURRENT_VERSION = "6.2.18.Final";
+	final static String CURRENT_VERSION = "6.2.18-SNAPSHOT";
 
 }
