511fd2a3393927131e4eb06a31ab573804ecfdfa
Koen Aers <koen.aers@gmail.com>
2024-03-01 16:23:50 +0100

    HBX-2741: Create Interface for ColumnWrapper   - Add new test case 'org.hibernate.tool.orm.jbt.api.ColumnWrapperTest#testGetName()'   - Add new default interface method 'org.hibernate.tool.orm.jbt.api.ColumnWrapper#getName()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ColumnWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ColumnWrapper.java
@@ -1,7 +1,10 @@
 package org.hibernate.tool.orm.jbt.api;
 
+import org.hibernate.mapping.Column;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface ColumnWrapper extends Wrapper {
+	
+	default String getName() { return ((Column)getWrappedObject()).getName(); }
 
 }
--- jbt/src/test/java/org/hibernate/tool/orm/jbt/api/ColumnWrapperTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/api/ColumnWrapperTest.java
@@ -1,7 +1,10 @@
 package org.hibernate.tool.orm.jbt.api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
+import org.hibernate.mapping.Column;
 import org.hibernate.tool.orm.jbt.internal.factory.ColumnWrapperFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,15 +12,24 @@ import org.junit.jupiter.api.Test;
 public class ColumnWrapperTest {
 
 	private ColumnWrapper columnWrapper = null; 
+	private Column wrappedColumn = null;
 	
 	@BeforeEach
 	public void beforeEach() throws Exception {
-		columnWrapper = ColumnWrapperFactory.createColumnWrapper("foo");
+		columnWrapper = ColumnWrapperFactory.createColumnWrapper(null);
+		wrappedColumn = (Column)columnWrapper.getWrappedObject();
 	}
 	
 	@Test
 	public void testConstruction() {
 		assertNotNull(columnWrapper);
+	}
+	
+	@Test
+	public void testGetName() {
+		assertNull(columnWrapper.getName());
+		wrappedColumn.setName("foobar");
+		assertEquals("foobar", columnWrapper.getName());
 	}
 	
 }
