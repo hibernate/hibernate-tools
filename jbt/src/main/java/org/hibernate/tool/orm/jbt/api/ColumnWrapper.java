0339e1e365b00a2c00607fdf5c4fa1de5ef331ee
Koen Aers <koen.aers@gmail.com>
2024-03-04 14:11:03 +0100

    HBX-2741: Create Interface for ColumnWrapper   - Add new test case 'org.hibernate.tool.orm.jbt.api.ColumnWrapperTest#testGetSqlType()'   - Add new default interface method 'org.hibernate.tool.orm.jbt.api.ColumnWrapper#getSqlType()'   - Add new default interface method 'org.hibernate.tool.orm.jbt.api.ColumnWrapper#getSqlType(Configuration)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ColumnWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ColumnWrapper.java
@@ -1,11 +1,16 @@
 package org.hibernate.tool.orm.jbt.api;
 
+import org.hibernate.cfg.Configuration;
 import org.hibernate.mapping.Column;
+import org.hibernate.tool.orm.jbt.util.MetadataHelper;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface ColumnWrapper extends Wrapper {
 	
 	default String getName() { return ((Column)getWrappedObject()).getName(); }
 	default Integer getSqlTypeCode() { return ((Column)getWrappedObject()).getSqlTypeCode(); }
+	default String getSqlType() { return ((Column)getWrappedObject()).getSqlType(); }
+	default String getSqlType(Configuration configuration) { return ((Column)getWrappedObject()).getSqlType(MetadataHelper.getMetadata(configuration));
+	}
 
 }
--- jbt/src/test/java/org/hibernate/tool/orm/jbt/api/ColumnWrapperTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/api/ColumnWrapperTest.java
@@ -4,8 +4,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.mapping.Column;
+import org.hibernate.mapping.Value;
 import org.hibernate.tool.orm.jbt.internal.factory.ColumnWrapperFactory;
+import org.hibernate.tool.orm.jbt.util.MockConnectionProvider;
+import org.hibernate.tool.orm.jbt.util.MockDialect;
+import org.hibernate.type.spi.TypeConfiguration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -37,6 +47,38 @@ public class ColumnWrapperTest {
 		assertNull(columnWrapper.getSqlTypeCode());
 		wrappedColumn.setSqlTypeCode(Integer.MAX_VALUE);
 		assertEquals(Integer.MAX_VALUE, columnWrapper.getSqlTypeCode().intValue());
+	}
+
+	@Test
+	public void testGetSqlType() {
+		// IColumn#getSqlType()
+		assertNull(columnWrapper.getSqlType());
+		wrappedColumn.setSqlType("foobar");
+		assertEquals("foobar", columnWrapper.getSqlType());
+		// IColumn#getSqlType(IConfiguration)
+		columnWrapper = ColumnWrapperFactory.createColumnWrapper(null);
+		wrappedColumn = (Column)columnWrapper.getWrappedObject();
+		wrappedColumn.setValue(createValue());
+		Configuration configuration = new Configuration();
+		configuration.setProperty(AvailableSettings.DIALECT, MockDialect.class.getName());
+		configuration.setProperty(AvailableSettings.CONNECTION_PROVIDER, MockConnectionProvider.class.getName());
+		assertEquals("integer", columnWrapper.getSqlType(configuration));
+	}
+	
+	private Value createValue() {
+		return (Value)Proxy.newProxyInstance(
+				getClass().getClassLoader(), 
+				new Class[] { Value.class }, 
+				new InvocationHandler() {					
+					@Override
+					public Object invoke(Object proxy, Method method, Object[] args) 
+							throws Throwable {
+						if (method.getName().equals("getType")) {
+							return new TypeConfiguration().getBasicTypeForJavaType(Integer.class);
+						}
+						return null;
+					}
+				});
 	}
 
 }
