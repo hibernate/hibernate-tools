bcdefa62b79c2c8dec8028669f1f9fcad4576090
Koen Aers <koen.aers@gmail.com>
2024-03-05 13:47:12 +0100

    HBX-2742: Create Interface for ConfigurationWrapper   - Add new test case 'org.hibernate.tool.orm.jbt.api.ConfigurationWrapperTest#testAddFile()'   - Add new default interface method 'org.hibernate.tool.orm.jbt.api.ConfigurationWrapper#addFile(File)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ConfigurationWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ConfigurationWrapper.java
@@ -1,10 +1,13 @@
 package org.hibernate.tool.orm.jbt.api;
 
+import java.io.File;
+
 import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface ConfigurationWrapper extends Wrapper {
 
 	default String getProperty(String property) { return ((Configuration)getWrappedObject()).getProperty(property); }
+	default ConfigurationWrapper addFile(File file) { ((Configuration)getWrappedObject()).addFile(file); return this; }
 	
 }
--- jbt/src/test/java/org/hibernate/tool/orm/jbt/api/ConfigurationWrapperTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/api/ConfigurationWrapperTest.java
@@ -1,11 +1,21 @@
 package org.hibernate.tool.orm.jbt.api;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.File;
+import java.io.PrintWriter;
+
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.jaxb.spi.Binding;
 import org.hibernate.tool.orm.jbt.internal.factory.ConfigurationWrapperFactory;
 import org.hibernate.tool.orm.jbt.util.JpaConfiguration;
+import org.hibernate.tool.orm.jbt.util.MetadataHelper;
 import org.hibernate.tool.orm.jbt.util.NativeConfiguration;
 import org.hibernate.tool.orm.jbt.util.RevengConfiguration;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +23,13 @@ import org.junit.jupiter.api.Test;
 
 public class ConfigurationWrapperTest {
 
+	private static final String TEST_HBM_XML_STRING =
+			"<hibernate-mapping package='org.jboss.tools.hibernate.orm.runtime.v_6_5'>" +
+			"  <class name='IConfigurationTest$Foo'>" + 
+			"    <id name='id'/>" +
+			"  </class>" +
+			"</hibernate-mapping>";
+	
 	private ConfigurationWrapper nativeConfigurationWrapper = null;
 	private NativeConfiguration wrappedNativeConfiguration = null;
 	private ConfigurationWrapper revengConfigurationWrapper = null;
@@ -60,4 +77,40 @@ public class ConfigurationWrapperTest {
 		assertEquals("bar", jpaConfigurationWrapper.getProperty("foo"));
 	}
 
+	@Test
+	public void testAddFile() throws Exception {
+		File testFile = File.createTempFile("test", "hbm.xml");
+		PrintWriter printWriter = new PrintWriter(testFile);
+		printWriter.write(TEST_HBM_XML_STRING);
+		printWriter.close();
+		testFile.deleteOnExit();
+		// For native configuration
+		MetadataSources metadataSources = MetadataHelper.getMetadataSources(wrappedNativeConfiguration);
+		assertTrue(metadataSources.getXmlBindings().isEmpty());
+		assertSame(
+				nativeConfigurationWrapper,
+				nativeConfigurationWrapper.addFile(testFile));
+		assertFalse(metadataSources.getXmlBindings().isEmpty());
+		Binding<?> binding = metadataSources.getXmlBindings().iterator().next();
+		assertEquals(testFile.getAbsolutePath(), binding.getOrigin().getName());
+		// For reveng configuration
+		try {
+			revengConfigurationWrapper.addFile(testFile);
+			fail();
+		} catch (RuntimeException e) {
+			assertEquals(
+					e.getMessage(),
+					"Method 'addFile' should not be called on instances of " + RevengConfiguration.class.getName());
+		}
+		// For jpa configuration
+		try {
+			jpaConfigurationWrapper.addFile(testFile);
+			fail();
+		} catch (RuntimeException e) {
+			assertEquals(
+					e.getMessage(),
+					"Method 'addFile' should not be called on instances of " + JpaConfiguration.class.getName());
+		}
+	}
+	
 }
