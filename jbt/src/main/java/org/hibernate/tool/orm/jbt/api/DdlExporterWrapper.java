e27812155d9c5bca822e889ffd133275ca082154
Koen Aers <koen.aers@gmail.com>
2024-03-14 17:11:52 +0200

    HBX-2749: Create Interface for DdlExporterWrapper   - Add new test case 'org.hibernate.tool.orm.jbt.api.DdlExporterWrapperTest#testSetExport()'   - Add new default interface method 'org.hibernate.tool.orm.jbt.api.DdlExporterWrapper#setExport(boolean)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/DdlExporterWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/DdlExporterWrapper.java
@@ -1,7 +1,14 @@
 package org.hibernate.tool.orm.jbt.api;
 
+import org.hibernate.tool.api.export.ExporterConstants;
+import org.hibernate.tool.internal.export.ddl.DdlExporter;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface DdlExporterWrapper extends Wrapper {
+	
+	default void setExport(boolean b) { 
+		((DdlExporter)getWrappedObject()).getProperties().put(
+				ExporterConstants.EXPORT_TO_DATABASE, b);
+	}
 
 }
--- jbt/src/test/java/org/hibernate/tool/orm/jbt/api/DdlExporterWrapperTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/api/DdlExporterWrapperTest.java
@@ -1,7 +1,11 @@
 package org.hibernate.tool.orm.jbt.api;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.hibernate.tool.api.export.ExporterConstants;
 import org.hibernate.tool.internal.export.ddl.DdlExporter;
 import org.hibernate.tool.orm.jbt.internal.factory.DdlExporterWrapperFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,4 +28,13 @@ public class DdlExporterWrapperTest {
 		assertNotNull(wrappedDdlExporter);
 	}
 	
+	@Test
+	public void testSetExport() {
+		assertNull(wrappedDdlExporter.getProperties().get(ExporterConstants.EXPORT_TO_DATABASE));
+		ddlExporterWrapper.setExport(false);
+		assertFalse((Boolean)wrappedDdlExporter.getProperties().get(ExporterConstants.EXPORT_TO_DATABASE));
+		ddlExporterWrapper.setExport(true);
+		assertTrue((Boolean)wrappedDdlExporter.getProperties().get(ExporterConstants.EXPORT_TO_DATABASE));
+	}
+
 }
