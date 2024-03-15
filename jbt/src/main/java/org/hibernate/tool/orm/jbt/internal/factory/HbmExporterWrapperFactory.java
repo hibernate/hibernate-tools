63dde36403228f13b9986559b02e36d1ac67d894
Koen Aers <koen.aers@gmail.com>
2024-03-15 15:22:07 +0200

    HBX-2750: Create Interface for HbmExporterWrapper   - Amend test case 'org.hibernate.tool.orm.jbt.api.HbmExporterWrapperTest#testConstruction()'   - Amend factory method 'org.hibernate.tool.orm.jbt.internal.factory.HbmExporterWrapperFactory#createHbmExporterWrapper(Configuration,File)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/HbmExporterWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/HbmExporterWrapperFactory.java
@@ -1,14 +1,32 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
+import java.io.File;
+
+import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.internal.export.hbm.HbmExporter;
 import org.hibernate.tool.orm.jbt.api.HbmExporterWrapper;
+import org.hibernate.tool.orm.jbt.util.ConfigurationMetadataDescriptor;
 
 public class HbmExporterWrapperFactory {
 
-	public static HbmExporterWrapper createHbmExporterWrapper(final HbmExporter wrappedHbmExporter) {
+	public static HbmExporterWrapper createHbmExporterWrapper(Configuration cfg, File f) {
+		final HbmExporterExtension wrappedHbmExporterExtension = new HbmExporterExtension(cfg, f);
 		return new HbmExporterWrapper() {
-			@Override public HbmExporter getWrappedObject() { return wrappedHbmExporter; }
+			@Override public HbmExporter getWrappedObject() { return wrappedHbmExporterExtension; }
 		};
+	}
+	
+	private static class HbmExporterExtension extends HbmExporter {
+		
+		private HbmExporterExtension(Configuration cfg, File f) {
+			getProperties().put(
+					METADATA_DESCRIPTOR, 
+					new ConfigurationMetadataDescriptor(cfg));
+			if (f != null) {
+				getProperties().put(OUTPUT_FILE_NAME, f);
+			}
+		}
+		
 	}
 	
 }
--- jbt/src/test/java/org/hibernate/tool/orm/jbt/api/HbmExporterWrapperTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/api/HbmExporterWrapperTest.java
@@ -1,27 +1,53 @@
 package org.hibernate.tool.orm.jbt.api;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.File;
+import java.lang.reflect.Field;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.api.export.ExporterConstants;
 import org.hibernate.tool.internal.export.hbm.HbmExporter;
 import org.hibernate.tool.orm.jbt.internal.factory.HbmExporterWrapperFactory;
+import org.hibernate.tool.orm.jbt.util.ConfigurationMetadataDescriptor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 public class HbmExporterWrapperTest {
 
 	private HbmExporterWrapper hbmExporterWrapper = null; 
 	private HbmExporter wrappedHbmExporter = null;
+
+	private Configuration cfg = null;
+	private File f = null;
+	
+	@TempDir private File tempFolder;
 	
 	@BeforeEach
 	public void beforeEach() {
-		wrappedHbmExporter = new HbmExporter();
-		hbmExporterWrapper = HbmExporterWrapperFactory.createHbmExporterWrapper(wrappedHbmExporter);
+		cfg = new Configuration();
+		f = new File(tempFolder, "foo");
+		hbmExporterWrapper = HbmExporterWrapperFactory.createHbmExporterWrapper(cfg, f);
+		wrappedHbmExporter = (HbmExporter)hbmExporterWrapper.getWrappedObject();
 	}
 	
 	@Test
-	public void testConstruction() {
+	public void testConstruction() throws Exception {
+		assertTrue(tempFolder.exists());
+		assertFalse(f.exists());
 		assertNotNull(wrappedHbmExporter);
 		assertNotNull(hbmExporterWrapper);
+		assertSame(f, wrappedHbmExporter.getProperties().get(ExporterConstants.OUTPUT_FILE_NAME));
+		ConfigurationMetadataDescriptor descriptor = (ConfigurationMetadataDescriptor)wrappedHbmExporter
+				.getProperties().get(ExporterConstants.METADATA_DESCRIPTOR);
+		assertNotNull(descriptor);
+		Field configurationField = ConfigurationMetadataDescriptor.class.getDeclaredField("configuration");
+		configurationField.setAccessible(true);
+		assertSame(cfg, configurationField.get(descriptor));
 	}
 	
 }
