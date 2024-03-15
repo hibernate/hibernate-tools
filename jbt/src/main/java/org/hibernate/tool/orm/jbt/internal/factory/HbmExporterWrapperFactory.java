0e1d85afe5451f006fb3b6f785da7c1318d416e8
Koen Aers <koen.aers@gmail.com>
2024-03-15 14:55:54 +0200

    HBX-2750: Create Interface for HbmExporterWrapper   - Add new test case 'org.hibernate.tool.orm.jbt.api.HbmExporterWrapperTest#testConstruction()'   - Add factory method 'org.hibernate.tool.orm.jbt.internal.factory.HbmExporterWrapperFactory#createHbmExporterWrapper(HbmExporter)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/HbmExporterWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/HbmExporterWrapperFactory.java
@@ -1,5 +1,14 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
+import org.hibernate.tool.internal.export.hbm.HbmExporter;
+import org.hibernate.tool.orm.jbt.api.HbmExporterWrapper;
+
 public class HbmExporterWrapperFactory {
 
+	public static HbmExporterWrapper createHbmExporterWrapper(final HbmExporter wrappedHbmExporter) {
+		return new HbmExporterWrapper() {
+			@Override public HbmExporter getWrappedObject() { return wrappedHbmExporter; }
+		};
+	}
+	
 }
--- jbt/src/test/java/org/hibernate/tool/orm/jbt/api/HbmExporterWrapperTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/api/HbmExporterWrapperTest.java
@@ -1,5 +1,27 @@
 package org.hibernate.tool.orm.jbt.api;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.hibernate.tool.internal.export.hbm.HbmExporter;
+import org.hibernate.tool.orm.jbt.internal.factory.HbmExporterWrapperFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class HbmExporterWrapperTest {
 
+	private HbmExporterWrapper hbmExporterWrapper = null; 
+	private HbmExporter wrappedHbmExporter = null;
+	
+	@BeforeEach
+	public void beforeEach() {
+		wrappedHbmExporter = new HbmExporter();
+		hbmExporterWrapper = HbmExporterWrapperFactory.createHbmExporterWrapper(wrappedHbmExporter);
+	}
+	
+	@Test
+	public void testConstruction() {
+		assertNotNull(wrappedHbmExporter);
+		assertNotNull(hbmExporterWrapper);
+	}
+	
 }
