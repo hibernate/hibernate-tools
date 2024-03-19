22d381c6f4537aab0f956e7edd1ede343e7131d5
Koen Aers <koen.aers@gmail.com>
2024-03-19 12:04:16 +0100

    HBX-2754: Create Interface for JoinWrapper   - Add new test case 'org.hibernate.tool.orm.jbt.api.HqlCompletionProposalWrapperTest#testGetPropertyIterator()'   - Add new test case 'org.hibernate.tool.orm.jbt.api.HqlCompletionProposalWrapperTest#getPropertyIterator()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/test/java/org/hibernate/tool/orm/jbt/api/JoinWrapperTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/api/JoinWrapperTest.java
@@ -1,8 +1,14 @@
 package org.hibernate.tool.orm.jbt.api;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Iterator;
 
 import org.hibernate.mapping.Join;
+import org.hibernate.mapping.Property;
 import org.hibernate.tool.orm.jbt.internal.factory.JoinWrapperFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,4 +30,16 @@ public class JoinWrapperTest {
 		assertNotNull(joinWrapper);
 	}
 	
+	@Test
+	public void testGetPropertyIterator() {
+		Iterator<Property> propertyIterator = joinWrapper.getPropertyIterator();
+		assertFalse(propertyIterator.hasNext());
+		Property property = new Property();
+		wrappedJoin.addProperty(property);
+		propertyIterator = joinWrapper.getPropertyIterator();
+		assertTrue(propertyIterator.hasNext());
+		Property p = propertyIterator.next();
+		assertSame(p, property);
+	}
+
 }
