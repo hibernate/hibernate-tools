bf8e4cc5b52025bf5bfc9cc5b921a61f528435cd
Koen Aers <koen.aers@gmail.com>
2024-03-20 14:22:12 +0100

    HBX-2735: Create Interface for OverrideRepositoryWrapper   - Add new test case 'org.hibernate.tool.orm.jbt.api.OverrideRepositoryWrapperTest#testAddFile()'   - Add new default interface method 'org.hibernate.tool.orm.jbt.api.addFile(File)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/OverrideRepositoryWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/OverrideRepositoryWrapper.java
@@ -1,7 +1,14 @@
 package org.hibernate.tool.orm.jbt.api;
 
+import java.io.File;
+
+import org.hibernate.tool.internal.reveng.strategy.OverrideRepository;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface OverrideRepositoryWrapper extends Wrapper {
+	
+	default void addFile(File file) {
+		((OverrideRepository)getWrappedObject()).addFile(file);
+	}
 
 }
--- jbt/src/test/java/org/hibernate/tool/orm/jbt/api/OverrideRepositoryWrapperTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/api/OverrideRepositoryWrapperTest.java
@@ -1,13 +1,31 @@
 package org.hibernate.tool.orm.jbt.api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.lang.reflect.Field;
+import java.util.List;
+
+import org.hibernate.mapping.Table;
 import org.hibernate.tool.internal.reveng.strategy.OverrideRepository;
 import org.hibernate.tool.orm.jbt.internal.factory.OverrideRepositoryWrapperFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OverrideRepositoryWrapperTest {
+
+	private static final String HIBERNATE_REVERSE_ENGINEERING_XML =
+			"<?xml version='1.0' encoding='UTF-8'?>                                 "+
+			"<!DOCTYPE hibernate-reverse-engineering PUBLIC                         "+
+			"      '-//Hibernate/Hibernate Reverse Engineering DTD 3.0//EN'         "+
+			"      'http://hibernate.org/dtd/hibernate-reverse-engineering-3.0.dtd'>"+
+			"<hibernate-reverse-engineering>                                        "+
+			"    <table name='FOO'/>                                                "+
+			"</hibernate-reverse-engineering>                                       ";
 
 	private OverrideRepositoryWrapper overrideRepositoryWrapper = null;
 	private OverrideRepository wrappedOverrideRepository = null;
@@ -22,6 +40,29 @@ public class OverrideRepositoryWrapperTest {
 	public void testConstruction() {
 		assertNotNull(wrappedOverrideRepository);
 		assertNotNull(overrideRepositoryWrapper);
+	}
+	
+	@Test
+	public void testAddFile() throws Exception {
+		File file = File.createTempFile("addFile", "tst");
+		file.deleteOnExit();
+		FileWriter fileWriter = new FileWriter(file);
+		fileWriter.write(HIBERNATE_REVERSE_ENGINEERING_XML);
+		fileWriter.close();
+		Field tablesField = wrappedOverrideRepository.getClass().getDeclaredField("tables");
+		tablesField.setAccessible(true);
+		Object object = tablesField.get(wrappedOverrideRepository);
+		List<?> tables = (List<?>)object;
+		assertNotNull(tables);
+		assertTrue(tables.isEmpty());
+		overrideRepositoryWrapper.addFile(file);
+		object = tablesField.get(wrappedOverrideRepository);
+		tables = (List<?>)object;
+		assertNotNull(tables);
+		assertFalse(tables.isEmpty());
+		Table table = (Table)tables.get(0);
+		assertNotNull(table);
+		assertEquals("FOO", table.getName());
 	}
 	
 }
