19b6d5668f1a07e51ccfc68b5e3e8f808c6585e0
Koen Aers <koen.aers@gmail.com>
2024-04-22 17:10:11 +0200

    HBX-2779: Create Interface for TypeWrapper   - Add new test case 'org.hibernate.tool.orm.jbt.api.TypeWrapperTest#testToString()'   - Add new default interface method 'org.hibernate.tool.orm.jbt.api.TypeWrapper#toString(Object)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/TypeWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/TypeWrapper.java
@@ -1,7 +1,30 @@
 package org.hibernate.tool.orm.jbt.api;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
+import org.hibernate.type.BasicType;
+import org.hibernate.type.descriptor.java.CalendarJavaType;
+import org.hibernate.type.descriptor.java.JavaType;
 
 public interface TypeWrapper extends Wrapper {
+
+	default public String toString(Object object) { 
+		if (BasicType.class.isAssignableFrom(getWrappedObject().getClass())) {
+			JavaType<?> javaType = ((BasicType<?>)getWrappedObject()).getJavaTypeDescriptor();
+			if (javaType instanceof CalendarJavaType && object instanceof Calendar) {
+				SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+				return simpleDateFormat.format(((Calendar)object).getTime());
+			} else {
+				return ((JavaType)javaType).toString(object);
+			}
+		} else {
+			throw new UnsupportedOperationException(
+					"Class '" + 
+					getWrappedObject().getClass().getName() + 
+					"' does not support 'toString(Object)'." ); 
+		}
+	}
 
 }
--- jbt/src/test/java/org/hibernate/tool/orm/jbt/api/TypeWrapperTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/api/TypeWrapperTest.java
@@ -1,8 +1,12 @@
 package org.hibernate.tool.orm.jbt.api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.hibernate.tool.orm.jbt.internal.factory.TypeWrapperFactory;
+import org.hibernate.type.ArrayType;
 import org.hibernate.type.spi.TypeConfiguration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -23,4 +27,23 @@ public class TypeWrapperTest {
 				typeConfiguration.getBasicTypeForJavaType(int.class)));
 	}
 
+	@Test
+	public void testToString() {
+		// first try type that is string representable
+		TypeWrapper classTypeWrapper = TypeWrapperFactory.createTypeWrapper(
+				typeConfiguration.getBasicTypeForJavaType(Class.class));
+		assertEquals(
+				TypeWrapperTest.class.getName(), 
+				classTypeWrapper.toString(TypeWrapperTest.class));
+		// next try a type that cannot be represented by a string
+		try {
+			TypeWrapper arrayTypeWrapper = 
+					TypeWrapperFactory.createTypeWrapper(new ArrayType("foo", "bar", String.class));
+			arrayTypeWrapper.toString(new String[] { "foo", "bar" });
+			fail();
+		} catch (UnsupportedOperationException e) {
+			assertTrue(e.getMessage().contains("does not support 'toString(Object)'"));
+		}
+	}
+	
 }
