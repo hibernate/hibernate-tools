ee7b7e1edd8a80df0db4b5a15a5ca5f62d1b2060
Koen Aers <koen.aers@gmail.com>
2024-05-03 13:50:50 +0200

    HBX-2781: Create Interface for ValueWrapper   - Add new test case 'org.hibernate.tool.orm.jbt.api.ValueWrapperTest#testIsCollection()'   - Add new default interface method 'org.hibernate.tool.orm.jbt.api.ValueWrapper#isCollection()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ValueWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ValueWrapper.java
@@ -1,10 +1,12 @@
 package org.hibernate.tool.orm.jbt.api;
 
+import org.hibernate.mapping.Collection;
 import org.hibernate.mapping.Value;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface ValueWrapper extends Wrapper {
 
 	default boolean isSimpleValue() { return ((Value)getWrappedObject()).isSimpleValue(); }
+	default boolean isCollection() { return Collection.class.isAssignableFrom(getWrappedObject().getClass()); }
 
 }
