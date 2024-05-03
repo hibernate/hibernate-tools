d74efef449d08f38f41886c4ba554ac585de52d7
Koen Aers <koen.aers@gmail.com>
2024-05-03 14:33:18 +0200

    HBX-2781: Create Interface for ValueWrapper   - Add new test case 'org.hibernate.tool.orm.jbt.api.ValueWrapperTest#testGetCollectionElement()'   - Add new default interface method 'org.hibernate.tool.orm.jbt.api.ValueWrapper#getCollectionElement()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ValueWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ValueWrapper.java
@@ -2,11 +2,19 @@ package org.hibernate.tool.orm.jbt.api;
 
 import org.hibernate.mapping.Collection;
 import org.hibernate.mapping.Value;
+import org.hibernate.tool.orm.jbt.internal.factory.ValueWrapperFactory;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface ValueWrapper extends Wrapper {
 
 	default boolean isSimpleValue() { return ((Value)getWrappedObject()).isSimpleValue(); }
 	default boolean isCollection() { return Collection.class.isAssignableFrom(getWrappedObject().getClass()); }
+	default ValueWrapper getCollectionElement() {
+		if (isCollection()) {
+			Value v = ((Collection)getWrappedObject()).getElement();
+			if (v != null) return ValueWrapperFactory.createValueWrapper(v);
+		}
+		return null;
+	}
 
 }
--- jbt/src/test/java/org/hibernate/tool/orm/jbt/api/ValueWrapperTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/api/ValueWrapperTest.java
@@ -2,12 +2,15 @@ package org.hibernate.tool.orm.jbt.api;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.hibernate.mapping.Any;
 import org.hibernate.mapping.Array;
 import org.hibernate.mapping.Bag;
 import org.hibernate.mapping.BasicValue;
+import org.hibernate.mapping.Collection;
 import org.hibernate.mapping.Component;
 import org.hibernate.mapping.DependantValue;
 import org.hibernate.mapping.IdentifierBag;
@@ -58,63 +61,64 @@ public class ValueWrapperTest {
 	private Value wrappedAnyValue = null;
 	private ValueWrapper identifierBagValueWrapper = null;
 	private Value wrappedIdentifierBagValue = null;
-	
+
 	private PersistentClassWrapper persistentClassWrapper = null;
 	private PersistentClass wrappedPersistentClass = null;
-	
+
 	private Table table = null;
-	
-	@BeforeEach 
+
+	@BeforeEach
 	public void beforeEach() {
 		persistentClassWrapper = PersistentClassWrapperFactory.createRootClassWrapper();
 		wrappedPersistentClass = persistentClassWrapper.getWrappedObject();
-		
+
 		table = new Table("HT");
-		
+
 		wrappedArrayValue = new Array(DummyMetadataBuildingContext.INSTANCE, wrappedPersistentClass);
 		arrayValueWrapper = ValueWrapperFactory.createValueWrapper(wrappedArrayValue);
-		
+
 		wrappedBagValue = new Bag(DummyMetadataBuildingContext.INSTANCE, wrappedPersistentClass);
 		bagValueWrapper = ValueWrapperFactory.createValueWrapper(wrappedBagValue);
-		
+
 		wrappedListValue = new List(DummyMetadataBuildingContext.INSTANCE, wrappedPersistentClass);
 		listValueWrapper = ValueWrapperFactory.createValueWrapper(wrappedListValue);
-		
+
 		wrappedManyToOneValue = new ManyToOne(DummyMetadataBuildingContext.INSTANCE, table);
 		manyToOneValueWrapper = ValueWrapperFactory.createValueWrapper(wrappedManyToOneValue);
-		
+
 		wrappedMapValue = new Map(DummyMetadataBuildingContext.INSTANCE, wrappedPersistentClass);
 		mapValueWrapper = ValueWrapperFactory.createValueWrapper(wrappedMapValue);
-		
+
 		wrappedOneToManyValue = new OneToMany(DummyMetadataBuildingContext.INSTANCE, wrappedPersistentClass);
 		oneToManyValueWrapper = ValueWrapperFactory.createValueWrapper(wrappedOneToManyValue);
-		
+
 		wrappedOneToOneValue = new OneToOne(DummyMetadataBuildingContext.INSTANCE, table, wrappedPersistentClass);
 		oneToOneValueWrapper = ValueWrapperFactory.createValueWrapper(wrappedOneToOneValue);
-		
+
 		wrappedPrimitiveArrayValue = new PrimitiveArray(DummyMetadataBuildingContext.INSTANCE, wrappedPersistentClass);
 		primitiveArrayValueWrapper = ValueWrapperFactory.createValueWrapper(wrappedPrimitiveArrayValue);
-		
+
 		wrappedSetValue = new Set(DummyMetadataBuildingContext.INSTANCE, wrappedPersistentClass);
 		setValueWrapper = ValueWrapperFactory.createValueWrapper(wrappedSetValue);
-		
+
 		wrappedSimpleValue = new BasicValue(DummyMetadataBuildingContext.INSTANCE);
 		simpleValueWrapper = ValueWrapperFactory.createValueWrapper(wrappedSimpleValue);
-		
+
 		wrappedComponentValue = new Component(DummyMetadataBuildingContext.INSTANCE, wrappedPersistentClass);
 		componentValueWrapper = ValueWrapperFactory.createValueWrapper(wrappedComponentValue);
-		
-		wrappedDependantValue = new DependantValue(DummyMetadataBuildingContext.INSTANCE, table, (KeyValue)wrappedSimpleValue);
+
+		wrappedDependantValue = new DependantValue(DummyMetadataBuildingContext.INSTANCE, table,
+				(KeyValue) wrappedSimpleValue);
 		dependantValueWrapper = ValueWrapperFactory.createValueWrapper(wrappedDependantValue);
-				
+
 		wrappedAnyValue = new Any(DummyMetadataBuildingContext.INSTANCE, table);
 		anyValueWrapper = ValueWrapperFactory.createValueWrapper(wrappedAnyValue);
-		
+
 		wrappedIdentifierBagValue = new IdentifierBag(DummyMetadataBuildingContext.INSTANCE, wrappedPersistentClass);
 		identifierBagValueWrapper = ValueWrapperFactory.createValueWrapper(wrappedIdentifierBagValue);
-		
+
 	}
-	
+
 	@Test
 	public void testConstruction() {
 		assertNotNull(arrayValueWrapper);
@@ -182,5 +186,37 @@ public class ValueWrapperTest {
 		assertFalse(anyValueWrapper.isCollection());
 		assertTrue(identifierBagValueWrapper.isCollection());
 	}
-	
+
+	@Test
+	public void testGetCollectionElement() {
+		assertNull(arrayValueWrapper.getCollectionElement());
+		((Collection)wrappedArrayValue).setElement(wrappedSimpleValue);
+		assertSame(wrappedSimpleValue, arrayValueWrapper.getCollectionElement().getWrappedObject());
+		assertNull(bagValueWrapper.getCollectionElement());
+		((Collection)wrappedBagValue).setElement(wrappedSimpleValue);
+		assertSame(wrappedSimpleValue,bagValueWrapper.getCollectionElement().getWrappedObject());
+		assertNull(listValueWrapper.getCollectionElement());
+		((Collection)wrappedListValue).setElement(wrappedSimpleValue);
+		assertSame(wrappedSimpleValue, listValueWrapper.getCollectionElement().getWrappedObject());
+		assertNull(manyToOneValueWrapper.getCollectionElement());
+		assertNull(mapValueWrapper.getCollectionElement());
+		((Collection)wrappedMapValue).setElement(wrappedSimpleValue);
+		assertSame(wrappedSimpleValue, mapValueWrapper.getCollectionElement().getWrappedObject());
+		assertNull(oneToManyValueWrapper.getCollectionElement());
+		assertNull(oneToOneValueWrapper.getCollectionElement());
+		assertNull(primitiveArrayValueWrapper.getCollectionElement());
+		((Collection)wrappedPrimitiveArrayValue).setElement(wrappedSimpleValue);
+		assertSame(wrappedSimpleValue, primitiveArrayValueWrapper.getCollectionElement().getWrappedObject());
+		assertNull(setValueWrapper.getCollectionElement());
+		((Collection)wrappedSetValue).setElement(wrappedSimpleValue);
+		assertSame(wrappedSimpleValue, setValueWrapper.getCollectionElement().getWrappedObject());
+		assertNull(simpleValueWrapper.getCollectionElement());
+		assertNull(componentValueWrapper.getCollectionElement());
+		assertNull(dependantValueWrapper.getCollectionElement());
+		assertNull(anyValueWrapper.getCollectionElement());
+		assertNull(identifierBagValueWrapper.getCollectionElement());
+		((Collection)wrappedIdentifierBagValue).setElement(wrappedSimpleValue);
+		assertSame(wrappedSimpleValue, identifierBagValueWrapper.getCollectionElement().getWrappedObject());
+	}
+
 }
