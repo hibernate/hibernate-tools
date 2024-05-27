80454ccc9b81e06257a2cb912a5185f68d961966
Koen Aers <koen.aers@gmail.com>
2024-05-27 19:36:49 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.ColumnWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.ColumnWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.ColumnWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ColumnWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ColumnWrapper.java
@@ -1,9 +1,7 @@
 package org.hibernate.tool.orm.jbt.api;
 
 import org.hibernate.cfg.Configuration;
-import org.hibernate.mapping.Column;
 import org.hibernate.mapping.Value;
-import org.hibernate.tool.orm.jbt.util.MetadataHelper;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface ColumnWrapper extends Wrapper {
@@ -12,30 +10,19 @@ public interface ColumnWrapper extends Wrapper {
 	static final int DEFAULT_PRECISION = 19;
 	static final int DEFAULT_SCALE = 2;
 	
-	default String getName() { return ((Column)getWrappedObject()).getName(); }
-	default Integer getSqlTypeCode() { return ((Column)getWrappedObject()).getSqlTypeCode(); }
-	default String getSqlType() { return ((Column)getWrappedObject()).getSqlType(); }
-	default String getSqlType(Configuration configuration) { 
-		return ((Column)getWrappedObject()).getSqlType(MetadataHelper.getMetadata(configuration)); 
-		}
-	default long getLength() { 
-		Long length = ((Column)getWrappedObject()).getLength();
-		return length == null ? Integer.MIN_VALUE : length; 
-		}
-	default int getDefaultLength() { return DEFAULT_LENGTH; }
-	default int getPrecision() {
-		Integer precision = ((Column)getWrappedObject()).getPrecision();
-		return precision == null ? Integer.MIN_VALUE : precision;
-	}
-	default int getDefaultPrecision() { return DEFAULT_PRECISION; }
-	default int getScale() {
-		Integer scale = ((Column)getWrappedObject()).getScale();
-		return scale == null ? Integer.MIN_VALUE : scale;
-		}
-	default int getDefaultScale() { return DEFAULT_SCALE; }
-	default boolean isNullable() { return ((Column)getWrappedObject()).isNullable(); }
-	default Value getValue() { return ((Column)getWrappedObject()).getValue(); 	}
-	default boolean isUnique() { return ((Column)getWrappedObject()).isUnique(); }
-	default void setSqlType(String sqlType) { ((Column)getWrappedObject()).setSqlType(sqlType); }
+	String getName();
+	Integer getSqlTypeCode();
+	String getSqlType();
+	String getSqlType(Configuration configuration);
+	long getLength();
+	int getDefaultLength();
+	int getPrecision();
+	int getDefaultPrecision();
+	int getScale();
+	int getDefaultScale();
+	boolean isNullable();
+	Value getValue();
+	boolean isUnique();
+	void setSqlType(String sqlType);
 	
 }
--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/ColumnWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/ColumnWrapperFactory.java
@@ -1,15 +1,103 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
+import org.hibernate.cfg.Configuration;
 import org.hibernate.mapping.Column;
+import org.hibernate.mapping.Value;
 import org.hibernate.tool.orm.jbt.api.ColumnWrapper;
+import org.hibernate.tool.orm.jbt.util.MetadataHelper;
 
 public class ColumnWrapperFactory {
 
 	public static ColumnWrapper createColumnWrapper(final String name) {
-		final Column wrappedColumn = new Column(name);
-		return new ColumnWrapper() {
-			@Override public Column getWrappedObject() { return wrappedColumn; }
-		};
+		return new ColumnWrapperImpl(name);
+	}
+	
+	private static class ColumnWrapperImpl implements ColumnWrapper {
+		
+		private Column wrappedColumn = null;
+		
+		private ColumnWrapperImpl(String name) {
+			wrappedColumn = new Column(name);
+		}
+		
+		@Override 
+		public Column getWrappedObject() { 
+			return wrappedColumn; 
+		}
+		
+		@Override
+		public String getName() { 
+			return wrappedColumn.getName(); 
+		}
+		
+		@Override
+		public Integer getSqlTypeCode() { 
+			return wrappedColumn.getSqlTypeCode(); 
+		}
+		
+		@Override
+		public String getSqlType() { 
+			return wrappedColumn.getSqlType(); 
+		}
+		
+		@Override
+		public String getSqlType(Configuration configuration) { 
+			return wrappedColumn.getSqlType(MetadataHelper.getMetadata(configuration)); 
+		}
+		
+		@Override
+		public long getLength() { 
+			Long length = wrappedColumn.getLength();
+			return length == null ? Integer.MIN_VALUE : length; 
+		}
+		
+		@Override
+		public int getDefaultLength() { 
+			return DEFAULT_LENGTH; 
+		}
+		
+		@Override
+		public int getPrecision() {
+			Integer precision = wrappedColumn.getPrecision();
+			return precision == null ? Integer.MIN_VALUE : precision;
+		}
+		
+		@Override
+		public int getDefaultPrecision() { 
+			return DEFAULT_PRECISION; 
+		}
+		
+		@Override
+		public int getScale() {
+			Integer scale = wrappedColumn.getScale();
+			return scale == null ? Integer.MIN_VALUE : scale;
+		}
+		
+		@Override
+		public int getDefaultScale() { 
+			return DEFAULT_SCALE; 
+		}
+		
+		@Override
+		public boolean isNullable() { 
+			return wrappedColumn.isNullable(); 
+		}
+		
+		@Override
+		public Value getValue() { 
+			return wrappedColumn.getValue(); 	
+		}
+		
+		@Override
+		public boolean isUnique() { 
+			return wrappedColumn.isUnique();
+		}
+		
+		@Override
+		public void setSqlType(String sqlType) { 
+			wrappedColumn.setSqlType(sqlType); 
+		}
+		
 	}
 	
 }
