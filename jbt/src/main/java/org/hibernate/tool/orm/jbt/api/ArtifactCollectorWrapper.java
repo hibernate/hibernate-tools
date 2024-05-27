125de682fa3f058d35bae32ccaae242cc5b8b5e6
Koen Aers <koen.aers@gmail.com>
2024-05-27 15:27:18 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.ArtifactCollectorWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.ArtifactCollectorWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.ArtifactCollectorWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ArtifactCollectorWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ArtifactCollectorWrapper.java
@@ -3,13 +3,12 @@ package org.hibernate.tool.orm.jbt.api;
 import java.io.File;
 import java.util.Set;
 
-import org.hibernate.tool.api.export.ArtifactCollector;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface ArtifactCollectorWrapper extends Wrapper {
 
-	default Set<String> getFileTypes() { return ((ArtifactCollector)getWrappedObject()).getFileTypes(); }
-	default void formatFiles() { ((ArtifactCollector)getWrappedObject()).formatFiles(); }
-	default File[] getFiles(String string) { return ((ArtifactCollector)getWrappedObject()).getFiles(string); }
+	Set<String> getFileTypes();
+	void formatFiles();
+	File[] getFiles(String string);
 
 }
--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/ArtifactCollectorWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/ArtifactCollectorWrapperFactory.java
@@ -1,5 +1,8 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
+import java.io.File;
+import java.util.Set;
+
 import org.hibernate.tool.api.export.ArtifactCollector;
 import org.hibernate.tool.internal.export.common.DefaultArtifactCollector;
 import org.hibernate.tool.orm.jbt.api.ArtifactCollectorWrapper;
@@ -7,10 +10,32 @@ import org.hibernate.tool.orm.jbt.api.ArtifactCollectorWrapper;
 public class ArtifactCollectorWrapperFactory {
 	
 	public static ArtifactCollectorWrapper createArtifactCollectorWrapper() {
-		ArtifactCollector wrappedArtifactCollector = new DefaultArtifactCollector();
-		return new ArtifactCollectorWrapper() {
-			@Override public ArtifactCollector getWrappedObject() { return wrappedArtifactCollector; }
-		};
+		return new ArtifactCollectorWrapperImpl();
+	}
+	
+	private static class ArtifactCollectorWrapperImpl implements ArtifactCollectorWrapper {
+		
+		private ArtifactCollector wrappedArtifactCollector = new DefaultArtifactCollector();
+		
+		@Override 
+		public ArtifactCollector getWrappedObject() { 
+			return wrappedArtifactCollector; 
+		}
+
+		@Override
+		public Set<String> getFileTypes() { 
+			return ((ArtifactCollector)getWrappedObject()).getFileTypes(); 
+		}
+
+		@Override
+		public void formatFiles() { 
+			((ArtifactCollector)getWrappedObject()).formatFiles(); 
+		}
+		
+		@Override
+		public File[] getFiles(String string) { 
+			return ((ArtifactCollector)getWrappedObject()).getFiles(string); 
+		}
 	}
 	
 }
