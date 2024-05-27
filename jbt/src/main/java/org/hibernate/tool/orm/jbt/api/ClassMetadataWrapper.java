d88b75905dbc349e67503eea46b286351723efb2
Koen Aers <koen.aers@gmail.com>
2024-05-27 16:39:55 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.ClassMetadataWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.ClassMetadataWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.ClassMetadataWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ClassMetadataWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ClassMetadataWrapper.java
@@ -1,25 +1,22 @@
 package org.hibernate.tool.orm.jbt.api;
 
 import org.hibernate.Session;
-import org.hibernate.engine.spi.SharedSessionContractImplementor;
-import org.hibernate.persister.entity.AbstractEntityPersister;
-import org.hibernate.persister.entity.EntityPersister;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 import org.hibernate.type.Type;
 
 public interface ClassMetadataWrapper extends Wrapper {
 
-	default String getEntityName() { return ((EntityPersister)getWrappedObject()).getEntityName(); }
-	default String getIdentifierPropertyName() { return ((EntityPersister)getWrappedObject()).getIdentifierPropertyName(); }
-	default String[] getPropertyNames() { return ((EntityPersister)getWrappedObject()).getPropertyNames(); }
-	default Type[] getPropertyTypes() { return ((EntityPersister)getWrappedObject()).getPropertyTypes(); }
-	default Class<?> getMappedClass() { return ((EntityPersister)getWrappedObject()).getMappedClass(); }
-	default Type getIdentifierType() { return ((EntityPersister)getWrappedObject()).getIdentifierType(); }
-	default Object getPropertyValue(Object object, String name) { return ((EntityPersister)getWrappedObject()).getPropertyValue(object, name); }
-	default boolean hasIdentifierProperty() { return ((EntityPersister)getWrappedObject()).hasIdentifierProperty(); }
-	default Object getIdentifier(Object object, Session session) { return ((EntityPersister)getWrappedObject()).getIdentifier(object, (SharedSessionContractImplementor)session); }
-	default boolean isInstanceOfAbstractEntityPersister() { return (EntityPersister)getWrappedObject() instanceof AbstractEntityPersister; }
-	default Integer getPropertyIndexOrNull(String id) { return ((EntityPersister)getWrappedObject()).getEntityMetamodel().getPropertyIndexOrNull(id); }
-	default Object getTuplizerPropertyValue(Object entity, int i) { return ((EntityPersister)getWrappedObject()).getValue(entity, i); }
+	String getEntityName();
+	String getIdentifierPropertyName();
+	String[] getPropertyNames();
+	Type[] getPropertyTypes();
+	Class<?> getMappedClass();
+	Type getIdentifierType();
+	Object getPropertyValue(Object object, String name);
+	boolean hasIdentifierProperty();
+	Object getIdentifier(Object object, Session session);
+	boolean isInstanceOfAbstractEntityPersister();
+	Integer getPropertyIndexOrNull(String id);
+	Object getTuplizerPropertyValue(Object entity, int i);
 
 }
