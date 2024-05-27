f8ad9f5c28a25d8ceae7e36bd8362713753894d7
Koen Aers <koen.aers@gmail.com>
2024-05-27 16:17:10 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.Cfg2HbmToolWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.Cfg2HbmToolWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.Cfg2HbmToolWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/Cfg2HbmToolWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/Cfg2HbmToolWrapper.java
@@ -1,37 +1,10 @@
 package org.hibernate.tool.orm.jbt.api;
 
-import org.hibernate.mapping.PersistentClass;
-import org.hibernate.mapping.Property;
-import org.hibernate.mapping.SimpleValue;
-import org.hibernate.tool.internal.export.hbm.Cfg2HbmTool;
-import org.hibernate.tool.internal.export.hbm.HBMTagForValueVisitor;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface Cfg2HbmToolWrapper extends Wrapper {
 
-	default String getTag(PersistentClassWrapper pcw) {
-		return ((Cfg2HbmTool)getWrappedObject()).getTag(pcw.getWrappedObject());
-	}
-	default String getTag(PropertyWrapper pw) {
-		PersistentClass persistentClass = pw.getPersistentClass();
-		if(persistentClass!=null) {
-			Property v = persistentClass.getVersion();
-			if (v instanceof Wrapper) {
-				v = (Property)((Wrapper)v).getWrappedObject();
-			}
-			if(v==pw.getWrappedObject()) {
-				String typeName = ((SimpleValue)pw.getValue()).getTypeName();
-				if("timestamp".equals(typeName) || "dbtimestamp".equals(typeName)) {
-					return "timestamp";
-				} else {
-					return "version";
-				}
-			}
-		}
-		String toolTag = (String) pw.getValue().accept(HBMTagForValueVisitor.INSTANCE);
-		if ("component".equals(toolTag) && "embedded".equals(pw.getPropertyAccessorName())){
-			toolTag = "properties";
-		}
-		return toolTag;
-	}
+	String getTag(PersistentClassWrapper pcw);
+	String getTag(PropertyWrapper pw);
+
 }
