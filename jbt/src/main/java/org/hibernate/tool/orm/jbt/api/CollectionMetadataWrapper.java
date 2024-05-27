a52c757652ee03d0f8eafa3d188680cf6387a204
Koen Aers <koen.aers@gmail.com>
2024-05-27 18:08:36 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Review interface 'org.hibernate.tool.orm.jbt.api.CollectionMetadataWrapper'   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.CollectionMetadataWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.CollectionMetadataWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.CollectionMetadataWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/CollectionMetadataWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/CollectionMetadataWrapper.java
@@ -1,11 +1,9 @@
 package org.hibernate.tool.orm.jbt.api;
 
-import org.hibernate.persister.collection.CollectionPersister;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
-import org.hibernate.type.Type;
 
 public interface CollectionMetadataWrapper extends Wrapper {
 	
-	default Type getElementType() { return ((CollectionPersister)getWrappedObject()).getElementType(); }
+	TypeWrapper getElementType();
 
 }
