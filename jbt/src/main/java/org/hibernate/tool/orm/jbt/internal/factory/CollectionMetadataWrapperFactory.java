8bda8678ddc60c7869f0c726d9ca454249c78e91
Koen Aers <koen.aers@gmail.com>
2024-05-27 18:08:36 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Review interface 'org.hibernate.tool.orm.jbt.api.CollectionMetadataWrapper'   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.CollectionMetadataWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.CollectionMetadataWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.CollectionMetadataWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/CollectionMetadataWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/CollectionMetadataWrapperFactory.java
@@ -2,13 +2,38 @@ package org.hibernate.tool.orm.jbt.internal.factory;
 
 import org.hibernate.persister.collection.CollectionPersister;
 import org.hibernate.tool.orm.jbt.api.CollectionMetadataWrapper;
+import org.hibernate.tool.orm.jbt.api.TypeWrapper;
+import org.hibernate.type.Type;
 
 public class CollectionMetadataWrapperFactory {
 
 	public static CollectionMetadataWrapper createCollectionMetadataWrapper(final CollectionPersister collectionPersister) {
-		return new CollectionMetadataWrapper() {
-			@Override public CollectionPersister getWrappedObject() { return collectionPersister; }
-		};
+		return new CollectionMetadataWrapperImpl(collectionPersister);
+	}
+	
+	private static class CollectionMetadataWrapperImpl implements CollectionMetadataWrapper {
+		
+		private CollectionPersister wrappedCollectionMetadata = null;
+		
+		private TypeWrapper elementTypeWrapper = null;
+		
+		private CollectionMetadataWrapperImpl(CollectionPersister collectionPersister) {
+			wrappedCollectionMetadata = collectionPersister;
+		}
+		
+		@Override 
+		public CollectionPersister getWrappedObject() { 
+			return wrappedCollectionMetadata; 
+		}
+		
+		@Override 
+		public TypeWrapper getElementType() { 
+			Type elementType = getWrappedObject().getElementType();
+			if (elementTypeWrapper == null || elementTypeWrapper.getWrappedObject() != elementType) {
+				elementTypeWrapper = TypeWrapperFactory.createTypeWrapper(elementType);
+			}
+			return elementTypeWrapper; 
+		}
 	}
 	
 }
