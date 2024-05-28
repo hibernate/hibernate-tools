7fa4a055ee529152d1259ae023bf52793aba5a31
Koen Aers <koen.aers@gmail.com>
2024-05-28 17:38:29 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Create new interface 'org.hibernate.tool.orm.jbt.internal.util.ExtendedConfiguration' to unify the three Configuration implementations   - Make the three Configuration implementations an implementation of ExtendedConfiguration   - Adapt factory class 'org.hibernate.tool.orm.jbt.internal.factory.ConfigurationWrapperFactory.ConfigurationWrapperImpl'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/util/JpaConfiguration.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/util/JpaConfiguration.java
@@ -14,6 +14,7 @@ import org.hibernate.jpa.boot.internal.EntityManagerFactoryBuilderImpl;
 import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.Table;
 import org.hibernate.tool.api.reveng.RevengStrategy;
+import org.hibernate.tool.orm.jbt.internal.util.ExtendedConfiguration;
 import org.hibernate.tool.orm.jbt.internal.util.DelegatingPersistentClassWrapperImpl;
 import org.hibernate.tool.orm.jbt.wrp.SessionFactoryWrapper;
 import org.w3c.dom.Document;
@@ -21,7 +22,7 @@ import org.xml.sax.EntityResolver;
 
 import jakarta.persistence.EntityManagerFactory;
 
-public class JpaConfiguration extends Configuration {
+public class JpaConfiguration extends Configuration implements ExtendedConfiguration {
 
 	Metadata metadata = null;
 	SessionFactoryWrapper sessionFactory;
