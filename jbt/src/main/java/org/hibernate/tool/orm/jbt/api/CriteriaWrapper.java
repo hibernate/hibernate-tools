f906a5a2c961ec9b469c3690c181b88d1208ccf6
Koen Aers <koen.aers@gmail.com>
2024-05-29 13:18:32 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.CriteriaWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.CriteriaWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.CriteriaWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/CriteriaWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/CriteriaWrapper.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
-import jakarta.persistence.Query;
-
 public interface CriteriaWrapper extends Wrapper {
 	
-	default void setMaxResults(int intValue) { ((Query)getWrappedObject()).setMaxResults(intValue); }
-	default List<?> list() { return ((Query)getWrappedObject()).getResultList(); }
+	void setMaxResults(int intValue);
+	List<?> list();
 
 }
--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/CriteriaWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/CriteriaWrapperFactory.java
@@ -1,5 +1,7 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
+import java.util.List;
+
 import org.hibernate.tool.orm.jbt.api.CriteriaWrapper;
 
 import jakarta.persistence.Query;
@@ -7,9 +9,32 @@ import jakarta.persistence.Query;
 public class CriteriaWrapperFactory {
 
 	public static CriteriaWrapper createCriteriaWrapper(final Query wrappedCriteria) {
-		return new CriteriaWrapper() {
-			@Override public Query getWrappedObject() { return wrappedCriteria; }
-		};
+		return new CriteriaWrapperImpl(wrappedCriteria);
+	}
+	
+	private static class CriteriaWrapperImpl implements CriteriaWrapper {
+		
+		private Query query = null;
+		
+		private CriteriaWrapperImpl(Query query) {
+			this.query = query;
+		}
+		
+		@Override 
+		public Query getWrappedObject() { 
+			return query; 
+		}
+		
+		@Override 
+		public void setMaxResults(int intValue) { 
+			((Query)getWrappedObject()).setMaxResults(intValue); 
+		}
+		
+		@Override
+		public List<?> list() { 
+			return ((Query)getWrappedObject()).getResultList(); 
+		}
+		
 	}
 	
 }
