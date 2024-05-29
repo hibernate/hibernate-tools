623d82560113a9cf2202fb3a34a256091071c560
Koen Aers <koen.aers@gmail.com>
2024-05-29 14:06:16 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.EnvironmentWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.EnvironmentWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.EnvironmentWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/EnvironmentWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/EnvironmentWrapperFactory.java
@@ -1,13 +1,44 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
+import org.hibernate.cfg.Environment;
 import org.hibernate.tool.orm.jbt.api.EnvironmentWrapper;
 
 public class EnvironmentWrapperFactory {
 	
-	static EnvironmentWrapper ENVIRONMENT_WRAPPER_INSTANCE = new EnvironmentWrapper() {};
+	static EnvironmentWrapper ENVIRONMENT_WRAPPER_INSTANCE = new EnvironmentWrapperImpl();
 
 	public static EnvironmentWrapper createEnvironmentWrapper() {
 		return ENVIRONMENT_WRAPPER_INSTANCE;
+	}
+	
+	private static class EnvironmentWrapperImpl implements EnvironmentWrapper {
+		
+		@Override public String getTransactionManagerStrategy() { return "hibernate.transaction.coordinator_class"; }
+
+		@Override public  String getDriver() { return Environment.DRIVER; }
+
+		@Override public  String getHBM2DDLAuto() { return Environment.HBM2DDL_AUTO; }
+
+		@Override public  String getDialect() { return Environment.DIALECT; }
+
+		@Override public  String getDataSource() { return Environment.DATASOURCE; }
+
+		@Override public  String getConnectionProvider() { return Environment.CONNECTION_PROVIDER; }
+
+		@Override public  String getURL() { return Environment.URL; }
+
+		@Override public  String getUser() { return Environment.USER; }
+
+		@Override public  String getPass() { return Environment.PASS; }
+
+		@Override public  String getSessionFactoryName() { return Environment.SESSION_FACTORY_NAME; }
+
+		@Override public  String getDefaultCatalog() { return Environment.DEFAULT_CATALOG; }
+
+		@Override public  String getDefaultSchema() { return Environment.DEFAULT_SCHEMA; }
+
+		@Override public  Class<Environment> getWrappedClass() { return Environment.class; }
+
 	}
 	
 }
