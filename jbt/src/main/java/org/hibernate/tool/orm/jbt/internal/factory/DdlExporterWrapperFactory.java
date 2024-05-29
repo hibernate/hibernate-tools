b04961de1f8ece5e9ba63873f1e8ea0b13bfa595
Koen Aers <koen.aers@gmail.com>
2024-05-29 13:52:30 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.DdlExporterWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.DdlExporterWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.DdlExporterWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/DdlExporterWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/DdlExporterWrapperFactory.java
@@ -1,14 +1,41 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
+import java.util.Properties;
+
+import org.hibernate.tool.api.export.ExporterConstants;
 import org.hibernate.tool.internal.export.ddl.DdlExporter;
 import org.hibernate.tool.orm.jbt.api.DdlExporterWrapper;
 
 public class DdlExporterWrapperFactory {
 
 	public static DdlExporterWrapper createDdlExporterWrapper(final DdlExporter wrappedDdlExporter) {
-		return new DdlExporterWrapper() {
-			@Override public DdlExporter getWrappedObject() { return wrappedDdlExporter; }
-		};
+		return new DdlExporterWrapperImpl(wrappedDdlExporter);
+	}
+	
+	private static class DdlExporterWrapperImpl implements DdlExporterWrapper {
+		
+		private DdlExporter ddlExporter = null;
+		
+		private DdlExporterWrapperImpl(DdlExporter ddlExporter) {
+			this.ddlExporter = ddlExporter;
+		}
+		
+		@Override 
+		public DdlExporter getWrappedObject() { 
+			return ddlExporter; 
+		}
+		
+		@Override
+		public void setExport(boolean b) { 
+			((DdlExporter)getWrappedObject()).getProperties().put(
+					ExporterConstants.EXPORT_TO_DATABASE, b);
+		}
+		
+		@Override
+		public Properties getProperties() {
+			return ((DdlExporter)getWrappedObject()).getProperties();
+		}
+
 	}
 	
 }
