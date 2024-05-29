fb54057feb1572d75431e3aff71cca01fa3c1ae3
Koen Aers <koen.aers@gmail.com>
2024-05-29 13:52:30 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.DdlExporterWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.DdlExporterWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.DdlExporterWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/DdlExporterWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/DdlExporterWrapper.java
@@ -2,19 +2,11 @@ package org.hibernate.tool.orm.jbt.api;
 
 import java.util.Properties;
 
-import org.hibernate.tool.api.export.ExporterConstants;
-import org.hibernate.tool.internal.export.ddl.DdlExporter;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface DdlExporterWrapper extends Wrapper {
 	
-	default void setExport(boolean b) { 
-		((DdlExporter)getWrappedObject()).getProperties().put(
-				ExporterConstants.EXPORT_TO_DATABASE, b);
-	}
-	
-	default Properties getProperties() {
-		return ((DdlExporter)getWrappedObject()).getProperties();
-	}
+	void setExport(boolean b);
+	Properties getProperties();
 
 }
