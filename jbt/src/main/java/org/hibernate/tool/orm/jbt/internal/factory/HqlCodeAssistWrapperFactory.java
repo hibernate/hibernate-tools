77dc7e37900a825de431a418cc5a0a9104ea751e
Koen Aers <koen.aers@gmail.com>
2024-05-30 17:54:24 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.HqlCodeAssistWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.HqlCodeAssistWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.HqlCodeAssistWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/HqlCodeAssistWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/HqlCodeAssistWrapperFactory.java
@@ -3,21 +3,34 @@ package org.hibernate.tool.orm.jbt.internal.factory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.tool.ide.completion.HQLCodeAssist;
 import org.hibernate.tool.orm.jbt.api.HqlCodeAssistWrapper;
+import org.hibernate.tool.orm.jbt.internal.util.HqlCompletionRequestor;
 
 public class HqlCodeAssistWrapperFactory {
 
 	public static HqlCodeAssistWrapper createHqlCodeAssistWrapper(Metadata m) {
-		final HqlCodeAssistExtension wrappedHqlCodeAssistExtension = new HqlCodeAssistExtension(m);
-		return new HqlCodeAssistWrapper() {
-			@Override public HQLCodeAssist getWrappedObject() { return wrappedHqlCodeAssistExtension; }
-		};
+		return new HqlCodeAssistWrapperImpl(m);
 	}
 	
-	public static class HqlCodeAssistExtension extends HQLCodeAssist {
-		public HqlCodeAssistExtension(Metadata m) {
-			super(m);
+	private static class HqlCodeAssistWrapperImpl implements HqlCodeAssistWrapper {
+		
+		private HQLCodeAssist hqlCodeAssist = null;
+		
+		private HqlCodeAssistWrapperImpl(Metadata metadata) {
+			this.hqlCodeAssist = new HQLCodeAssist(metadata);
 		}
 		
+		@Override public HQLCodeAssist getWrappedObject() {
+			return hqlCodeAssist; 
+		}
+		
+		@Override
+		public void codeComplete(String query, int position, Object handler) {
+			hqlCodeAssist.codeComplete(
+					query, 
+					position, 
+					new HqlCompletionRequestor(handler));
+		}
+
 	}
 	
 }
