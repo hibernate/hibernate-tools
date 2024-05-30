8b49bca29f19121f67c3bfd5b7d153f5d173356e
Koen Aers <koen.aers@gmail.com>
2024-05-30 16:13:19 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.ForeignKeyWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.ForeignKeyWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.ForeignKeyWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/ForeignKeyWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/ForeignKeyWrapperFactory.java
@@ -1,14 +1,58 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.mapping.Column;
 import org.hibernate.mapping.ForeignKey;
+import org.hibernate.mapping.Table;
 import org.hibernate.tool.orm.jbt.api.ForeignKeyWrapper;
 
 public class ForeignKeyWrapperFactory {
 
 	public static ForeignKeyWrapper createForeignKeyWrapper(final ForeignKey wrappedForeignKey) {
-		return new ForeignKeyWrapper() {
-			@Override public ForeignKey getWrappedObject() { return wrappedForeignKey; }
-		};
+		return new ForeignKeyWrapperImpl(wrappedForeignKey);
+	}
+	
+	private static class ForeignKeyWrapperImpl implements ForeignKeyWrapper {
+		
+		private ForeignKey foreignKey = null;
+		
+		private ForeignKeyWrapperImpl(ForeignKey foreignKey) {
+			this.foreignKey = foreignKey;
+		}
+		
+		@Override 
+		public ForeignKey getWrappedObject() { 
+			return foreignKey; 
+		}
+		
+		@Override
+		public Table getReferencedTable() { 
+			return ((ForeignKey)getWrappedObject()).getReferencedTable(); 
+		}
+		
+		@Override
+		public Iterator<Column> columnIterator() { 
+			return ((ForeignKey)getWrappedObject()).getColumns().iterator(); 
+		}
+		
+		@Override
+		public boolean isReferenceToPrimaryKey() { 
+			return ((ForeignKey)getWrappedObject()).isReferenceToPrimaryKey(); 
+		}
+		
+		@Override
+		public List<Column> getReferencedColumns() { 
+			return ((ForeignKey)getWrappedObject()).getReferencedColumns(); 
+		}
+
+		@Override
+		public boolean containsColumn(Column column) { 
+			return ((ForeignKey)getWrappedObject()).containsColumn(column); 
+		}
+
+		
 	}
 	
 }
