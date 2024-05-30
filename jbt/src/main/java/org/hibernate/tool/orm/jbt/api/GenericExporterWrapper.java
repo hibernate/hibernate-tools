8a3f9d918e949f0021eba6953e9bbe4567cb3d73
Koen Aers <koen.aers@gmail.com>
2024-05-30 17:16:49 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.GenericExporterWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.GenericExporterWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.GenericExporterWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/GenericExporterWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/GenericExporterWrapper.java
@@ -1,34 +1,17 @@
 package org.hibernate.tool.orm.jbt.api;
 
-import org.hibernate.tool.api.export.ExporterConstants;
-import org.hibernate.tool.internal.export.common.GenericExporter;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface GenericExporterWrapper extends Wrapper {
 	
-	default void setFilePattern(String filePattern) { 
-		((GenericExporter)getWrappedObject()).getProperties().setProperty(
-				ExporterConstants.FILE_PATTERN, filePattern);
-	}
+	void setFilePattern(String filePattern);
 	
-	default void setTemplateName(String templateName) {
-		((GenericExporter)getWrappedObject()).getProperties().setProperty(
-				ExporterConstants.TEMPLATE_NAME, templateName);
-	}
+	void setTemplateName(String templateName) ;
 	
-	default void setForEach(String forEach) {
-		((GenericExporter)getWrappedObject()).getProperties().setProperty(
-				ExporterConstants.FOR_EACH, forEach);
-	}
+	void setForEach(String forEach);
 	
-	default String getFilePattern() {
-		return ((GenericExporter)getWrappedObject()).getProperties().getProperty(
-				ExporterConstants.FILE_PATTERN);
-	}
+	String getFilePattern();
 	
-	default String getTemplateName() {
-		return ((GenericExporter)getWrappedObject()).getProperties().getProperty(
-				ExporterConstants.TEMPLATE_NAME);
-	}
+	String getTemplateName();
 
 }
--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/GenericExporterWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/GenericExporterWrapperFactory.java
@@ -1,14 +1,58 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
+import org.hibernate.tool.api.export.ExporterConstants;
 import org.hibernate.tool.internal.export.common.GenericExporter;
 import org.hibernate.tool.orm.jbt.api.GenericExporterWrapper;
 
 public class GenericExporterWrapperFactory {
 
 	public static GenericExporterWrapper createGenericExporterWrapper(final GenericExporter wrappedGenericExporter) {
-		return new GenericExporterWrapper() {
-			@Override public GenericExporter getWrappedObject() { return wrappedGenericExporter; }
-		};
+		return new GenericExporterWrapperImpl(wrappedGenericExporter);
+	}
+	
+	private static class GenericExporterWrapperImpl implements GenericExporterWrapper {
+		
+		private GenericExporter genericExporter = null;
+		
+		private GenericExporterWrapperImpl(GenericExporter genericExporter) {
+			this.genericExporter = genericExporter;
+		}
+		
+		@Override 
+		public GenericExporter getWrappedObject() { 
+			return genericExporter; 
+		}
+		
+		@Override
+		public void setFilePattern(String filePattern) { 
+			genericExporter.getProperties().setProperty(
+					ExporterConstants.FILE_PATTERN, filePattern);
+		}
+		
+		@Override
+		public void setTemplateName(String templateName) {
+			genericExporter.getProperties().setProperty(
+					ExporterConstants.TEMPLATE_NAME, templateName);
+		}
+		
+		@Override
+		public void setForEach(String forEach) {
+			genericExporter.getProperties().setProperty(
+					ExporterConstants.FOR_EACH, forEach);
+		}
+		
+		@Override
+		public String getFilePattern() {
+			return genericExporter.getProperties().getProperty(
+					ExporterConstants.FILE_PATTERN);
+		}
+		
+		@Override
+		public String getTemplateName() {
+			return genericExporter.getProperties().getProperty(
+					ExporterConstants.TEMPLATE_NAME);
+		}
+
 	}
 	
 }
