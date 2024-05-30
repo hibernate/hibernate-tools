238fa37d34d88c0ee71265824944d03fefe8b0b0
Koen Aers <koen.aers@gmail.com>
2024-05-30 16:01:14 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Review interface 'org.hibernate.tool.orm.jbt.api.ExporterWrapper'   - Adapt implementation class 'org.hibernate.tool.orm.jbt.internal.factory.ExporterWrapperFactory'   - Adapt test class 'org.hibernate.tool.orm.jbt.api.ExporterWrapperTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ExporterWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ExporterWrapper.java
@@ -4,15 +4,11 @@ import java.io.File;
 import java.io.StringWriter;
 import java.util.Properties;
 
-import org.hibernate.cfg.Configuration;
-import org.hibernate.tool.internal.export.common.GenericExporter;
-import org.hibernate.tool.internal.export.ddl.DdlExporter;
-import org.hibernate.tool.internal.export.query.QueryExporter;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface ExporterWrapper extends Wrapper {
 	
-	void setConfiguration(Configuration configuration);
+	void setConfiguration(ConfigurationWrapper configuration);
 	
 	void setArtifactCollector(ArtifactCollectorWrapper artifactCollectorWrapper);
 	
@@ -24,11 +20,11 @@ public interface ExporterWrapper extends Wrapper {
 
 	Properties getProperties();
 
-	GenericExporter getGenericExporter();
+	ExporterWrapper getGenericExporter();
 
-	DdlExporter getHbm2DDLExporter();
+	ExporterWrapper getHbm2DDLExporter();
 
-	QueryExporter getQueryExporter();
+	ExporterWrapper getQueryExporter();
 
 	void setCustomProperties(Properties properties);
 
