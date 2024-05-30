e3106a2073602e06bfd32d6c478a36645ab1daf7
Koen Aers <koen.aers@gmail.com>
2024-05-30 16:13:19 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.ForeignKeyWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.ForeignKeyWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.ForeignKeyWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ForeignKeyWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/ForeignKeyWrapper.java
@@ -4,20 +4,19 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.hibernate.mapping.Column;
-import org.hibernate.mapping.ForeignKey;
 import org.hibernate.mapping.Table;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface ForeignKeyWrapper extends Wrapper {
 	
-	default Table getReferencedTable() { return ((ForeignKey)getWrappedObject()).getReferencedTable(); }
+	Table getReferencedTable();
 	
-	default Iterator<Column> columnIterator() { return ((ForeignKey)getWrappedObject()).getColumns().iterator(); }
+	Iterator<Column> columnIterator();
 	
-	default boolean isReferenceToPrimaryKey() { return ((ForeignKey)getWrappedObject()).isReferenceToPrimaryKey(); }
+	boolean isReferenceToPrimaryKey();
 	
-	default List<Column> getReferencedColumns() { return ((ForeignKey)getWrappedObject()).getReferencedColumns(); }
+	List<Column> getReferencedColumns();
 
-	default boolean containsColumn(Column column) { return ((ForeignKey)getWrappedObject()).containsColumn(column); }
+	boolean containsColumn(Column column);
 
 }
