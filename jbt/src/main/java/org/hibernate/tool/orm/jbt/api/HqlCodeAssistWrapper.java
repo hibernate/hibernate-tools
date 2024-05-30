6ddd0c35f430fc92bdc2fbb9eb3e470a2036dbc6
Koen Aers <koen.aers@gmail.com>
2024-05-30 17:54:24 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.HqlCodeAssistWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.HqlCodeAssistWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.HqlCodeAssistWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/HqlCodeAssistWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/HqlCodeAssistWrapper.java
@@ -1,16 +1,9 @@
 package org.hibernate.tool.orm.jbt.api;
 
-import org.hibernate.tool.orm.jbt.internal.factory.HqlCodeAssistWrapperFactory.HqlCodeAssistExtension;
-import org.hibernate.tool.orm.jbt.internal.util.HqlCompletionRequestor;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface HqlCodeAssistWrapper extends Wrapper {
 	
-	default void codeComplete(String query, int position, Object handler) {
-		((HqlCodeAssistExtension)getWrappedObject()).codeComplete(
-				query, 
-				position, 
-				new HqlCompletionRequestor(handler));
-	}
-
+	void codeComplete(String query, int position, Object handler);
+	
 }
