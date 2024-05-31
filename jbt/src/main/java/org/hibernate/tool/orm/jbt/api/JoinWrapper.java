617bee4a46c44f985bc9b1ed00c70b9568f0e731
Koen Aers <koen.aers@gmail.com>
2024-05-31 11:56:51 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.JoinWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.JoinWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.JoinWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/JoinWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/JoinWrapper.java
@@ -2,14 +2,11 @@ package org.hibernate.tool.orm.jbt.api;
 
 import java.util.Iterator;
 
-import org.hibernate.mapping.Join;
 import org.hibernate.mapping.Property;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface JoinWrapper extends Wrapper {
 
-	default Iterator<Property> getPropertyIterator() {
-		return ((Join)getWrappedObject()).getProperties().iterator();
-	}
+	Iterator<Property> getPropertyIterator();
 	
 }
--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/JoinWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/JoinWrapperFactory.java
@@ -1,14 +1,35 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
+import java.util.Iterator;
+
 import org.hibernate.mapping.Join;
+import org.hibernate.mapping.Property;
 import org.hibernate.tool.orm.jbt.api.JoinWrapper;
 
 public class JoinWrapperFactory {
 
 	public static JoinWrapper createJoinWrapper(Join wrappedJoin) {
-		return new JoinWrapper() {
-			@Override public Join getWrappedObject() { return wrappedJoin; }
-		};
+		return new JoinWrapperImpl(wrappedJoin);
+	}
+	
+	private static class JoinWrapperImpl implements JoinWrapper {
+		
+		private Join join = null;
+		
+		private JoinWrapperImpl(Join join) {
+			this.join = join;
+		}
+		
+		@Override 
+		public Join getWrappedObject() { 
+			return join; 
+		}
+		
+		@Override
+		public Iterator<Property> getPropertyIterator() {
+			return join.getProperties().iterator();
+		}
+		
 	}
 	
 }
