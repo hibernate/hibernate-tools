64859d9f33a613f88cd2c80b427d12292dc1cf3a
Koen Aers <koen.aers@gmail.com>
2024-05-31 13:23:04 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.OverrideRepositoryWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.OverrideRepositoryWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.OverrideRepositoryWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/OverrideRepositoryWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/OverrideRepositoryWrapper.java
@@ -3,22 +3,15 @@ package org.hibernate.tool.orm.jbt.api;
 import java.io.File;
 
 import org.hibernate.tool.api.reveng.RevengStrategy;
-import org.hibernate.tool.internal.reveng.strategy.OverrideRepository;
 import org.hibernate.tool.internal.reveng.strategy.TableFilter;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface OverrideRepositoryWrapper extends Wrapper {
 	
-	default void addFile(File file) {
-		((OverrideRepository)getWrappedObject()).addFile(file);
-	}
+	void addFile(File file);
 	
-	default RevengStrategy getReverseEngineeringStrategy(RevengStrategy res) {
-		return ((OverrideRepository)getWrappedObject()).getReverseEngineeringStrategy(res);
-	}
+	RevengStrategy getReverseEngineeringStrategy(RevengStrategy res);
 	
-	default void addTableFilter(TableFilter tf) {
-		((OverrideRepository)getWrappedObject()).addTableFilter(tf);
-	}
+	void addTableFilter(TableFilter tf);
 
 }
--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/OverrideRepositoryWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/OverrideRepositoryWrapperFactory.java
@@ -1,14 +1,46 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
+import java.io.File;
+
+import org.hibernate.tool.api.reveng.RevengStrategy;
 import org.hibernate.tool.internal.reveng.strategy.OverrideRepository;
+import org.hibernate.tool.internal.reveng.strategy.TableFilter;
 import org.hibernate.tool.orm.jbt.api.OverrideRepositoryWrapper;
 
 public class OverrideRepositoryWrapperFactory {
 
 	public static OverrideRepositoryWrapper createOverrideRepositoryWrapper(OverrideRepository wrappedOverrideRepository) {
-		return new OverrideRepositoryWrapper() {
-			@Override public OverrideRepository getWrappedObject() { return wrappedOverrideRepository; }
-		};
+		return new OverrideRepositoryWrapperImpl(wrappedOverrideRepository);
+	}
+	
+	public static class OverrideRepositoryWrapperImpl implements OverrideRepositoryWrapper {
+		
+		private OverrideRepository overrideRepository = null;
+		
+		private OverrideRepositoryWrapperImpl(OverrideRepository overrideRepository) {
+			this.overrideRepository = overrideRepository;
+		}
+		
+		@Override 
+		public OverrideRepository getWrappedObject() { 
+			return overrideRepository; 
+		}
+		
+		@Override
+		public void addFile(File file) {
+			overrideRepository.addFile(file);
+		}
+		
+		@Override
+		public RevengStrategy getReverseEngineeringStrategy(RevengStrategy res) {
+			return overrideRepository.getReverseEngineeringStrategy(res);
+		}
+		
+		@Override
+		public void addTableFilter(TableFilter tf) {
+			overrideRepository.addTableFilter(tf);
+		}
+
 	}
 	
 }
