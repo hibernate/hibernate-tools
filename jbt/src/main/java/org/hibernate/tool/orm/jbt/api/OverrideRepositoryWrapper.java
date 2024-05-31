562a7befd50ddda7e4cca2d10b9ac42c83d3461c
Koen Aers <koen.aers@gmail.com>
2024-05-31 13:34:41 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Review interface 'org.hibernate.tool.orm.jbt.api.OverrideRepositoryWrapper'   - Adapt implementation class 'org.hibernate.tool.orm.jbt.internal.factory.OverrideRepositoryWrapperFactory'   - Adapt test class 'org.hibernate.tool.orm.jbt.api.OverrideRepositoryWrapperTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/OverrideRepositoryWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/OverrideRepositoryWrapper.java
@@ -2,16 +2,14 @@ package org.hibernate.tool.orm.jbt.api;
 
 import java.io.File;
 
-import org.hibernate.tool.api.reveng.RevengStrategy;
-import org.hibernate.tool.internal.reveng.strategy.TableFilter;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface OverrideRepositoryWrapper extends Wrapper {
 	
 	void addFile(File file);
 	
-	RevengStrategy getReverseEngineeringStrategy(RevengStrategy res);
+	RevengStrategyWrapper getReverseEngineeringStrategy(RevengStrategyWrapper res);
 	
-	void addTableFilter(TableFilter tf);
+	void addTableFilter(TableFilterWrapper tf);
 
 }
