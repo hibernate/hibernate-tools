b93879cb98bb83881e6b472248f874d0362bed13
Koen Aers <koen.aers@gmail.com>
2024-06-03 16:15:25 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.SchemaExportWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.SchemaExportWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.SchemaExportWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/SchemaExportWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/SchemaExportWrapper.java
@@ -1,23 +1,14 @@
 package org.hibernate.tool.orm.jbt.api;
 
-import java.util.EnumSet;
 import java.util.List;
 
 import org.hibernate.cfg.Configuration;
-import org.hibernate.tool.hbm2ddl.SchemaExport;
-import org.hibernate.tool.orm.jbt.util.MetadataHelper;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
-import org.hibernate.tool.schema.TargetType;
 
 public interface SchemaExportWrapper extends Wrapper {
 	
 	Configuration getConfiguration();
-	default void create() { (
-			(SchemaExport)getWrappedObject()).create(EnumSet.of(
-				TargetType.DATABASE), 
-				MetadataHelper.getMetadata(getConfiguration()));
-	}
-	@SuppressWarnings("unchecked")
-	default List<Throwable> getExceptions() { return ((SchemaExport)getWrappedObject()).getExceptions(); }
+	void create();
+	List<Throwable> getExceptions();
 
 }
