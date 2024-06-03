30d6eddcf5b9d5ec744a3ddc06d9d8a43568f8a3
Koen Aers <koen.aers@gmail.com>
2024-06-03 17:04:59 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.RevengSettingsWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.RevengSettingsWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.RevengSettingsWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/RevengSettingsWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/RevengSettingsWrapperFactory.java
@@ -6,9 +6,42 @@ import org.hibernate.tool.orm.jbt.api.RevengSettingsWrapper;
 public class RevengSettingsWrapperFactory {
 
 	public static RevengSettingsWrapper createRevengSettingsWrapper(RevengSettings wrappedRevengSettings) {
-		return new RevengSettingsWrapper() {
-			@Override public RevengSettings getWrappedObject() { return wrappedRevengSettings; }
-		};
+		return new RevengSettingsWrapperImpl(wrappedRevengSettings);
+	}
+	
+	private static class RevengSettingsWrapperImpl implements RevengSettingsWrapper {
+		
+		private RevengSettings revengSettings = null;
+		
+		private RevengSettingsWrapperImpl(RevengSettings revengSettings) {
+			this.revengSettings = revengSettings;
+		}
+		
+		@Override 
+		public RevengSettings getWrappedObject() { 
+			return revengSettings; 
+		}
+		
+		@Override 
+		public void setDefaultPackageName(String s) { 
+			revengSettings.setDefaultPackageName(s); 
+		}
+		
+		@Override 
+		public void setDetectManyToMany(boolean b) { 
+			revengSettings.setDetectManyToMany(b); 
+		}
+		
+		@Override 
+		public void setDetectOneToOne(boolean b) { 
+			revengSettings.setDetectOneToOne(b); 
+		}
+		
+		@Override 
+		public void setDetectOptimisticLock(boolean b) { 
+			revengSettings.setDetectOptimisticLock(b); 
+		}
+
 	}
 
 }
