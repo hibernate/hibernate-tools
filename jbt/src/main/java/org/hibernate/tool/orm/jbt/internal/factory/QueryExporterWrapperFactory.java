f047ef846621d84e4e930ad8ad79fed4355268e7
Koen Aers <koen.aers@gmail.com>
2024-06-03 17:56:11 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.QueryExporterWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.QueryExporterWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.QueryExporterWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/QueryExporterWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/QueryExporterWrapperFactory.java
@@ -1,14 +1,40 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
+import java.util.List;
+
+import org.hibernate.tool.api.export.ExporterConstants;
 import org.hibernate.tool.internal.export.query.QueryExporter;
 import org.hibernate.tool.orm.jbt.api.QueryExporterWrapper;
 
 public class QueryExporterWrapperFactory {
 
 	public static QueryExporterWrapper createQueryExporterWrapper(QueryExporter wrappedQueryExporter) {
-		return new QueryExporterWrapper() {
-			@Override public QueryExporter getWrappedObject() { return wrappedQueryExporter; }
-		};
+		return new QueryExporterWrapperImpl(wrappedQueryExporter);
+	}
+	
+	private static class QueryExporterWrapperImpl implements QueryExporterWrapper {
+		
+		private QueryExporter queryExporter = null;
+		
+		private QueryExporterWrapperImpl(QueryExporter queryExporter) {
+			this.queryExporter = queryExporter;
+		}
+		
+		@Override 
+		public QueryExporter getWrappedObject() { 
+			return queryExporter; 
+		}
+		
+		@Override
+		public void setQueries(List<String> queries) { 
+			queryExporter.setQueries(queries); 
+		}	
+		
+		@Override
+		public void setFilename(String fileName) { 
+			queryExporter.getProperties().put(ExporterConstants.OUTPUT_FILE_NAME, fileName); 
+		}
+
 	}
 
 }
