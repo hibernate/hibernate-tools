259f2a8e948d0cb5794be34c8cb65a6a723b274b
Koen Aers <koen.aers@gmail.com>
2024-06-03 16:15:25 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.SchemaExportWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.SchemaExportWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.SchemaExportWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/SchemaExportWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/SchemaExportWrapperFactory.java
@@ -1,18 +1,55 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
+import java.util.EnumSet;
+import java.util.List;
+
 import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.hibernate.tool.orm.jbt.api.SchemaExportWrapper;
+import org.hibernate.tool.orm.jbt.util.MetadataHelper;
+import org.hibernate.tool.schema.TargetType;
 
 public class SchemaExportWrapperFactory {
 
 	public static SchemaExportWrapper createSchemaExportWrapper(
 			SchemaExport wrappedSchemaExport,
 			Configuration configuration) { 
-		return new SchemaExportWrapper() {
-			@Override public SchemaExport getWrappedObject() { return wrappedSchemaExport; }
-			@Override public Configuration getConfiguration() { return configuration; }
-		};
+		return new SchemaExportWrapperImpl(wrappedSchemaExport, configuration);
+	}
+	
+	private static class SchemaExportWrapperImpl implements SchemaExportWrapper {
+		
+		private Configuration configuration = null;
+		private SchemaExport schemaExport = null;
+		
+		private SchemaExportWrapperImpl(SchemaExport se, Configuration c) {
+			this.configuration = c;
+			this.schemaExport = se;
+		}
+		
+		@Override 
+		public SchemaExport getWrappedObject() {
+			return schemaExport;
+		}
+		
+		@Override
+		public Configuration getConfiguration() {
+			return configuration;
+		}
+		
+		@Override
+		public void create() { 
+				schemaExport.create(EnumSet.of(
+					TargetType.DATABASE), 
+					MetadataHelper.getMetadata(getConfiguration()));
+		}
+		
+		@SuppressWarnings("unchecked")
+		@Override
+		public List<Throwable> getExceptions() { 
+			return schemaExport.getExceptions(); 
+		}
+
 	}
 
 }
