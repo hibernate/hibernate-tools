a61e81af9e866bc31ec1011113dd626e261967bc
Koen Aers <koen.aers@gmail.com>
2024-06-03 18:30:11 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.PropertyWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.PropertyWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.PropertyWrapper'   - Adapt 'org.hibernate.tool.orm.jbt.wrp.PropertyWrapperFactory'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/PropertyWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/PropertyWrapper.java
@@ -1,36 +1,31 @@
 package org.hibernate.tool.orm.jbt.api;
 
-import org.hibernate.mapping.BasicValue;
 import org.hibernate.mapping.PersistentClass;
-import org.hibernate.mapping.Property;
 import org.hibernate.mapping.Value;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 import org.hibernate.type.Type;
 
 public interface PropertyWrapper extends Wrapper {
 
-	default Value getValue() { return ((Property)getWrappedObject()).getValue(); }
-	default void setName(String name) { ((Property)getWrappedObject()).setName(name); }
-	default void setPersistentClass(PersistentClass pc) { ((Property)getWrappedObject()).setPersistentClass(pc); }
-	default PersistentClass getPersistentClass() { return ((Property)getWrappedObject()).getPersistentClass(); }
-	default boolean isComposite() { return ((Property)getWrappedObject()).isComposite(); }
-	default String getPropertyAccessorName() { return ((Property)getWrappedObject()).getPropertyAccessorName(); }
-	default String getName() { return ((Property)getWrappedObject()).getName(); }
-	default Type getType() { 
-		Value v = ((Property)getWrappedObject()).getValue();
-		return v == null ? null : v.getType();
-	}
-	default void setValue(BasicValue value) { ((Property)getWrappedObject()).setValue(value); }
-	default void setPropertyAccessorName(String s) { ((Property)getWrappedObject()).setPropertyAccessorName(s); }
-	default void setCascade(String s) { ((Property)getWrappedObject()).setCascade(s); }
-	default boolean isBackRef() { return ((Property)getWrappedObject()).isBackRef(); }
-	default boolean isSelectable() { return ((Property)getWrappedObject()).isSelectable(); }
-	default boolean isInsertable() { return ((Property)getWrappedObject()).isInsertable(); }
-	default boolean isUpdateable() { return ((Property)getWrappedObject()).isUpdateable(); }
-	default String getCascade() { return ((Property)getWrappedObject()).getCascade(); }
-	default boolean isLazy() { return ((Property)getWrappedObject()).isLazy(); }
-	default boolean isOptional() { return ((Property)getWrappedObject()).isOptional(); }
-	default boolean isNaturalIdentifier() { return ((Property)getWrappedObject()).isNaturalIdentifier(); }
-	default boolean isOptimisticLocked() { return ((Property)getWrappedObject()).isOptimisticLocked(); }
+	Value getValue();
+	void setName(String name);
+	void setPersistentClass(PersistentClass pc);
+	PersistentClass getPersistentClass();
+	boolean isComposite();
+	String getPropertyAccessorName();
+	String getName();
+	Type getType();
+	void setValue(Value value);
+	void setPropertyAccessorName(String s);
+	void setCascade(String s);
+	boolean isBackRef();
+	boolean isSelectable();
+	boolean isInsertable();
+	boolean isUpdateable();
+	String getCascade();
+	boolean isLazy();
+	boolean isOptional();
+	boolean isNaturalIdentifier();
+	boolean isOptimisticLocked();
 
 }
--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/PropertyWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/PropertyWrapperFactory.java
@@ -1,14 +1,132 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
+import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.Property;
+import org.hibernate.mapping.Value;
 import org.hibernate.tool.orm.jbt.api.PropertyWrapper;
+import org.hibernate.type.Type;
 
 public class PropertyWrapperFactory {
 
 	public static PropertyWrapper createPropertyWrapper(Property wrappedProperty) {
-		return new PropertyWrapper() {
-			@Override public Property getWrappedObject() { return wrappedProperty; }
-		};
+		return new PropertyWrapperImpl(wrappedProperty);
+	}
+	
+	private static class PropertyWrapperImpl implements PropertyWrapper {
+		
+		private Property property = null;
+		
+		private PropertyWrapperImpl(Property property) {
+			this.property = property;
+		}
+		
+		@Override 
+		public Property getWrappedObject() { 
+			return property; 
+		}
+		
+		@Override 
+		public Value getValue() { 
+			return property.getValue(); 
+		}
+		
+		@Override 
+		public void setName(String name) { 
+			property.setName(name); 
+		}
+		
+		@Override 
+		public void setPersistentClass(PersistentClass pc) { 
+			property.setPersistentClass(pc); 
+		}
+		
+		@Override 
+		public PersistentClass getPersistentClass() { 
+			return property.getPersistentClass(); 
+		}
+		
+		@Override 
+		public boolean isComposite() { 
+			return property.isComposite(); 
+		}
+		
+		@Override 
+		public String getPropertyAccessorName() { 
+			return property.getPropertyAccessorName(); 
+		}
+		
+		@Override 
+		public String getName() { 
+			return property.getName(); 
+		}
+		
+		@Override 
+		public Type getType() { 
+			Value v = ((Property)getWrappedObject()).getValue();
+			return v == null ? null : v.getType();
+		}
+		
+		@Override 
+		public void setValue(Value value) { 
+			property.setValue(value); 
+		}
+		
+		@Override 
+		public void setPropertyAccessorName(String s) { 
+			property.setPropertyAccessorName(s); 
+		}
+		
+		@Override 
+		public void setCascade(String s) { 
+			property.setCascade(s); 
+		}
+		
+		@Override 
+		public boolean isBackRef() { 
+			return property.isBackRef(); 
+		}
+		
+		@Override 
+		public boolean isSelectable() { 
+			return property.isSelectable(); 
+		}
+		
+		@Override 
+		public boolean isInsertable() { 
+			return property.isInsertable(); 
+		}
+		
+		@Override 
+		public boolean isUpdateable() { 
+			return property.isUpdateable(); 
+		}
+		
+		@Override 
+		public String getCascade() { 
+			return property.getCascade(); 
+		}
+		
+		@Override 
+		public boolean isLazy() { 
+			return property.isLazy(); 
+		}
+		
+		@Override 
+		public boolean isOptional() { 
+			return property.isOptional(); 
+		}
+		
+		@Override 
+		public boolean isNaturalIdentifier() { 
+			return property.isNaturalIdentifier(); 
+		}
+		
+		@Override 
+		public boolean isOptimisticLocked() { 
+			return property.isOptimisticLocked(); 
+		}
+		
+
 	}
 	
 }
