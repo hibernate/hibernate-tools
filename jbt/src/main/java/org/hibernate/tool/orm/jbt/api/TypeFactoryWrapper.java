67abfe99a275551ddf582763322edac1a8f3bf36
Koen Aers <koen.aers@gmail.com>
2024-06-03 12:53:40 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.TypeFactoryWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.TypeFactoryWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.TypeFactoryWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/TypeFactoryWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/TypeFactoryWrapper.java
@@ -2,35 +2,33 @@ package org.hibernate.tool.orm.jbt.api;
 
 import java.util.Map;
 
-import org.hibernate.tool.orm.jbt.internal.util.TypeRegistry;
-
 public interface TypeFactoryWrapper {
 
-	default TypeWrapper getBooleanType() { return TypeRegistry.getType("boolean"); }
-	default TypeWrapper getByteType() { return TypeRegistry.getType("byte"); }
-	default TypeWrapper getBigIntegerType() { return TypeRegistry.getType("big_integer"); }
-	default TypeWrapper getShortType() { return TypeRegistry.getType("short"); }
-	default TypeWrapper getCalendarType() { return TypeRegistry.getType("calendar"); }
-	default TypeWrapper getCalendarDateType() { return TypeRegistry.getType("calendar_date"); }
-	default TypeWrapper getIntegerType() { return TypeRegistry.getType("integer"); }
-	default TypeWrapper getBigDecimalType() { return TypeRegistry.getType("big_decimal"); }
-	default TypeWrapper getCharacterType() { return TypeRegistry.getType("character"); }
-	default TypeWrapper getClassType() { return TypeRegistry.getType("class"); }
-	default TypeWrapper getCurrencyType() { return TypeRegistry.getType("currency"); }
-	default TypeWrapper getDateType() { return TypeRegistry.getType("date"); }
-	default TypeWrapper getDoubleType() { return TypeRegistry.getType("double"); }
-	default TypeWrapper getFloatType() { return TypeRegistry.getType("float"); }
-	default TypeWrapper getLocaleType() { return TypeRegistry.getType("locale"); }
-	default TypeWrapper getLongType() { return TypeRegistry.getType("long"); }
-	default TypeWrapper getStringType() { return TypeRegistry.getType("string"); }
-	default TypeWrapper getTextType() { return TypeRegistry.getType("text"); }
-	default TypeWrapper getTimeType() { return TypeRegistry.getType("time"); }
-	default TypeWrapper getTimestampType() { return TypeRegistry.getType("timestamp"); }
-	default TypeWrapper getTimezoneType() { return TypeRegistry.getType("timezone"); }
-	default TypeWrapper getTrueFalseType() { return TypeRegistry.getType("true_false"); }
-	default TypeWrapper getYesNoType() { return TypeRegistry.getType("true_false"); }
-	default TypeWrapper getNamedType(String name) { return TypeRegistry.getType(name); }
-	default TypeWrapper getBasicType(String name) { return getNamedType(name); }
-	default Map<TypeWrapper, String> getTypeFormats() { return TypeRegistry.getTypeFormats(); }
+	TypeWrapper getBooleanType();
+	TypeWrapper getByteType();
+	TypeWrapper getBigIntegerType();
+	TypeWrapper getShortType() ;
+	TypeWrapper getCalendarType();
+	TypeWrapper getCalendarDateType();
+	TypeWrapper getIntegerType();
+	TypeWrapper getBigDecimalType();
+	TypeWrapper getCharacterType();
+	TypeWrapper getClassType();
+	TypeWrapper getCurrencyType();
+	TypeWrapper getDateType();
+	TypeWrapper getDoubleType();
+	TypeWrapper getFloatType();
+	TypeWrapper getLocaleType();
+	TypeWrapper getLongType();
+	TypeWrapper getStringType();
+	TypeWrapper getTextType();
+	TypeWrapper getTimeType();
+	TypeWrapper getTimestampType();
+	TypeWrapper getTimezoneType();
+	TypeWrapper getTrueFalseType();
+	TypeWrapper getYesNoType();
+	TypeWrapper getNamedType(String name);
+	TypeWrapper getBasicType(String name);
+	Map<TypeWrapper, String> getTypeFormats();
 
 }
