c4cd25634a094231d5796e04cc3570b68494ffb8
Koen Aers <koen.aers@gmail.com>
2024-06-03 15:25:06 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Review interface 'org.hibernate.tool.orm.jbt.api.SessionWrapper'   - Adapt implementation class 'org.hibernate.tool.orm.jbt.internal.factory.SessionWrapperFactory'   - Adapt test class 'org.hibernate.tool.orm.jbt.api.SessionWrapperTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/SessionWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/SessionWrapper.java
@@ -1,17 +1,17 @@
 package org.hibernate.tool.orm.jbt.api;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
+
+import jakarta.persistence.Query;
 
 public interface SessionWrapper extends Wrapper {
 
 	String getEntityName(Object o) ;
-	SessionFactory getSessionFactory();
-	Query<?> createQuery(String s);
+	SessionFactoryWrapper getSessionFactory();
+	QueryWrapper createQuery(String s);
 	boolean isOpen();
 	void close();
 	boolean contains(Object o);
-	jakarta.persistence.Query createCriteria(Class<?> c);
+	Query createCriteria(Class<?> c);
 
 }
