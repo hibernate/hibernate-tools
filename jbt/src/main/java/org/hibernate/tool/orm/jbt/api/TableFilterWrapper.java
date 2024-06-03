481821b032781a9cd8aa6ca1ffbeb2f0029f5afc
Koen Aers <koen.aers@gmail.com>
2024-06-03 14:34:59 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.TableFilterWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.TableFilterWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.TableFilterWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/TableFilterWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/TableFilterWrapper.java
@@ -1,24 +1,15 @@
 package org.hibernate.tool.orm.jbt.api;
 
-import org.hibernate.tool.internal.reveng.strategy.TableFilter;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface TableFilterWrapper extends Wrapper {
 
-	default void setExclude(boolean b) { ((TableFilter)getWrappedObject()).setExclude(b); }
-
-	default void setMatchCatalog(String s) { ((TableFilter)getWrappedObject()).setMatchCatalog(s); }
-
-	default void setMatchSchema(String s) { ((TableFilter)getWrappedObject()).setMatchSchema(s); }
-
-	default void setMatchName(String s) { ((TableFilter)getWrappedObject()).setMatchName(s); }
-
-	default Boolean getExclude() { return ((TableFilter)getWrappedObject()).getExclude(); }
-
-	default String getMatchCatalog() { return ((TableFilter)getWrappedObject()).getMatchCatalog(); }
-
-	default String getMatchSchema() { return ((TableFilter)getWrappedObject()).getMatchSchema(); }
-
-	default String getMatchName() { return ((TableFilter)getWrappedObject()).getMatchName(); }
-
+	void setExclude(boolean b);
+	void setMatchCatalog(String s);
+	void setMatchSchema(String s);
+	void setMatchName(String s);
+	Boolean getExclude();
+	String getMatchCatalog();
+	String getMatchSchema();
+	String getMatchName();
 }
