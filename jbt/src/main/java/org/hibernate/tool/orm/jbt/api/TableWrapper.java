16a4bf0744b025fdbb1bed66d3ba940fab9899b7
Koen Aers <koen.aers@gmail.com>
2024-06-03 13:33:24 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.TableWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.TableWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.TableWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/TableWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/TableWrapper.java
@@ -5,37 +5,23 @@ import java.util.Iterator;
 import org.hibernate.mapping.Column;
 import org.hibernate.mapping.KeyValue;
 import org.hibernate.mapping.PrimaryKey;
-import org.hibernate.mapping.Table;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface TableWrapper extends Wrapper {
 
-	default String getName() { return ((Table)getWrappedObject()).getName(); }
-
-	default void addColumn(Column column) { ((Table)getWrappedObject()).addColumn(column); }
-
-	default String getCatalog() { return ((Table)getWrappedObject()).getCatalog(); }
-
-	default String getSchema() { return ((Table)getWrappedObject()).getSchema(); }
-
-	default PrimaryKey getPrimaryKey() { return ((Table)getWrappedObject()).getPrimaryKey(); }
-
-	default Iterator<Column> getColumnIterator() { return ((Table)getWrappedObject()).getColumns().iterator(); }
-
-	default String getComment() { return ((Table)getWrappedObject()).getComment(); }
-
-	default String getRowId() { return ((Table)getWrappedObject()).getRowId(); }
-
-	default String getSubselect() { return ((Table)getWrappedObject()).getSubselect(); }
-
-	default boolean hasDenormalizedTables() { return ((Table)getWrappedObject()).hasDenormalizedTables(); }
-
-	default boolean isAbstract() { return ((Table)getWrappedObject()).isAbstract(); }
-
-	default boolean isAbstractUnionTable() { return ((Table)getWrappedObject()).isAbstractUnionTable(); }
-
-	default boolean isPhysicalTable() { return ((Table)getWrappedObject()).isPhysicalTable(); }
-
-	default KeyValue getIdentifierValue() { return ((Table)getWrappedObject()).getIdentifierValue(); }
-
+	String getName();
+	void addColumn(Column column);
+	String getCatalog();
+	String getSchema();
+	PrimaryKey getPrimaryKey();
+	Iterator<Column> getColumnIterator();
+	String getComment();
+	String getRowId();
+	String getSubselect();
+	boolean hasDenormalizedTables() ;
+	boolean isAbstract();
+	boolean isAbstractUnionTable();
+	boolean isPhysicalTable();
+	KeyValue getIdentifierValue();
+	
 }
--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/TableWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/TableWrapperFactory.java
@@ -1,14 +1,104 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
+import java.util.Iterator;
+
+import org.hibernate.mapping.Column;
+import org.hibernate.mapping.KeyValue;
+import org.hibernate.mapping.PrimaryKey;
 import org.hibernate.mapping.Table;
 import org.hibernate.tool.orm.jbt.api.TableWrapper;
 
 public class TableWrapperFactory {
 
 	public static TableWrapper createTableWrapper(Table wrappedTable) {
-		return new TableWrapper() {
-			@Override public Table getWrappedObject() { return wrappedTable; }
-		};
+		return new TableWrapperImpl(wrappedTable);
+	}
+	
+	private static class TableWrapperImpl implements TableWrapper {
+		
+		private Table table = null;
+		
+		private TableWrapperImpl(Table table) {
+			this.table = table;
+		}
+		
+		@Override 
+		public Table getWrappedObject() { 
+			return table; 
+		}
+		
+		@Override
+		public String getName() { 
+			return table.getName(); 
+		}
+
+		@Override
+		public void addColumn(Column column) { 
+			table.addColumn(column); 
+		}
+
+		@Override
+		public String getCatalog() { 
+			return table.getCatalog(); 
+		}
+
+		@Override
+		public String getSchema() { 
+			return table.getSchema(); 
+		}
+
+		@Override
+		public PrimaryKey getPrimaryKey() { 
+			return table.getPrimaryKey(); 
+		}
+
+		@Override
+		public Iterator<Column> getColumnIterator() { 
+			return table.getColumns().iterator(); 
+		}
+
+		@Override
+		public String getComment() { 
+			return table.getComment(); 
+		}
+
+		@Override
+		public String getRowId() { 
+			return table.getRowId(); 
+		}
+
+		@Override
+		public String getSubselect() { 
+			return table.getSubselect(); 
+		}
+
+		@Override
+		public boolean hasDenormalizedTables() { 
+			return table.hasDenormalizedTables(); 
+		}
+
+		@Override
+		public boolean isAbstract() { 
+			return table.isAbstract(); 
+		}
+
+		@Override
+		public boolean isAbstractUnionTable() { 
+			return table.isAbstractUnionTable(); 
+		}
+
+		@Override
+		public boolean isPhysicalTable() { 
+			return table.isPhysicalTable(); 
+		}
+
+		@Override
+		public KeyValue getIdentifierValue() { 
+			return table.getIdentifierValue(); 
+		}
+
+		
+		
 	}
 
 }
