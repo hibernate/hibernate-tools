9881279bff87d430a1aa3ce559cc4c83eb778069
Koen Aers <koen.aers@gmail.com>
2024-06-03 14:57:51 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.SessionWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.SessionWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.SessionWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/SessionWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/SessionWrapperFactory.java
@@ -1,14 +1,81 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.hibernate.tool.orm.jbt.api.SessionWrapper;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 public class SessionWrapperFactory {
 
 	public static SessionWrapper createSessionWrapper(Session wrappedSession) {
-		return new SessionWrapper() {
-			@Override public Session getWrappedObject() { return wrappedSession; }
-		};
+		return new SessionWrapperImpl(wrappedSession);
+	}
+	
+	private static class SessionWrapperImpl implements SessionWrapper {
+		
+		private Session session = null;
+		
+		private SessionWrapperImpl(Session session) {
+			this.session = session;
+		}
+		
+		@Override 
+		public Session getWrappedObject() { 
+			return session; 
+		}
+		
+		@Override 
+		public String getEntityName(Object o) { 
+			return session.getEntityName(o); 
+		}
+
+		@Override 
+		public SessionFactory getSessionFactory() { 
+			return session.getSessionFactory(); 
+		}
+
+		@Override 
+		public Query<?> createQuery(String s) { 
+			return session.createQuery(s); 
+		}
+
+		@Override 
+		public boolean isOpen() { 
+			return session.isOpen(); 
+		}
+
+		@Override 
+		public void close() { 
+			session.close(); 
+		}
+
+		@Override 
+		public boolean contains(Object o) { 
+			boolean result = false;
+			try {
+				result = session.contains(o);
+			} catch (IllegalArgumentException e) {
+				String message = e.getMessage();
+				if (!(message.startsWith("Class '") && message.endsWith("' is not an entity class"))) {
+					throw e;
+				}
+			}
+			return result;
+		}
+
+		@Override 
+		public jakarta.persistence.Query createCriteria(Class<?> c) {
+			CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+			CriteriaQuery<?> criteriaQuery = criteriaBuilder.createQuery(c);
+			Root root = criteriaQuery.from(c);
+			criteriaQuery.select(root);
+			return ((Session)getWrappedObject()).createQuery(criteriaQuery);
+		}
+
 	}
 
 }
