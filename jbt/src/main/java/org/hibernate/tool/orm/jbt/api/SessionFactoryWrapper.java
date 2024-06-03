b84a765dd50c5b254f5d7dc715974b370bf61832
Koen Aers <koen.aers@gmail.com>
2024-06-03 15:45:23 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.SessionFactoryWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.SessionFactoryWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.SessionFactoryWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/SessionFactoryWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/SessionFactoryWrapper.java
@@ -1,45 +1,20 @@
 package org.hibernate.tool.orm.jbt.api;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.persister.collection.CollectionPersister;
 import org.hibernate.persister.entity.EntityPersister;
-import org.hibernate.tool.orm.jbt.wrp.CollectionPersisterWrapperFactory;
-import org.hibernate.tool.orm.jbt.wrp.EntityPersisterWrapperFactory;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface SessionFactoryWrapper extends Wrapper {
 
-	default void close() { ((SessionFactory)getWrappedObject()).close(); }
-
-	default Map<String, EntityPersister> getAllClassMetadata() {
-		Map<String, EntityPersister> origin = ((SessionFactoryImplementor)getWrappedObject()).getMetamodel().entityPersisters();
-		Map<String, EntityPersister> result = new HashMap<String, EntityPersister>(origin.size());
-		for (String key : origin.keySet()) {
-			result.put(key, (EntityPersister)EntityPersisterWrapperFactory.create(origin.get(key)));
-		}
-		return result;
-	}
-
-	default Map<String, CollectionPersister> getAllCollectionMetadata() {
-		Map<String, CollectionPersister> origin = ((SessionFactoryImplementor)getWrappedObject()).getMetamodel().collectionPersisters();
-		Map<String, CollectionPersister> result = new HashMap<String, CollectionPersister>(origin.size());
-		for (String key : origin.keySet()) {
-			result.put(key, (CollectionPersister)CollectionPersisterWrapperFactory.create(origin.get(key)));
-		}
-		return result;
-	}
-
-	default Session openSession() { return ((SessionFactory)getWrappedObject()).openSession(); }
-
-	default EntityPersister getClassMetadata(String s) { return getAllClassMetadata().get(s); }
-
-	default EntityPersister getClassMetadata(Class<?> c) { return getAllClassMetadata().get(c.getName()); }
-
-	default CollectionPersister getCollectionMetadata(String s) { return getAllCollectionMetadata().get(s); }
+	void close();
+	Map<String, EntityPersister> getAllClassMetadata();
+	Map<String, CollectionPersister> getAllCollectionMetadata();
+	Session openSession();
+	EntityPersister getClassMetadata(String s);
+	EntityPersister getClassMetadata(Class<?> c);
+	CollectionPersister getCollectionMetadata(String s);
 
 }
