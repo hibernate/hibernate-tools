ad230f21051b450f04b94dbb86d9bda015efc12f
Koen Aers <koen.aers@gmail.com>
2024-06-03 17:19:58 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.QueryWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.QueryWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.QueryWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/QueryWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/QueryWrapperFactory.java
@@ -1,14 +1,65 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
+import java.util.List;
+
 import org.hibernate.query.Query;
 import org.hibernate.tool.orm.jbt.api.QueryWrapper;
+import org.hibernate.type.Type;
 
 public class QueryWrapperFactory {
 
 	public static QueryWrapper createQueryWrapper(Query<?> wrappedQuery) {
-		return new QueryWrapper() {
-			@Override public Query<?> getWrappedObject() { return wrappedQuery; }
-		};
+		return new QueryWrapperImpl(wrappedQuery);
+	}
+	
+	private static class QueryWrapperImpl implements QueryWrapper {
+		
+		private Query<?> query = null;
+		
+		private QueryWrapperImpl(Query<?> query) {
+			this.query = query;
+		}
+		
+		@Override 
+		public Query<?> getWrappedObject() { 
+			return query; 
+		}
+		
+		@Override 
+		public List<?> list() { 
+			return query.list(); 
+		}
+		
+		@Override 
+		public void setMaxResults(int i) { 
+			query.setMaxResults(i); 
+		}
+		
+		@Override 
+		public void setParameterList(String parameter, List<?> list, Object anything) {
+			query.setParameterList(parameter, list);
+		}
+		
+		@Override 
+		public void setParameter(String parameter, Object value, Object anything) {
+			query.setParameter(parameter, value);
+		}
+		
+		@Override 
+		public void setParameter(int position, Object value, Object anything) {
+			query.setParameter(position, value);
+		}
+		
+		@Override 
+		public String[] getReturnAliases() { 
+			return new String[0]; 
+		}
+		
+		@Override 
+		public Type[] getReturnTypes() {
+			return new Type[0]; 
+		}
+		
 	}
 
 }
