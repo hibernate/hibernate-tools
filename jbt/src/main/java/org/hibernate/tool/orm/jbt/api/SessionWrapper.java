f65c39d566b8a1fef8f8532bc7af7380e2d2af03
Koen Aers <koen.aers@gmail.com>
2024-06-03 14:57:51 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.SessionWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.SessionWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.SessionWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/SessionWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/SessionWrapper.java
@@ -1,45 +1,17 @@
 package org.hibernate.tool.orm.jbt.api;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
-
 public interface SessionWrapper extends Wrapper {
 
-	default String getEntityName(Object o) { return ((Session)getWrappedObject()).getEntityName(o); }
-
-	default SessionFactory getSessionFactory() { return ((Session)getWrappedObject()).getSessionFactory(); }
-
-	default Query<?> createQuery(String s) { return ((Session)getWrappedObject()).createQuery(s); }
-
-	default boolean isOpen() { return ((Session)getWrappedObject()).isOpen(); }
-
-	default void close() { ((Session)getWrappedObject()).close(); }
-
-	default boolean contains(Object o) { 
-		boolean result = false;
-		try {
-			result = ((Session)getWrappedObject()).contains(o);
-		} catch (IllegalArgumentException e) {
-			String message = e.getMessage();
-			if (!(message.startsWith("Class '") && message.endsWith("' is not an entity class"))) {
-				throw e;
-			}
-		}
-		return result;
-	}
-
-	default jakarta.persistence.Query createCriteria(Class<?> c) {
-		CriteriaBuilder criteriaBuilder = ((Session)getWrappedObject()).getCriteriaBuilder();
-		CriteriaQuery<?> criteriaQuery = criteriaBuilder.createQuery(c);
-		Root root = criteriaQuery.from(c);
-		criteriaQuery.select(root);
-		return ((Session)getWrappedObject()).createQuery(criteriaQuery);
-	}
+	String getEntityName(Object o) ;
+	SessionFactory getSessionFactory();
+	Query<?> createQuery(String s);
+	boolean isOpen();
+	void close();
+	boolean contains(Object o);
+	jakarta.persistence.Query createCriteria(Class<?> c);
 
 }
