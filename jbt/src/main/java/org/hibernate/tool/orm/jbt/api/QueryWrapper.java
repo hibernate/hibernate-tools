7ee21621d9f488a85ae25b223b770400ca3c02df
Koen Aers <koen.aers@gmail.com>
2024-06-03 17:19:58 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.QueryWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.QueryWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.QueryWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/QueryWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/QueryWrapper.java
@@ -2,24 +2,17 @@ package org.hibernate.tool.orm.jbt.api;
 
 import java.util.List;
 
-import org.hibernate.query.Query;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 import org.hibernate.type.Type;
 
 public interface QueryWrapper extends Wrapper {
 
-	default List<?> list() { return ((Query<?>)getWrappedObject()).list(); }
-	default void setMaxResults(int i) { ((Query<?>)getWrappedObject()).setMaxResults(i); }
-	default void setParameterList(String parameter, List<?> list, Object anything) {
-		((Query<?>)getWrappedObject()).setParameterList(parameter, list);
-	}
-	default void setParameter(String parameter, Object value, Object anything) {
-		((Query<?>)getWrappedObject()).setParameter(parameter, value);
-	}
-	default void setParameter(int position, Object value, Object anything) {
-		((Query<?>)getWrappedObject()).setParameter(position, value);
-	}
-	default String[] getReturnAliases() { return new String[0]; }
-	default Type[] getReturnTypes() {return new Type[0]; }
+	List<?> list();
+	void setMaxResults(int i);
+	void setParameterList(String parameter, List<?> list, Object anything);
+	void setParameter(String parameter, Object value, Object anything);
+	void setParameter(int position, Object value, Object anything);
+	String[] getReturnAliases();
+	Type[] getReturnTypes();
 	
 }
