0d9f83616af151d69fc53641484ea890fa815a31
Koen Aers <koen.aers@gmail.com>
2024-06-03 16:53:35 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Review interface 'org.hibernate.tool.orm.jbt.api.RevengStrategyWrapper'   - Adapt implementation class 'org.hibernate.tool.orm.jbt.internal.factory.RevengStrategyWrapperFactory'   - Adapt test class 'org.hibernate.tool.orm.jbt.api.RevengStrategyWrapperTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/RevengStrategyWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/RevengStrategyWrapper.java
@@ -1,10 +1,9 @@
 package org.hibernate.tool.orm.jbt.api;
 
-import org.hibernate.tool.api.reveng.RevengSettings;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface RevengStrategyWrapper extends Wrapper {
 
-	void setSettings(RevengSettings revengSettings);
+	void setSettings(RevengSettingsWrapper revengSettings);
 
 }
