3db43aedb296aa8b45ff33264cbab4b49e80a284
Koen Aers <koen.aers@gmail.com>
2024-06-04 12:38:33 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.PrimaryKeyWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.PrimaryKeyWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.PrimaryKeyWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/PrimaryKeyWrapperFactory.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/internal/factory/PrimaryKeyWrapperFactory.java
@@ -1,14 +1,72 @@
 package org.hibernate.tool.orm.jbt.internal.factory;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.mapping.Column;
 import org.hibernate.mapping.PrimaryKey;
+import org.hibernate.mapping.Table;
 import org.hibernate.tool.orm.jbt.api.PrimaryKeyWrapper;
 
 public class PrimaryKeyWrapperFactory {
 
 	public static PrimaryKeyWrapper createPrimaryKeyWrapper(PrimaryKey wrappedPrimaryKey) {
-		return new PrimaryKeyWrapper() {
-			@Override public PrimaryKey getWrappedObject() { return wrappedPrimaryKey; }
-		};
+		return new PrimaryKeyWrapperImpl(wrappedPrimaryKey);
+	}
+	
+	private static class PrimaryKeyWrapperImpl implements PrimaryKeyWrapper {
+		
+		private PrimaryKey primaryKey = null;
+		
+		private PrimaryKeyWrapperImpl(PrimaryKey primaryKey) {
+			this.primaryKey = primaryKey;
+		}
+		
+		@Override 
+		public PrimaryKey getWrappedObject() { 
+			return primaryKey; 
+		}
+		
+		@Override 
+		public void addColumn(Column column) { 
+			primaryKey.addColumn(column); 
+		}
+		
+		@Override 
+		public int getColumnSpan() { 
+			return primaryKey.getColumnSpan(); 
+		}
+		
+		@Override 
+		public List<Column> getColumns() { 
+			return primaryKey.getColumns(); 
+		}
+		
+		@Override 
+		public Column getColumn(int i) { 
+			return primaryKey.getColumn(i); 
+		}
+		
+		@Override 
+		public Table getTable() { 
+			return primaryKey.getTable(); 
+		}
+		
+		@Override 
+		public boolean containsColumn(Column column) { 
+			return primaryKey.containsColumn(column); 
+		}
+		
+		@Override 
+		public Iterator<Column> columnIterator() { 
+			return getColumns().iterator(); 
+		}
+		
+		@Override 
+		public String getName() { 
+			return primaryKey.getName(); 
+		}
+
 	}
 	
 }
