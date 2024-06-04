e69562f21a4aa75b60bccc9c92f6132161a7d3d0
Koen Aers <koen.aers@gmail.com>
2024-06-04 12:56:46 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Review interface 'org.hibernate.tool.orm.jbt.api.PrimaryKeyWrapper'   - Adapt implementation class 'org.hibernate.tool.orm.jbt.internal.factory.PrimaryKeyWrapperFactory'   - Adapt test class 'org.hibernate.tool.orm.jbt.api.PrimaryKeyWrapperTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/PrimaryKeyWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/PrimaryKeyWrapper.java
@@ -3,19 +3,17 @@ package org.hibernate.tool.orm.jbt.api;
 import java.util.Iterator;
 import java.util.List;
 
-import org.hibernate.mapping.Column;
-import org.hibernate.mapping.Table;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface PrimaryKeyWrapper extends Wrapper {
 
-	void addColumn(Column column);
+	void addColumn(ColumnWrapper column);
 	int getColumnSpan();
-	List<Column> getColumns();
-	Column getColumn(int i);
-	Table getTable();
-	boolean containsColumn(Column column);
-	Iterator<Column> columnIterator();
+	List<ColumnWrapper> getColumns();
+	ColumnWrapper getColumn(int i);
+	TableWrapper getTable();
+	boolean containsColumn(ColumnWrapper column);
+	Iterator<ColumnWrapper> columnIterator();
 	String getName();
 
 }
