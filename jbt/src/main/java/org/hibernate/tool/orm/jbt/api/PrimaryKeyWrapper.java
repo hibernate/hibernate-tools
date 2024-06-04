f66c9ed68630a40a85afca260791a039bbd8dc30
Koen Aers <koen.aers@gmail.com>
2024-06-04 12:38:33 +0200

    HBX-2788: Review Wrapper Interfaces and make sure they only accept/return Wrapper instances   - Remove the default implementations of interface 'org.hibernate.tool.orm.jbt.api.PrimaryKeyWrapper'   - Factory class 'org.hibernate.tool.orm.jbt.internal.factory.PrimaryKeyWrapperFactory' now returns an inner class implementation of interface 'org.hibernate.tool.orm.jbt.api.PrimaryKeyWrapper'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/main/java/org/hibernate/tool/orm/jbt/api/PrimaryKeyWrapper.java
+++ jbt/src/main/java/org/hibernate/tool/orm/jbt/api/PrimaryKeyWrapper.java
@@ -4,19 +4,18 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.hibernate.mapping.Column;
-import org.hibernate.mapping.PrimaryKey;
 import org.hibernate.mapping.Table;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 
 public interface PrimaryKeyWrapper extends Wrapper {
 
-	default void addColumn(Column column) { ((PrimaryKey)getWrappedObject()).addColumn(column); }
-	default int getColumnSpan() { return ((PrimaryKey)getWrappedObject()).getColumnSpan(); }
-	default List<Column> getColumns() { return ((PrimaryKey)getWrappedObject()).getColumns(); }
-	default Column getColumn(int i) { return ((PrimaryKey)getWrappedObject()).getColumn(i); }
-	default Table getTable() { return ((PrimaryKey)getWrappedObject()).getTable(); }
-	default boolean containsColumn(Column column) { return ((PrimaryKey)getWrappedObject()).containsColumn(column); }
-	default Iterator<Column> columnIterator() { return getColumns().iterator(); }
-	default String getName() { return ((PrimaryKey)getWrappedObject()).getName(); }
+	void addColumn(Column column);
+	int getColumnSpan();
+	List<Column> getColumns();
+	Column getColumn(int i);
+	Table getTable();
+	boolean containsColumn(Column column);
+	Iterator<Column> columnIterator();
+	String getName();
 
 }
