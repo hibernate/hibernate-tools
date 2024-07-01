b2e8606f804e4bd6797c5152497a419fe2fb9589
Koen Aers <koen.aers@gmail.com>
2024-07-01 13:40:31 +0300

    HBX-2835: Move classes from package 'org.hibernate.tool.orm.jbt.util' to package 'org.hibernate.tool.orm.jbt.internal.util'   - Move class 'org.hibernate.tool.orm.jbt.util.HibernateToolsPersistenceProvider'   - Move test class 'org.hibernate.tool.orm.jbt.util.HibernateToolsPersistenceProviderTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- jbt/src/test/java/org/hibernate/tool/orm/jbt/internal/util/HibernateToolsPersistenceProviderTest.java
+++ jbt/src/test/java/org/hibernate/tool/orm/jbt/internal/util/HibernateToolsPersistenceProviderTest.java
@@ -1,4 +1,4 @@
-package org.hibernate.tool.orm.jbt.util;
+package org.hibernate.tool.orm.jbt.internal.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -12,6 +12,7 @@ import java.nio.file.Files;
 import java.util.Properties;
 
 import org.hibernate.jpa.boot.internal.EntityManagerFactoryBuilderImpl;
+import org.hibernate.tool.orm.jbt.internal.util.HibernateToolsPersistenceProvider;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
