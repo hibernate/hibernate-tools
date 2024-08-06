826098d7b51629668ce7c33cd9c1df09072ee550
Jan Schatteman <jschatte@redhat.com>
2024-08-06 18:13:35 +0200

    HBX-2891: Update version identifier to 6.2.30.Final
    Signed-off-by: Jan Schatteman <jschatte@redhat.com>

--- orm/src/main/java/org/hibernate/tool/api/version/Version.java
+++ orm/src/main/java/org/hibernate/tool/api/version/Version.java
@@ -2,6 +2,6 @@ package org.hibernate.tool.api.version;
 
 public interface Version {
 	
-	final static String CURRENT_VERSION = "6.2.29-SNAPSHOT";
+	final static String CURRENT_VERSION = "6.2.30.Final";
 
 }
