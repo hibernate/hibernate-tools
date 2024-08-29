6116ad935496c0d206037378e129891700c0fe48
Jan Schatteman <jschatte@redhat.com>
2024-08-29 14:26:15 +0200

    HBX-2914: Update version identifier to 6.2.32-SNAPSHOT
    Signed-off-by: Jan Schatteman <jschatte@redhat.com>

--- orm/src/main/java/org/hibernate/tool/api/version/Version.java
+++ orm/src/main/java/org/hibernate/tool/api/version/Version.java
@@ -2,6 +2,6 @@ package org.hibernate.tool.api.version;
 
 public interface Version {
 	
-	final static String CURRENT_VERSION = "6.2.31.Final";
+	final static String CURRENT_VERSION = "6.2.32-SNAPSHOT";
 
 }
