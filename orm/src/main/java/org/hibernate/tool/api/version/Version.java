f515a5a4dc79c6a0b2f9317ac44ae66be267e777
Koen Aers <koen.aers@gmail.com>
2025-02-27 15:26:48 +0100

    HBX-2962: Inject the version string during the build
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/src/main/java/org/hibernate/tool/api/version/Version.java
+++ orm/src/main/java/org/hibernate/tool/api/version/Version.java
@@ -1,7 +1,16 @@
 package org.hibernate.tool.api.version;
 
 public interface Version {
-	
-	final static String CURRENT_VERSION = "6.4.11-SNAPSHOT";
+
+	/**
+	 * @deprecated Use {@link #versionString()} instead.
+	 */
+	@Deprecated
+	final static String CURRENT_VERSION = versionString();
+
+	static String versionString() {
+		// This implementation is replaced during the build with another one that returns the correct value.
+		return "UNKNOWN";
+	}
 
 }
