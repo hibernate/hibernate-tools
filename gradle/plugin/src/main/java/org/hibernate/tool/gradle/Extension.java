f045c315863acbc0a9ded7202791a60dc8dcfff6
fanste <mails@ist-einmalig.de>
2025-09-12 13:54:21 +0200

    HBX-3113: Add support for a reveng.xml (#5458)
    Co-authored-by: Stefan Grubisic <grubisef@schaeffler.com>

--- gradle/plugin/src/main/java/org/hibernate/tool/gradle/Extension.java
+++ gradle/plugin/src/main/java/org/hibernate/tool/gradle/Extension.java
@@ -9,6 +9,7 @@ public class Extension {
 	public String outputFolder = "generated-sources";
 	public String packageName = "";
 	public String revengStrategy = null;
+	public String revengFile = null;
 	public Boolean generateAnnotations = true;
 	public Boolean useGenerics = true;
 	
