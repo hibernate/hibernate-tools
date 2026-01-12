5f9e1ab0384044ffd36f6cfd405de8c54921e741
Koen Aers <koen.aers@gmail.com>
2026-01-12 14:37:53 +0100

    HBX-2042: Track the ignored tests that fail because of org.hibernate.NotYetImplementedFor6Exception   - Fix org.hibernate.tool.hbm2x.query.QueryExporterTest.TestCase#testQueryExporter
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/common/src/main/java/org/hibernate/tool/hbm2x/query/QueryExporterTest/User.java
+++ test/common/src/main/java/org/hibernate/tool/hbm2x/query/QueryExporterTest/User.java
@@ -27,32 +27,22 @@ public class User implements Serializable {
 	private static final long serialVersionUID = 
 			ObjectStreamClass.lookup(User.class).getSerialVersionUID();
 	
-	private String org;
-	private String name;
 	private Set<Group> groups = new HashSet<Group>();
+	private UserID userId;
 
 	public User(String name, String org) {
-		this.org = org;
-		this.name = name;
+		this.setUserId(new UserID(name, org));
 	}
 
 	public User() {
 	}
-
-	public String getName() {
-		return name;
+	
+	public void setUserId(UserID userId) {
+		this.userId = userId;
 	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getOrg() {
-		return org;
-	}
-
-	public void setOrg(String org) {
-		this.org = org;
+	
+	public UserID getUserId() {
+		return userId;
 	}
 
 	public Set<Group> getGroups() {
