69fa509e99f689bf657266a24e268c776a6dbbda
Koen Aers <koen.aers@gmail.com>
2026-01-14 18:37:15 +0100

    HBX-3276: Move the 'Apache Ant' tests in the 'test/common' module to the 'legacyTest' source tree in the 'ant' module   - Move the 'ant/GenericExport' test
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/common/src/main/java/org/hibernate/tool/test/db/DbTestSuite.java
+++ test/common/src/main/java/org/hibernate/tool/test/db/DbTestSuite.java
@@ -28,7 +28,6 @@ public class DbTestSuite {
 	@Nested public class Cfg2HbmWithPackageName extends org.hibernate.tool.ant.Cfg2HbmWithPackageName.TestCase {}
 	@Nested public class Cfg2HbmWithPackageNameAndReverseNamingStrategy extends org.hibernate.tool.ant.Cfg2HbmWithPackageNameAndReverseNamingStrategy.TestCase {}
 	@Nested public class EJB3Configuration extends org.hibernate.tool.ant.EJB3Configuration.TestCase {}
-	@Nested public class GenericExport extends org.hibernate.tool.ant.GenericExport.TestCase {}
 	@Nested public class Hbm2JavaConfiguration extends org.hibernate.tool.ant.Hbm2JavaConfiguration.TestCase {}
 	@Nested public class Hbm2JavaEJB3Configuration extends org.hibernate.tool.ant.Hbm2JavaEJB3Configuration.TestCase {}
 	@Nested public class HbmLint extends org.hibernate.tool.ant.HbmLint.TestCase {}
