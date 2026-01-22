b16c1ee64768e5dc7d597d5050fc6bbaff9e45e8
Koen Aers <koen.aers@gmail.com>
2026-01-22 11:44:19 +0100

    HBX-3282: Move the relevant tests in module 'test/nodb' to the 'legacyTest' source tree in the 'orm' module   - Move 'org.hibernate.tool.hbm2x.PropertiesTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/src/legacyTest/java/org/hibernate/tool/hbm2x/PropertiesTest/TestCase.java
+++ orm/src/legacyTest/java/org/hibernate/tool/hbm2x/PropertiesTest/TestCase.java
@@ -18,20 +18,6 @@
 
 package org.hibernate.tool.hbm2x.PropertiesTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.File;
-import java.nio.file.Files;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
-
 import org.hibernate.tool.api.export.Exporter;
 import org.hibernate.tool.api.export.ExporterConstants;
 import org.hibernate.tool.api.export.ExporterFactory;
@@ -39,10 +25,10 @@ import org.hibernate.tool.api.export.ExporterType;
 import org.hibernate.tool.api.metadata.MetadataDescriptor;
 import org.hibernate.tool.internal.export.common.DefaultArtifactCollector;
 import org.hibernate.tool.internal.export.hbm.HbmExporter;
-import org.hibernate.tools.test.util.FileUtil;
-import org.hibernate.tools.test.util.HibernateUtil;
-import org.hibernate.tools.test.util.JUnitUtil;
-import org.hibernate.tools.test.util.JavaUtil;
+import org.hibernate.tool.test.utils.FileUtil;
+import org.hibernate.tool.test.utils.HibernateUtil;
+import org.hibernate.tool.test.utils.JUnitUtil;
+import org.hibernate.tool.test.utils.JavaUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -50,6 +36,17 @@ import org.junit.jupiter.api.io.TempDir;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Josh Moore josh.moore@gmx.de
@@ -68,15 +65,14 @@ public class TestCase {
 	
 	private DefaultArtifactCollector artifactCollector;
 	private File outputDir = null;
-	private File resourcesDir = null;
-	
-	@BeforeEach
+
+    @BeforeEach
 	public void setUp() throws Exception {
 		artifactCollector = new DefaultArtifactCollector();
 		outputDir = new File(outputFolder, "src");
-		outputDir.mkdir();
-		resourcesDir = new File(outputFolder, "resources");
-		resourcesDir.mkdir();
+		assertTrue(outputDir.mkdir());
+        File resourcesDir = new File(outputFolder, "resources");
+		assertTrue(resourcesDir.mkdir());
 		MetadataDescriptor metadataDescriptor = HibernateUtil
 				.initializeMetadataDescriptor(this, HBM_XML_FILES, resourcesDir);
 		Exporter exporter = ExporterFactory.createExporter(ExporterType.JAVA);
@@ -110,7 +106,7 @@ public class TestCase {
 				.evaluate(document, XPathConstants.NODESET);
 		assertEquals(1, nodeList.getLength(), "Expected to get one properties element");
 		Element element = (Element) nodeList.item(0);
-		assertEquals(element.getAttribute( "name" ),"emergencyContact");
+		assertEquals("emergencyContact", element.getAttribute( "name" ));
 		assertNotNull(
 				FileUtil.findFirstString(
 						"name", 
@@ -125,10 +121,10 @@ public class TestCase {
 	@Test
 	public void testCompilable() throws Exception {
 		String propertiesUsageResourcePath = "/org/hibernate/tool/hbm2x/PropertiesTest/PropertiesUsage.java_";
-		File propertiesUsageOrigin = new File(getClass().getResource(propertiesUsageResourcePath).toURI());
+		File propertiesUsageOrigin = new File(Objects.requireNonNull(getClass().getResource(propertiesUsageResourcePath)).toURI());
 		File propertiesUsageDestination = new File(outputDir, "properties/PropertiesUsage.java");
 		File targetDir = new File(outputFolder, "target" );
-		targetDir.mkdir();	
+		assertTrue(targetDir.mkdir());
 		Files.copy(propertiesUsageOrigin.toPath(), propertiesUsageDestination.toPath());
 		JavaUtil.compile(outputDir, targetDir);
 		assertTrue(new File(targetDir, "properties/PCompany.class").exists());
