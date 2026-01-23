ad615d4681653e851d14d42752e843d9533995c8
Koen Aers <koen.aers@gmail.com>
2026-01-23 12:43:05 +0100

    HBX-3282: Move the relevant tests in module 'test/nodb' to the 'legacyTest' source tree in the 'orm' module   - Move 'org.hibernate.tool.hbm2x.Hbm2JavaConstructorTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/src/legacyTest/java/org/hibernate/tool/hbm2x/Hbm2JavaConstructorTest/TestCase.java
+++ orm/src/legacyTest/java/org/hibernate/tool/hbm2x/Hbm2JavaConstructorTest/TestCase.java
@@ -18,17 +18,6 @@
 
 package org.hibernate.tool.hbm2x.Hbm2JavaConstructorTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.File;
-import java.nio.file.Files;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import org.hibernate.boot.Metadata;
 import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.Property;
@@ -40,13 +29,22 @@ import org.hibernate.tool.api.metadata.MetadataDescriptor;
 import org.hibernate.tool.internal.export.java.Cfg2JavaTool;
 import org.hibernate.tool.internal.export.java.EntityPOJOClass;
 import org.hibernate.tool.internal.export.java.POJOClass;
-import org.hibernate.tools.test.util.FileUtil;
-import org.hibernate.tools.test.util.HibernateUtil;
-import org.hibernate.tools.test.util.JavaUtil;
+import org.hibernate.tool.test.utils.FileUtil;
+import org.hibernate.tool.test.utils.HibernateUtil;
+import org.hibernate.tool.test.utils.JavaUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author max
@@ -64,16 +62,15 @@ public class TestCase {
 	public File outputFolder = new File("output");
 	
 	private File srcDir = null;
-	private File resourcesDir = null;
-	
-	private Metadata metadata = null;
+
+    private Metadata metadata = null;
 	
 	@BeforeEach
 	public void setUp() throws Exception {
 		srcDir = new File(outputFolder, "src");
-		srcDir.mkdir();
-		resourcesDir = new File(outputFolder, "resources");
-		resourcesDir.mkdir();
+		assertTrue(srcDir.mkdir());
+        File resourcesDir = new File(outputFolder, "resources");
+		assertTrue(resourcesDir.mkdir());
 		MetadataDescriptor metadataDescriptor = HibernateUtil
 				.initializeMetadataDescriptor(this, HBM_XML_FILES, resourcesDir);
 		metadata = metadataDescriptor.createMetadata();
@@ -86,10 +83,10 @@ public class TestCase {
 	@Test
 	public void testCompilable() throws Exception {
 		String constructorUsageResourcePath = "/org/hibernate/tool/hbm2x/Hbm2JavaConstructorTest/ConstructorUsage.java_";
-		File constructorUsageOrigin = new File(getClass().getResource(constructorUsageResourcePath).toURI());
+		File constructorUsageOrigin = new File(Objects.requireNonNull(getClass().getResource(constructorUsageResourcePath)).toURI());
 		File constructorUsageDestination = new File(srcDir, "ConstructorUsage.java");
 		File targetDir = new File(outputFolder, "compilerOutput" );
-		targetDir.mkdir();	
+		assertTrue(targetDir.mkdir());
 		Files.copy(constructorUsageOrigin.toPath(), constructorUsageDestination.toPath());
 		JavaUtil.compile(srcDir, targetDir);
 		assertTrue(new File(targetDir, "ConstructorUsage.class").exists());
@@ -100,7 +97,7 @@ public class TestCase {
 
 	@Test
 	public void testNoVelocityLeftOvers() {
-		assertNull(FileUtil.findFirstString( 
+		assertNull(FileUtil.findFirstString(
 				"$", 
 				new File(srcDir, "Company.java" ) ) );
 		assertNull(FileUtil.findFirstString(
@@ -156,16 +153,15 @@ public class TestCase {
 	}
 	
 	private void assertNoDuplicates(List<?> bigall) {
-		Set<Object> set = new HashSet<Object>();
-		set.addAll(bigall);
+        Set<Object> set = new HashSet<>(bigall);
 		assertEquals(set.size(),bigall.size(), "list had duplicates!");	
 	}
 
 	private void assertNoOverlap(List<?> first, List<?> second) {
-		Set<Object> set = new HashSet<Object>();
+		Set<Object> set = new HashSet<>();
 		set.addAll(first);
 		set.addAll(second);	
-		assertEquals(set.size(),first.size()+second.size());		
+		assertEquals(set.size(),first.size()+second.size());
 	}
 
 }
